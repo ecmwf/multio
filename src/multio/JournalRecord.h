89b28ee46d750e7daf1cfff52746c8fbde341182
Simon Smart <simon.smart@ecmwf.int>
2015-12-17 17:22:26 +0000

    Add JournalEntry struct to JournalRecord

--- src/multio/JournalRecord.h
+++ src/multio/JournalRecord.h
@@ -19,6 +19,7 @@
 #include <string>
 #include <vector>
 #include <sys/time.h>
+#include <stdint.h>
 
 #include "eckit/memory/NonCopyable.h"
 #include "eckit/types/FixedString.h"
@@ -50,6 +51,8 @@ struct JournalRecord {
     //   -- End
     //      >> Code 'E'
 
+    // ---------------------------------------------------------
+
     enum RecordType {
         EndOfJournal
     };
@@ -67,10 +70,35 @@ struct JournalRecord {
                                          /// TOTAL: 32
     } head_;
 
+    // ---------------------------------------------------------
+
+    // Inside the JournalRecord, there are a series of JournalEntries.
+    // Each of these starts with a (very brief) header, describing what it is.
+
+    struct JournalEntry {
+
+        enum EntryType {
+            Data = 'D',
+            Entry = 'J',
+            End = 'E'
+        };
+
+        struct Header {
+
+            unsigned char     tag_;
+            unsigned char     unused_[3];
+
+            uint64_t          payload_length;
+
+        } head_;
+    };
+
+
     // Add a way to stream all the journal elements out.
 
     // Payload goes here (should depend on the specific type. Will be pointed to.
     // eckit::ScopedPtr<JournalRecordPayload> payload_
+    // ---------------------------------------------------------
 
     eckit::FixedString<4> marker_;           /// (4) Termination marker
 

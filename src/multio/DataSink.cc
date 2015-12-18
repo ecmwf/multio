04acc1183160eba892ffbb62afc02d4eb2b7d8ba
Simon Smart <simon.smart@ecmwf.int>
2015-12-18 18:18:00 +0000

    Ooops. A DataSink should write the journal entry if it does not belong to a parent
    Not only if it does...

--- src/multio/DataSink.cc
+++ src/multio/DataSink.cc
@@ -121,7 +121,7 @@ void DataSink::write(const void* buffer, const Length& length, JournalRecord * c
     // If we are the creator of the journal record, we are responsible for ensuring that it
     // gets written if it is populated.
     // TODO: Add test here. For now, ALWAYS WRITE.
-    if (parent_record) {
+    if (!parent_record) {
         journal_->write_record(journal_record);
     }
 

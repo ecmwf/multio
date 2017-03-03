1c59397964a7b706a56e3a112b60786e4c13dbe5
Simon Smart <simon.smart@ecmwf.int>
2017-03-03 10:02:37 +0000

    Add LibMultio object --> specific log channels available

---
+++ src/multio/LibMultio.h
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 1996-2017 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @author Simon Smart
+/// @date   March 2017
+
+#ifndef eckit_system_LibMultio_H
+#define eckit_system_LibMultio_H
+
+#include "eckit/system/Library.h"
+
+namespace multio {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class LibMultio : public eckit::system::Library {
+public:
+
+    LibMultio();
+
+    static const LibMultio& instance();
+
+protected:
+
+    const void* addr() const;
+
+    virtual std::string version() const;
+
+    virtual std::string gitsha1(unsigned int count) const;
+
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+} // namespace multio
+
+#endif // eckit_system_LibMultio_H

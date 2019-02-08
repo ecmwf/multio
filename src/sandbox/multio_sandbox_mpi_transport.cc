a79472df8ed08d1bf899b3ad0124ad06ff37bcaf
Domokos Sármány <domokos.sarmany@ecmwf.int>
2019-02-08 17:57:12 +0000

    Add placeholder executables for TCP and MPI transports

---
+++ src/sandbox/multio_sandbox_mpi_transport.cc
@@ -0,0 +1,8 @@
+
+#include <iostream>
+
+//---------------------------------------------------------------------------------------------------
+
+int main(int argc, char** argv) {
+    std::cout << "placeholder for MultioMpiTool" << std::endl;
+}

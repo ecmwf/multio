ddc9d0b810e5e244a587fbcc0dbcd9b44e18cba3
Domokos Sármány <domokos.sarmany@ecmwf.int>
2019-02-11 11:26:40 +0000

    ThreadExample: use std::tuple to return all 'out' parameters

--- src/sandbox/multio_sandbox_thread_transport.cc
+++ src/sandbox/multio_sandbox_thread_transport.cc
@@ -1,10 +1,6 @@
 
 #include <algorithm>
-#include <chrono>
-#include <functional>
 #include <iostream>
-#include <mutex>
-#include <string>
 #include <thread>
 
 #include "eckit/config/YAMLConfiguration.h"
@@ -18,8 +14,7 @@
 #include "sandbox/Peer.h"
 #include "sandbox/Transport.h"
 
-using namespace eckit;
-using namespace multio;
+using eckit::Log;
 using namespace multio::sandbox;
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -29,7 +24,8 @@ public:  // methods
 
     ThreadExample(int argc, char** argv);
 
-    virtual void usage(const std::string &tool) const {
+private:
+    void usage(const std::string &tool) const override {
         Log::info() << std::endl
                     << "Usage: " << tool << " [options]" << std::endl
                     << std::endl
@@ -41,20 +37,16 @@ public:  // methods
                     << std::endl;
     }
 
-protected:  // methods
-    virtual void init(const eckit::option::CmdArgs& args);
+    void init(const eckit::option::CmdArgs& args) override;
 
-private:  // methods
+    void execute(const eckit::option::CmdArgs& args) override;
 
-    virtual void execute(const eckit::option::CmdArgs& args);
+    std::tuple<std::vector<Peer>, std::vector<std::thread>> spawnServers(
+        const eckit::Configuration& config, std::shared_ptr<Transport> transport, size_t nbServers);
 
-    std::vector<std::thread> spawnServers(const Configuration& config,
-                                          std::shared_ptr<Transport> transport, size_t nbServers,
-                                          std::vector<Peer>& peerServers);
     std::vector<std::thread> spawnClients(std::shared_ptr<Transport> transport, size_t nbClients,
-                                          const std::vector<Peer>& servers);
+                                          const std::vector<Peer>& serverPeers);
 
-private:  // members
     size_t nbClients_ = 1;
 };
 
@@ -62,80 +54,77 @@ ThreadExample::ThreadExample(int argc, char** argv) : MultioServerTool(argc, arg
     options_.push_back(new eckit::option::SimpleOption<size_t>("nbclients", "Number of clients"));
 }
 
-void ThreadExample::init(const option::CmdArgs& args) {
+void ThreadExample::init(const eckit::option::CmdArgs& args) {
     MultioServerTool::init(args);
     args.get("nbclients", nbClients_);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-std::vector<std::thread> ThreadExample::spawnServers(const eckit::Configuration& config,
-                                                   std::shared_ptr<Transport> transport,
-                                                   size_t nbServers,
-                                                   std::vector<Peer>& peerServers) {
+std::tuple<std::vector<Peer>, std::vector<std::thread>> ThreadExample::spawnServers(
+    const eckit::Configuration& config, std::shared_ptr<Transport> transport, size_t nbServers) {
     auto listen = [&config, transport]() {
         Listener listener(config, *transport);
 
         listener.listen();
     };
 
+    std::vector<Peer> serverPeers;
     std::vector<std::thread> servers;
-
     for (size_t i = 0; i != nbServers; ++i) {
-        eckit::Log::info() << "starting server " << i << std::endl;
+        Log::info() << "starting server " << i << std::endl;
 
         std::thread t(listen);
 
-        peerServers.push_back(Peer("thread", std::hash<std::thread::id>{}(t.get_id())));
+        serverPeers.push_back(Peer("thread", std::hash<std::thread::id>{}(t.get_id())));
 
-        servers.emplace_back(std::move(t));
+        servers.push_back(std::move(t));
     }
 
-    return servers;
+    return std::make_tuple(serverPeers, std::move(servers));
 }
 
 std::vector<std::thread> ThreadExample::spawnClients(std::shared_ptr<Transport> transport,
-                                                   size_t nbClients,
-                                                   const std::vector<Peer>& servers) {
-    const int nmessages = 10;
+                                                     size_t nbClients,
+                                                     const std::vector<Peer>& serverPeers) {
+    auto sendMsg = [transport, serverPeers]() {
+        Peer client = transport->localPeer();
+
+        // open all servers
+        for (auto& server : serverPeers) {
+            Message open{Message::Tag::Open, client, server, std::string("open")};
+            transport->send(open);
+        }
+
+        // send N messages
+        const int nmessages = 10;
+        for (int ii = 0; ii < nmessages; ++ii) {
+            for (auto& server : serverPeers) {
+                std::ostringstream oss;
+                oss << "Once upon a midnight dreary " << " + " << client;
+
+                Message msg{Message::Tag::Field, client, server, oss.str()};
+
+                transport->send(msg);
+            }
+        }
+
+        // close all servers
+        for (auto& server : serverPeers) {
+            Message close{Message::Tag::Close, client, server, std::string("close")};
+            transport->send(close);
+        }
+    };
 
     std::vector<std::thread> clients;
 
-    for (size_t i = 0; i != nbClients; ++i) {
-        auto sendMsg = [transport, servers]() {
-            Peer client = transport->localPeer();
-
-            // open all servers
-            for (auto& server : servers) {
-                Message open{Message::Tag::Open, client, server, std::string("open")};
-                transport->send(open);
-            }
-
-            // send N messages
-            for (int i = 0; i < nmessages; ++i) {
-                for (auto& server : servers) {
-                    std::ostringstream oss;
-                    oss << "Once upon a midnight dreary "
-                        << " + " << client << "\n";
-
-                    Message msg{Message::Tag::Field, client, server, oss.str()};
-
-                    transport->send(msg);
-                }
-            }
-
-            // close all servers
-            for (auto& server : servers) {
-                Message close{Message::Tag::Close, client, server, std::string("close")};
-                transport->send(close);
-            }
-        };
-
+    for (size_t ii = 0; ii != nbClients; ++ii) {
         clients.emplace_back(sendMsg);
     }
 
     return clients;
 }
+
 
 std::string local_plan() {
     return R"json(
@@ -162,23 +151,16 @@ std::string local_plan() {
     )json";
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-
 void ThreadExample::execute(const eckit::option::CmdArgs&) {
-
     eckit::YAMLConfiguration config{local_plan()};
 
-    std::shared_ptr<Transport> transport(TransportFactory::instance().build("Thread", config));
+    std::shared_ptr<Transport> transport{TransportFactory::instance().build("Thread", config)};
 
-    // spawn servers
+    std::vector<Peer> serverPeers;
+    std::vector<std::thread> servers;
+    std::tie(serverPeers, servers) = spawnServers(config, transport, nbServers_);
 
-    std::vector<Peer> peerServers;
-
-    std::vector<std::thread> servers = spawnServers(config, transport, nbServers_, peerServers);
-
-    // spawn clients
-
-    std::vector<std::thread> clients = spawnClients(transport, nbClients_, peerServers);
+    std::vector<std::thread> clients = spawnClients(transport, nbClients_, serverPeers);
 
     std::for_each(begin(clients), end(clients), [](std::thread& t) { t.join(); });
     std::for_each(begin(servers), end(servers), [](std::thread& t) { t.join(); });

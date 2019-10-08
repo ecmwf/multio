8534ea68e68b5649f103aaccfb95c89cfd571099
Domokos Sármány <domokos.sarmany@ecmwf.int>
2019-10-08 15:54:03 +0100

    Add multio-hammer tests

--- tests/server/tcp_test.sh
+++ tests/server/tcp_test.sh
@@ -1,6 +1,8 @@
 
+binary=${1:-multio-hammer}
+
 function fork_tcp_transport {
-    local cmd="multio-hammer --transport=\"tcp\" --port=$@ --nbclients=5 --nbservers=3"
+    local cmd="$binary --transport=\"tcp\" --port=$@ --nbclients=5 --nbservers=3"
     eval $cmd
 }
 
@@ -8,12 +10,10 @@ fork_tcp_transport 7771 &
 fork_tcp_transport 7772 &
 fork_tcp_transport 7773 &
 
-sleep 0.01
+sleep 0.05
 
 fork_tcp_transport 4441 &
 fork_tcp_transport 4442 &
 fork_tcp_transport 4443 &
 fork_tcp_transport 4444 &
 fork_tcp_transport 4445
-
-sleep 1

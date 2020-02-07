de36ef61f1a96e4cbab43e1b8d8d968acca90530
Domokos Sármány <domokos.sarmany@ecmwf.int>
2020-02-07 17:38:18 +0000

    MUL-16: Fix grib encoding

--- src/multio/server/actions/Encode.cc
+++ src/multio/server/actions/Encode.cc
@@ -17,6 +17,7 @@
 #include "eckit/config/Configuration.h"
 #include "eckit/io/StdFile.h"
 
+#include "metkit/grib/GribDataBlob.h"
 #include "metkit/grib/GribHandle.h"
 
 #include "multio/LibMultio.h"
@@ -32,20 +33,39 @@ eckit::PathName configuration_path() {
 }
 
 class GribEncoder : public metkit::grib::GribHandle {
+    using NemoKey = std::string;
+
+    struct GribData {
+        long param;
+        std::string gridType;
+    };
+
+    std::map<NemoKey, GribData> parameters_ = {{"sst", {151129, "orca_grid_T"}},
+                                               {"ssu", {151131, "orca_grid_U"}},
+                                               {"ssv", {151132, "orca_grid_V"}},
+                                               {"ssw", {151133, "orca_grid_W"}}};
+
 public:
     GribEncoder(codes_handle* handle) : metkit::grib::GribHandle{handle} {}
 
     void setValue(const std::string& key, long value) {
+        eckit::Log::debug<multio::LibMultio>() << "Setting value for key " << key << std::endl;
         CODES_CHECK(codes_set_long(raw(), key.c_str(), value), NULL);
     }
 
     void setValue(const std::string& key, double value) {
+        eckit::Log::debug<multio::LibMultio>() << "Setting value for key " << key << std::endl;
         CODES_CHECK(codes_set_double(raw(), key.c_str(), value), NULL);
     }
 
     void setValue(const std::string& key, const std::string& value) {
+        eckit::Log::debug<multio::LibMultio>() << "Setting value for key " << key << std::endl;
         size_t sz = value.size();
         CODES_CHECK(codes_set_string(raw(), key.c_str(), value.c_str(), &sz), NULL);
+    }
+
+    long getParam(const NemoKey& key) {
+        return parameters_[key].param;
     }
 };
 }  // namespace
@@ -60,6 +80,7 @@ Encode::Encode(const eckit::Configuration& config) :
     template_{config.has("template") ? config.getString("template") : ""} {}
 
 void Encode::execute(Message msg) const {
+
     if (format_ == "grib") {
         eckit::Log::debug<LibMultio>() << "*** Executing encoding: " << *this << std::endl;
 
@@ -70,22 +91,29 @@ void Encode::execute(Message msg) const {
         const auto& md = msg.metadata();
 
         // setCommonMetadata
-        encoder.setValue("expver", md.getString("expver"));
-        encoder.setValue("class", md.getString("class"));
-        encoder.setValue("stream", md.getString("stream"));
-        encoder.setValue("type", md.getString("type"));
-        encoder.setValue("levtype", md.getLong("levtype"));
-        encoder.setValue("step", md.getLong("step"));
-        encoder.setValue("level", md.getLong("level"));
+        encoder.setValue("expver", "xxxx");
+        encoder.setValue("class", "rd");
+        encoder.setValue("stream", "oper");
+        encoder.setValue("type", "fc");
+        encoder.setValue("levtype", static_cast<long>(168));
+        encoder.setValue("step", md.getLong("istep"));
+        encoder.setValue("level", md.getLong("ilevg"));
 
         // setDomainDimensions
-        encoder.setValue("numberOfDataPoints", md.getLong("globalSize"));
-        encoder.setValue("numberOfValues", md.getLong("globalSize"));
+        encoder.setValue("numberOfDataPoints", md.getLong("isizeg"));
+        encoder.setValue("numberOfValues", md.getLong("isizeg"));
 
-        encoder.setValue("param", md.getLong("param"));
+        // Setting parameter ID
+        encoder.setValue("param", encoder.getParam(md.getString("igrib")));
 
+        // Setting field values
         auto beg = reinterpret_cast<const double*>(msg.payload().data());
         encoder.setDataValues(beg, msg.globalSize());
+
+        eckit::Buffer buf{encoder.message()->length()};
+        encoder.write(buf);
+        msg =
+            Message{Message::Header{Message::Tag::Grib, Peer{"", 0}, Peer{"", 0}}, std::move(buf)};
     }
     else if (format_ == "none") {
         ; // leave in raw binary format
@@ -99,10 +127,12 @@ void Encode::execute(Message msg) const {
     }
 }
 
-        void Encode::print(std::ostream & os) const { os << "Encode(format=" << format_ << ")"; }
+void Encode::print(std::ostream& os) const {
+    os << "Encode(format=" << format_ << ")";
+}
 
-        static ActionBuilder<Encode> EncodeBuilder("Encode");
+static ActionBuilder<Encode> EncodeBuilder("Encode");
 
-    }  // namespace actions
+}  // namespace actions
 }  // namespace server
 }  // namespace multio

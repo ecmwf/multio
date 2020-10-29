55120f7a3c7970168b7de185843fcfbc9a1eb919
Domokos Sármány <domokos.sarmany@ecmwf.int>
2020-10-29 20:23:49 +0000

    Allow mpi_args to be empty

--- tests/server/replay-nemo.sh
+++ tests/server/replay-nemo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mpi_args=$1
-client_binary=${2:-multio-replay}
-server_binary=${3:-multio-probe}
+client_binary=${1:-multio-replay}
+server_binary=${2:-multio-probe}
+mpi_args=${3:- }
 
 function launch_mpi {
     local cmd="mpiexec $mpi_args -np 7 $client_binary : -np 1 $server_binary --test"

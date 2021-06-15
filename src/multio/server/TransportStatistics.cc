f5844ccdf9730a23df792decfb9507fd86f58c14
Domokos Sármány <domokos.sarmany@ecmwf.int>
2021-06-15 18:01:00 +0100

    MUL-35: Add framework for transport statistics

---
+++ src/multio/server/TransportStatistics.cc
@@ -0,0 +1,10 @@
+
+#include "TransportStatistics.h"
+
+namespace multio {
+namespace server {
+
+TransportStatistics::TransportStatistics() {}
+
+}  // namespace server
+}  // namespace multio
---
+++ src/multio/server/TransportStatistics.h
@@ -0,0 +1,36 @@
+
+#ifndef multio_server_TransportStatistics_H
+#define multio_server_TransportStatistics_H
+
+#include <iosfwd>
+
+#include <eckit/log/Statistics.h>
+
+namespace multio {
+namespace server {
+
+class TransportStatistics {
+public:
+    TransportStatistics();
+
+    std::size_t sendCount_;
+    std::size_t receiveCount_;
+    std::size_t sendSize_;
+    std::size_t receiveSize_;
+
+    eckit::Timing sendTiming_;
+    eckit::Timing totSendTiming_;
+    eckit::Timing receiveTiming_;
+    eckit::Timing probeTiming_;
+    eckit::Timing pushToQueueTiming_;
+    eckit::Timing decodeTiming_;
+    eckit::Timing returnTiming_;
+    eckit::Timing totReturnTiming_;
+
+    void report(std::ostream &out, const char* indent = "") const;
+};
+
+}  // namespace server
+}  // namespace multio
+
+#endif // multio_server_TransportStatistics_H

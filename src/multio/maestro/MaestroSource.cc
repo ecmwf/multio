5645ab12b591bf2a87ce6f6c93d2b6309607b14f
Milos Lompar <Milos.Lompar@ecmwf.int>
2021-09-21 22:08:48 +0200

    Use fields size operator to return from MaestroSource

--- src/multio/maestro/MaestroSource.cc
+++ src/multio/maestro/MaestroSource.cc
@@ -13,12 +13,11 @@ MaestroSource::MaestroSource(const eckit::option::CmdArgs &args):
 
 size_t MaestroSource::retrieve(const std::map<std::string, std::string> &retrieve, eckit::Buffer &field) const {
     auto cdo_name = cdo_namer_.name(retrieve);
-    auto&cdo = CdoMap::instance().at(cdo_name);
+    auto& cdo = CdoMap::instance().at(cdo_name);
     cdo.demand();
     field = std::move(eckit::Buffer{cdo.data(), cdo.size()});
-    auto size = cdo.size();
     CdoMap::instance().erase(cdo_name);
-    return size;
+    return field.size();
 }
 
 void MaestroSource::print(std::ostream& out) const {

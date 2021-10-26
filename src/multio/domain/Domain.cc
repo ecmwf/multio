02f48ae083d0587cec790501237bcbb2abc3772a
Domokos Sármány <domokos.sarmany@ecmwf.int>
2021-10-26 18:38:05 +0100

    Check if all global values are filled in

--- src/multio/domain/Domain.cc
+++ src/multio/domain/Domain.cc
@@ -7,6 +7,7 @@
 
 #include "multio/message/Message.h"
 #include "multio/LibMultio.h"
+#include "multio/util/print_buffer.h"
 
 namespace multio {
 namespace domain {
@@ -76,6 +77,10 @@ void Structured::to_global(const message::Message& local, message::Message& glob
     auto data_nj = definition_[10];
     // auto data_dim = definition_[6]; -- Unused here
 
+    eckit::Log::info() << "*** Aggregator -- mapping info: ";
+    util::print_buffer(definition_, eckit::Log::info());
+    eckit::Log::info() << std::endl;
+
     ASSERT(sizeof(double) * ni_global * nj_global * levelCount == global.size());
     std::ostringstream os;
     os << "Local size is " << local.payload().size() / levelCount / sizeof(double)
@@ -84,18 +89,29 @@ void Structured::to_global(const message::Message& local, message::Message& glob
 
     auto lit = static_cast<const double*>(local.payload().data());
     auto git = static_cast<double*>(global.payload().data());
+    static std::set<int32_t> glIndices;
+    static auto domainCount = 0;
     for (long lev = 0; lev != levelCount; ++lev) {
         auto offset = lev * local.globalSize();
         for (auto j = data_jbegin; j != data_jbegin + data_nj; ++j) {
             for (auto i = data_ibegin; i != data_ibegin + data_ni; ++i, ++lit) {
                 if (inRange(i, 0, ni) && inRange(j, 0, nj)) {
                     auto gidx = offset + (jbegin + j) * ni_global + (ibegin + i);
+                    ASSERT(glIndices.find(gidx) == std::end(glIndices));
+                    glIndices.insert(gidx);
                     *(git + gidx) = *lit;
                 }
             }
         }
     }
-
+    ++domainCount;
+    if(domainCount == local.metadata().getLong("domainCount")) {
+        std::ostringstream oss;
+        oss << "Number of inserted unique indices: " << glIndices.size() << " (expected "
+            << local.globalSize() << ")";
+        eckit::Log::info() << oss.str() << std::endl;
+        // ASSERT_MSG(glIndices.size() == local.globalSize(), oss.str());
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------

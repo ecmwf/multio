295cd21d6912b337a13998744823adc65293cdeb
Domokos Sármány <domokos.sarmany@ecmwf.int>
2022-04-22 16:29:46 +0100

    Clean up MultioClient

--- src/multio/server/MultioClient.cc
+++ src/multio/server/MultioClient.cc
@@ -10,9 +10,8 @@
 
 #include "multio/LibMultio.h"
 #include "multio/message/Message.h"
-#include "multio/server/MpiTransport.h"
-#include "multio/server/TcpTransport.h"
 
+using multio::message::Message;
 using multio::message::Peer;
 
 namespace multio {
@@ -22,16 +21,7 @@ size_t serverIdDenom(size_t clientCount, size_t serverCount) {
     return (serverCount == 0) ? 1 : (((clientCount - 1) / serverCount) + 1);
 }
 
-MultioClient::MultioClient(const eckit::Configuration& config)
-//    :
-//    transport_(TransportFactory::instance().build(config.getString("transport"), config)),
-//    client_{transport_->localPeer()},
-//    serverId_{client_.id() / serverIdDenom(clientCount_, serverCount_)},
-//    usedServerCount_{eckit::Resource<size_t>("multioMpiPoolSize;$MULTIO_USED_SERVERS", 1)},
-//    serverPeers_{transport_->createServerPeers()},
-//    counters_(serverPeers_.size()),
-//    distType_{distributionType()}
-{
+MultioClient::MultioClient(const eckit::Configuration& config) {
     LOG_DEBUG_LIB(multio::LibMultio) << "Client config: " << config << std::endl;
     const std::vector<eckit::LocalConfiguration> plans =
         config.getSubConfiguration("client").getSubConfigurations("plans");
@@ -44,8 +34,6 @@ MultioClient::MultioClient(const eckit::Configuration& config)
 MultioClient::~MultioClient() = default;
 
 void MultioClient::openConnections() const {
-    // transport_->openConnections();
-
     Message msg{Message::Header{Message::Tag::Open, Peer{}, Peer{}}};
     for (const auto& plan : plans_) {
         plan->process(msg);
@@ -53,52 +41,11 @@ void MultioClient::openConnections() const {
 }
 
 void MultioClient::closeConnections() const {
-    // transport_->closeConnections();
-
     Message msg{Message::Header{Message::Tag::Close, Peer{}, Peer{}}};
     for (const auto& plan : plans_) {
         plan->process(msg);
     }
 }
-
-//void MultioClient::sendDomain(message::Metadata metadata, eckit::Buffer&& domain) {
-//    for (auto& server : serverPeers_) {
-//        Message msg{Message::Header{Message::Tag::Domain, client_, *server, std::move(metadata)},
-//                    domain};
-
-//        transport_->bufferedSend(msg);
-//    }
-//}
-
-//void MultioClient::sendMask(message::Metadata metadata, eckit::Buffer&& mask) {
-//    for (auto& server : serverPeers_) {
-//        Message msg{Message::Header{Message::Tag::Mask, client_, *server, std::move(metadata)},
-//                    mask};
-
-//        transport_->bufferedSend(msg);
-//    }
-//}
-
-//void MultioClient::sendField(message::Metadata metadata, eckit::Buffer&& field,
-//                             bool to_all_servers) {
-//    if (to_all_servers) {
-//        for (auto& server : serverPeers_) {
-//            Message msg{Message::Header{Message::Tag::Field, client_, *server, std::move(metadata)},
-//                        field};
-
-//            transport_->bufferedSend(msg);
-//        }
-//    }
-//    else {
-//        auto server = chooseServer(metadata);
-
-//        Message msg{
-//            Message::Header{Message::Tag::Field, client_, server, std::move(metadata)},
-//            std::move(field)};
-
-//        transport_->bufferedSend(msg);
-//    }
-//}
 
 void MultioClient::dispatch(message::Metadata metadata, eckit::Buffer&& payload, int itag) {
     auto tag = static_cast<Message::Tag>(itag);
@@ -116,79 +63,6 @@ void MultioClient::dispatch(message::Message msg) {
         plan->process(msg);
     }
 }
-
-//void MultioClient::sendStepComplete() const {
-//    for (auto& server : serverPeers_) {
-//        Message msg{Message::Header{Message::Tag::StepComplete, client_, *server}};
-//        transport_->bufferedSend(msg);
-//    }
-//}
-
-//message::Peer MultioClient::chooseServer(const message::Metadata& metadata) {
-//    ASSERT_MSG(serverCount_ > 0, "No server to choose from");
-
-//    switch (distType_) {
-//        case DistributionType::hashed_cyclic: {
-//            std::ostringstream os;
-//            os << metadata.getString("category") << metadata.getString("nemoParam")
-//               << metadata.getString("param") << metadata.getLong("level");
-
-//            ASSERT(usedServerCount_ <= serverCount_);
-
-//            auto offset = std::hash<std::string>{}(os.str()) % usedServerCount_;
-//            auto id = (serverId_ + offset) % serverCount_;
-
-//            ASSERT(id < serverPeers_.size());
-
-//            return *serverPeers_[id];
-//        }
-//        case DistributionType::hashed_to_single: {
-//            std::ostringstream os;
-//            os << metadata.getString("category") << metadata.getString("nemoParam")
-//               << metadata.getString("param") << metadata.getLong("level");
-
-//            auto id = std::hash<std::string>{}(os.str()) % serverCount_;
-
-//            ASSERT(id < serverPeers_.size());
-
-//            return *serverPeers_[id];
-//        }
-//        case DistributionType::even: {
-//            std::ostringstream os;
-//            os << metadata.getString("category") << metadata.getString("nemoParam")
-//               << metadata.getString("param") << metadata.getLong("level");
-
-//            if (destinations_.find(os.str()) != end(destinations_)) {
-//                return destinations_.at(os.str());
-//            }
-
-//            auto it = std::min_element(begin(counters_), end(counters_));
-//            auto id = static_cast<size_t>(std::distance(std::begin(counters_), it));
-
-//            ASSERT(id < serverPeers_.size());
-//            ASSERT(id < counters_.size());
-
-//            ++counters_[id];
-
-//            auto dest = *serverPeers_[id];
-//            destinations_[os.str()] = *serverPeers_[id];
-
-//            return dest;
-//        }
-//        default:
-//            throw eckit::SeriousBug("Unhandled distribution type");
-//    }
-//}
-
-//MultioClient::DistributionType MultioClient::distributionType() {
-//    const std::map<std::string, enum DistributionType> str2dist = {
-//        {"hashed_cyclic", DistributionType::hashed_cyclic},
-//        {"hashed_to_single", DistributionType::hashed_to_single},
-//        {"even", DistributionType::even}};
-
-//    auto key = std::getenv("MULTIO_SERVER_DISTRIBUTION");
-//    return key ? str2dist.at(key) : DistributionType::hashed_to_single;
-//}
 
 }  // namespace server
 }  // namespace multio
--- src/multio/server/MultioClient.h
+++ src/multio/server/MultioClient.h
@@ -16,18 +16,18 @@
 #include <map>
 
 #include "multio/action/Plan.h"
-#include "multio/message/Metadata.h"
-#include "multio/message/Peer.h"
 
 namespace eckit {
 class Buffer;
 class Configuration;
+class LocalConfiguration;
 }  // namespace eckit
 
 namespace multio {
 
 namespace message {
 class Message;
+using Metadata = eckit::LocalConfiguration;
 }
 
 namespace server {
@@ -58,34 +58,6 @@ public:
 
 private:
     std::vector<std::unique_ptr<action::Plan>> plans_;
-
-//    using PeerList = std::vector<std::unique_ptr<message::Peer>>;
-
-//    size_t clientCount_ = 5;
-//    size_t serverCount_ = 3;
-
-//    std::shared_ptr<Transport> transport_ = nullptr;
-
-//    const message::Peer client_;
-
-//    size_t serverId_;
-//    size_t usedServerCount_;
-//    PeerList serverPeers_;
-
-//    // Distribute fields
-//    message::Peer chooseServer(const message::Metadata& metadata);
-//    std::map<std::string, message::Peer> destinations_;
-//    std::vector<uint64_t> counters_;
-
-//    enum class DistributionType : unsigned
-//    {
-//        hashed_cyclic,
-//        hashed_to_single,
-//        even,
-//    };
-//    DistributionType distType_;
-
-//    enum DistributionType distributionType();
 };
 
 }  // namespace server

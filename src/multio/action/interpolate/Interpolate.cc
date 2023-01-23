8a8146a01788ef6002921e7ebe4ebef66cf81998
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-01-23 09:18:14 +0000

    MUL-70: Strip templates and comments

--- src/multio/action/interpolate/Interpolate.cc
+++ src/multio/action/interpolate/Interpolate.cc
@@ -3,15 +3,17 @@
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
  * In applying this licence, ECMWF does not waive the privileges and immunities
- * granted to it by virtue of its status as an intergovernmental organisation
- * nor does it submit to any jurisdiction.
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
  */
 
 
 #include "multio/action/interpolate/Interpolate.h"
 
 #include <fstream>
+#include <type_traits>
 #include <vector>
 
 #include "eckit/exception/Exceptions.h"
@@ -25,9 +27,7 @@
 #include "multio/util/PrecisionTag.h"
 
 
-namespace multio {
-namespace action {
-namespace interpolate {
+namespace multio::action::interpolate {
 
 
 template <typename DestinationType>
@@ -92,186 +92,75 @@ void fill(const eckit::LocalConfiguration& sub, DestinationType& destination) {
 };
 
 
-template <>
-message::Message Interpolate::InterpolateRawMessage<float>(message::Message&& msg) const {
-    // Print Input metadata
-    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: Metadata of the input message :: " << std::endl
-                             << msg.metadata() << std::endl
-                             << std::endl;
-
-    // Prepare input data for the interpolator (value of the input field)
-    const float* data = reinterpret_cast<const float*>(msg.payload().data());
-    const size_t size = msg.payload().size() / sizeof(float);
-
-    // Prepare a double precision buffer
-    std::vector<double> inData(size, 0.0);
-
-    // Recast the fata from single precision input data to double precion buffer
-    // needed to mir
-    for (int i = 0; i < size; ++i) {
-        inData[i] = double(data[i]);
-    }
-
-    // Prepare input parameters (description of the input field)
-    mir::param::SimpleParametrisation inputPar;
-    fill(configurationContext_.getSubConfiguration("inputConfiguration"), inputPar);
-
-    mir::input::RawInput input(inData.data(), size, inputPar);
-
-    // Prepare interpolation Job (configuration of the interpolation task)
-    mir::api::MIRJob job;
-    fill(configurationContext_.getSubConfiguration("jobConfiguration"), job);
-
-    // Add missing values support
-    if (msg.metadata().has("missingValue")) {
-        job.set("missing_value", msg.metadata().getDouble("missingValue"));
-    }
-
-    // Show mir input configuration
-    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: mir input field description :: " << std::endl
-                             << inputPar << std::endl
-                             << std::endl;
-
-    // Show mir job configuration
-    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: nir job description " << std::endl << job << std::endl << std::endl;
-
-    // Allocate and initialize the out values
-    std::vector<double> outDataDouble(outputSize_, 0.0);
-
-    // Construction of the output metadata (mir just forward the job configuration
-    // in this object)
-    mir::param::SimpleParametrisation outMetadata;
-
-    // Preparation of the output object
-    mir::output::RawOutput output(outDataDouble.data(), outDataDouble.size(), outMetadata);
-
-    // Execute the job
-    job.execute(input, output);
-
-    // Allocation of the single precision output vector
-    std::vector<float> outData(outputSize_, 0.0);
-
-    // Recast the fata from double precision mit output data to single precion
-    // buffer needed to output a single precision message
-    for (int i = 0; i < outDataDouble.size(); ++i) {
-        outData[i] = float(outDataDouble[i]);
-    }
-
-    // Construction of the output message
-    message::Metadata md{};
-
-    // By default the only metadata to in the outptu message is the global size
-    md.set("globalSize", outData.size());
-    // Needed by the Grib encoder always needs to be forwarded
-    // md.set("name", msg.name());
-    // md.set("category", msg.category());
-    // md.set("domain", msg.domain());
-    md.set("precision", "single");
-
-    // Fill the output metdata from configuration file
-    fill(configurationContext_.getSubConfiguration("outputConfiguration"), md);
-
-    // construction of the buffer
-    eckit::Buffer buffer(reinterpret_cast<const char*>(outData.data()), outputSize_ * sizeof(float));
-
-    // Show metadata of the output message
-    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: Metadata of the output message :: " << std::endl
-                             << md << std::endl
-                             << std::endl;
-
-    // Next action
-    return {message::Message::Header{message::Message::Tag::Field, msg.source(), msg.destination(), std::move(md)},
-            std::move(buffer)};
+message::Message Interpolate::InterpolateInSinglePrecision(message::Message&& msg) const {
+    NOTIMP;
 }
 
 
-template <>
-message::Message Interpolate::InterpolateRawMessage<double>(message::Message&& msg) const {
-    // Print Input metadata
+message::Message Interpolate::InterpolateInDoublePrecision(message::Message&& msg) const {
     LOG_DEBUG_LIB(LibMultio) << "Interpolate :: Metadata of the input message :: " << std::endl
                              << msg.metadata() << std::endl
                              << std::endl;
 
-    // Prepare input data for the interpolator (value of the input field)
+    const auto& config = Action::confCtx_.config();
+    const auto outputSize = config.getLong("outputSize");
+
     const double* data = reinterpret_cast<const double*>(msg.payload().data());
     const size_t size = msg.payload().size() / sizeof(double);
 
-    // Prepare input parameters (description of the input field)
     mir::param::SimpleParametrisation inputPar;
-    fill(configurationContext_.getSubConfiguration("inputConfiguration"), inputPar);
+    fill(config.getSubConfiguration("inputConfiguration"), inputPar);
 
     mir::input::RawInput input(data, size, inputPar);
 
-    // Prepare interpolation Job (configuration of the interpolation task)
     mir::api::MIRJob job;
-    fill(configurationContext_.getSubConfiguration("jobConfiguration"), job);
+    fill(config.getSubConfiguration("jobConfiguration"), job);
 
-    // Add missing values support
     if (msg.metadata().has("missingValue")) {
         job.set("missing_value", msg.metadata().getDouble("missingValue"));
     }
 
-    // Show mir input configuration
-    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: mir input field description :: " << std::endl
-                             << inputPar << std::endl
-                             << std::endl;
+    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: input :: " << std::endl << inputPar << std::endl << std::endl;
 
-    // Show mir job configuration
-    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: nir job description " << std::endl << job << std::endl << std::endl;
+    LOG_DEBUG_LIB(LibMultio) << "Interpolate :: job " << std::endl << job << std::endl << std::endl;
 
-    // Allocate and initialize the out values
-    std::vector<double> outData(outputSize_, 0.0);
+    std::vector<double> outData(outputSize, 0.0);
 
-    // Construction of the output metadata (mir just forward the job configuration
-    // in this object)
     mir::param::SimpleParametrisation outMetadata;
 
-    // Preparation of the output object
     mir::output::RawOutput output(outData.data(), outData.size(), outMetadata);
 
-    // Execute the job
     job.execute(input, output);
 
-    // Construction of the output message
-    message::Metadata md{};
-
-    // By default the only metadata to in the outptu message is the global size
+    message::Metadata md;
     md.set("globalSize", outData.size());
-    // Needed by the Grib encoder always needs to be forwarded
-    // md.set("name", msg.name());
-    // md.set("category", msg.category());
-    // md.set("domain", msg.domain());
     md.set("precision", "double");
 
-    // Fill the output metdata from configuration file
-    fill(configurationContext_.getSubConfiguration("outputConfiguration"), md);
+    fill(config.getSubConfiguration("outputConfiguration"), md);
 
-    // construction of the buffer
-    eckit::Buffer buffer(reinterpret_cast<const char*>(outData.data()), outputSize_ * sizeof(double));
+    eckit::Buffer buffer(reinterpret_cast<const char*>(outData.data()), outputSize * sizeof(double));
 
-    // Show metadata of the output message
     LOG_DEBUG_LIB(LibMultio) << "Interpolate :: Metadata of the output message :: " << std::endl
                              << md << std::endl
                              << std::endl;
 
-    // Next action
     return {message::Message::Header{message::Message::Tag::Field, msg.source(), msg.destination(), std::move(md)},
             std::move(buffer)};
 }
-
-
-Interpolate::Interpolate(const ConfigurationContext& confCtx) :
-    ChainedAction(confCtx),
-    configurationContext_(confCtx_.config()),
-    outputSize_(configurationContext_.getLong("outputSize")) {}
 
 
 void Interpolate::executeImpl(message::Message msg) const {
     switch (msg.tag()) {
         case (message::Message::Tag::Field): {
-            executeNext(util::dispatchPrecisionTag(msg.precision(), [&](auto pt) {
+            executeNext(util::dispatchPrecisionTag(msg.precision(), [&](auto pt) -> message::Message {
                 using PT = typename decltype(pt)::type;
-                return InterpolateRawMessage<PT>(std::move(msg));
+                if (std::is_same<double, PT>::value) {
+                    return InterpolateInDoublePrecision(std::move(msg));
+                }
+                else if (std::is_same<float, PT>::value) {
+                    return InterpolateInSinglePrecision(std::move(msg));
+                }
+                NOTIMP;
             }));
             break;
         };
@@ -280,8 +169,7 @@ void Interpolate::executeImpl(message::Message msg) const {
             break;
         }
         default: {
-            throw eckit::BadValue("Action::Interpolate :: Unsupported message tag",
-                                  eckit::CodeLocation(__FILE__, __LINE__, __FUNCTION__));
+            throw eckit::BadValue("Action::Interpolate :: Unsupported message tag", Here());
             break;
         }
     };
@@ -296,6 +184,4 @@ void Interpolate::print(std::ostream& os) const {
 static ActionBuilder<Interpolate> InterpolateBuilder("interpolate");
 
 
-}  // namespace interpolate
-}  // namespace action
-}  // namespace multio
+}  // namespace multio::action::interpolate
--- src/multio/action/interpolate/Interpolate.h
+++ src/multio/action/interpolate/Interpolate.h
@@ -3,25 +3,10 @@
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
  * In applying this licence, ECMWF does not waive the privileges and immunities
- * granted to it by virtue of its status as an intergovernmental organisation
- * nor does it submit to any jurisdiction.
- */
-
-/**
- * \file This file is used to define a special action needed to interpolate
- * fields.
- *
- *
- * @see Action.h
- * @see ChainedAction.h
- *
- * @author Mirco Valentini
- * @author Domokos Sarmany
- * @author Simon Smart
- * @author Tiago Quintino
- *
- * @date Nov 2022
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
  */
 
 
@@ -30,67 +15,22 @@
 #include "multio/action/ChainedAction.h"
 
 
-namespace multio {
-namespace action {
-namespace interpolate {
+namespace multio::action::interpolate {
 
 
 /**
- * \class Object used to wrap the MIR interpolation tool in order to enable
- *        interpolation between different grids in multio pipelines
- *
- * \todo  rework mir raw IO objects to allow cleaner code
+ * \class MultIO Action for interpolation/regridding
  */
 class Interpolate final : public ChainedAction {
-private:
-    /**
-     * \brief Configuration for the interpolation object
-     */
-    const eckit::LocalConfiguration configurationContext_;
-
-    int outputSize_;
-
-    /**
-     * \brief function used to perform an interpolation when
-     * the message is raw; the template is used to perform the interpolation in sigle/double precision
-     *
-     * \param [in] msg input message
-     * \param [in] mainConfiguration all the parameters/configuration needed to
-     * perform a specific interpolation
-     *
-     * \see executeImpl
-     * \see InterpolateRawMessageFloat
-     */
-    template <typename T>
-    message::Message InterpolateRawMessage(message::Message&& msg) const;
-
-    /**
-     * \brief helper used to just log useful informations
-     *
-     * \param [inout] os output stream where to log infortmations
-     */
-    void print(std::ostream& os) const override;
-
 public:
-    /**
-     * \brief constructor for the interpolation action
-     *
-     * \param [in] confCtx configuration parameters for the action
-     */
-    explicit Interpolate(const ConfigurationContext& confCtx);
+    using ChainedAction::ChainedAction;
 
-    /**
-     * \brief method used as high level wrapper for the interpolation library
-     *
-     * \param [in] msg perform the interpolation action
-     *
-     * \see InterpolateRawMessage
-     *
-     */
-    void executeImpl(message::Message msg) const override;
+private:
+    message::Message InterpolateInSinglePrecision(message::Message&&) const;
+    message::Message InterpolateInDoublePrecision(message::Message&&) const;
+    void print(std::ostream&) const override;
+    void executeImpl(message::Message) const override;
 };
 
 
-}  // namespace interpolate
-}  // namespace action
-}  // namespace multio
+}  // namespace multio::action::interpolate

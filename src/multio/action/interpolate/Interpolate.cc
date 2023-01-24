fcc8cce5238f914764373672a33c25b05442f23d
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-01-24 09:40:55 +0000

    MUL-70: Interpolation post-processing keywords from ~metkit/share/metkit/language.yaml

--- src/multio/action/interpolate/Interpolate.cc
+++ src/multio/action/interpolate/Interpolate.cc
@@ -12,16 +12,23 @@
 
 #include "multio/action/interpolate/Interpolate.h"
 
+#include <algorithm>
 #include <fstream>
+#include <regex>
+#include <string>
 #include <type_traits>
 #include <vector>
 
 #include "eckit/exception/Exceptions.h"
+#include "eckit/parser/YAMLParser.h"
+
+#include "metkit/mars/MarsLanguage.h"
 
 #include "mir/api/MIRJob.h"
 #include "mir/input/RawInput.h"
-#include "mir/output/RawOutput.h"
+#include "mir/output/ResizableOutput.h"
 #include "mir/param/SimpleParametrisation.h"
+#include "mir/repres/gauss/reduced/Reduced.h"
 
 #include "multio/LibMultio.h"
 #include "multio/util/PrecisionTag.h"
@@ -30,64 +37,81 @@
 namespace multio::action::interpolate {
 
 
-void fill(const eckit::LocalConfiguration& sub, mir::param::SimpleParametrisation& destination) {
-    if (sub.has("loadFromFile")) {
-        auto cfg = sub.getSubConfiguration("loadFromFile");
+void fill_input(const eckit::LocalConfiguration& cfg, mir::param::SimpleParametrisation& param) {
+    ASSERT(cfg.has("input"));
+    const auto input = cfg.getString("input");
 
-        for (const auto& key : cfg.keys()) {
-            std::fstream inputFile;
-            inputFile.open(cfg.getString(key));
-            ASSERT(inputFile.is_open());
+    static const std::regex sh("(T|TCO|TL)([1-9][0-9]*)");
+    static const std::regex gg("([FNO])([1-9][0-9]*)");
 
-            std::vector<long> val;
-            std::string str;
-            while (getline(inputFile, str)) {
-                val.push_back(long(atoi(str.c_str())));
-            }
-            destination.set(key, val);
+    std::smatch match;
+    if (std::regex_match(input, match, sh)) {
+        param.set("spectral", true);
+        param.set("truncation", std::stol(match[2].str()));
+        return;
+    }
+
+    if (std::regex_match(input, match, gg)) {
+        param.set("gridded", true);
+        param.set("gridType", match[1].str() == "F" ? "regular_gg" : "reduced_gg");
+        param.set("N", std::stol(match[2].str()));
+        param.set("north", 90.);
+        param.set("west", 0.);
+        param.set("south", -90.);
+        param.set("east", 360.);
+
+        if (match[1].str() != "F") {
+            param.set("pl", mir::repres::gauss::reduced::Reduced::pls(match[0].str()));
         }
-    };
+        return;
+    }
 
-    if (sub.has("setByValue")) {
-        auto cfg = sub.getSubConfiguration("setByValue");
+    NOTIMP;
+}
 
-        for (const auto& key : cfg.keys()) {
-            eckit::Value cfgVal = cfg.getSubConfiguration(key).get();
-            if (cfgVal.isMap()) {
-                throw eckit::NotImplemented("Action::Interpolate :: Nested forwarding is not supported", Here());
-            }
 
-            if (cfgVal.isList()) {
-                if (cfgVal.head().isDouble()) {
-                    destination.set(key, cfg.getDoubleVector(key));
-                }
-                else if (cfgVal.head().isNumber()) {
-                    destination.set(key, cfg.getLongVector(key));
-                }
-                else if (cfgVal.head().isString()) {
-                    destination.set(key, cfg.getStringVector(key));
-                }
-                else {
-                    throw eckit::NotImplemented("Action::Interpolate :: Unsupported datatype", Here());
-                };
-            }
-            else if (cfgVal.isBool()) {
-                destination.set(key, cfg.getBool(key));
-            }
-            else if (cfgVal.isDouble()) {
-                destination.set(key, cfg.getDouble(key));
-            }
-            else if (cfgVal.isNumber()) {
-                destination.set(key, cfg.getInt(key));
-            }
-            else if (cfgVal.isString()) {
-                destination.set(key, cfg.getString(key).c_str());
-            }
-            else {
-                throw eckit::NotImplemented("Action::Interpolate :: Unsupported datatype", Here());
+void fill_job(const eckit::LocalConfiguration& cfg, mir::param::SimpleParametrisation& destination) {
+    static const struct PostProcKeys : std::vector<std::string> {
+        PostProcKeys() {
+            const auto yaml = eckit::YAMLParser::decodeFile(metkit::mars::MarsLanguage::languageYamlFile());
+            for (const auto& key : yaml["_postproc"].keys().as<eckit::ValueList>()) {
+                emplace_back(key.as<std::string>());
             }
         }
+        bool contains(const std::string& key) const { return std::find(begin(), end(), key) != end(); }
+    } postproc;
+
+    ASSERT(not postproc.contains("input"));
+    ASSERT(not postproc.contains("options"));
+
+    auto set = [&destination, &cfg](const std::string& key, const eckit::Value& value) {
+        if (value.isList()) {
+            value.head().isDouble()   ? destination.set(key, cfg.getDoubleVector(key))
+            : value.head().isNumber() ? destination.set(key, cfg.getLongVector(key))
+            : value.head().isString() ? destination.set(key, cfg.getStringVector(key))
+                                      : NOTIMP;
+            return;
+        }
+
+        value.isBool()     ? destination.set(key, cfg.getBool(key))
+        : value.isDouble() ? destination.set(key, cfg.getDouble(key))
+        : value.isNumber() ? destination.set(key, cfg.getInt(key))
+        : value.isString() ? destination.set(key, cfg.getString(key).c_str())
+                           : NOTIMP;
     };
+
+    for (const auto& key : postproc) {
+        if (cfg.has(key)) {
+            set(key, cfg.getSubConfiguration(key).get());
+        }
+    }
+
+    if (cfg.has("options")) {
+        const auto& options = cfg.getSubConfiguration("options");
+        for (const auto& key : options.keys()) {
+            set(key, cfg.getSubConfiguration(key).get());
+        }
+    }
 };
 
 
@@ -128,12 +152,12 @@ message::Message Interpolate::InterpolateInDoublePrecision(message::Message&& ms
     const size_t size = msg.payload().size() / sizeof(double);
 
     mir::param::SimpleParametrisation inputPar;
-    fill(config.getSubConfiguration("inputConfiguration"), inputPar);
+    fill_input(config, inputPar);
 
     mir::input::RawInput input(data, size, inputPar);
 
     mir::api::MIRJob job;
-    fill(config.getSubConfiguration("jobConfiguration"), job);
+    fill_job(config, job);
 
     if (msg.metadata().has("missingValue")) {
         job.set("missing_value", msg.metadata().getDouble("missingValue"));

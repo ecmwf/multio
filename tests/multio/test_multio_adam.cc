91cbcd0c9b39b9148a4a4f08d4c5b00479269089
Adam Warde <adam.warde@ecmwf.int>
2023-03-23 13:20:18 +0000

    Refactored tests to remove EXPECTS in tests, instead fuction test_check() now is used. Re factored all tests to use type traits to deleet objects at end of test and use eckit log for any printing

--- tests/multio/test_multio_adam.cc
+++ tests/multio/test_multio_adam.cc
@@ -31,110 +31,37 @@ using multio::util::configuration_path_name;
 
 namespace std {
 template <> struct default_delete<multio_metadata_t> {
-    void operator() (multio_metadata_t* m) { 
-        EXPECT(multio_delete_metadata(m) == MULTIO_SUCCESS);
+    void operator() (multio_metadata_t* md) { 
+        EXPECT(multio_delete_metadata(md) == MULTIO_SUCCESS);
+        eckit::Log::error() << "Metadata Object Deleted" << std::endl;
     }
 };
 
 template <> struct default_delete<multio_handle_t> {
-    void operator() (multio_handle_t* m) { 
-        EXPECT(multio_delete_handle(m) == MULTIO_SUCCESS);
+    void operator() (multio_handle_t* mio) { 
+        EXPECT(multio_delete_handle(mio) == MULTIO_SUCCESS);
+        eckit::Log::error() << "Handle Object Deleted" << std::endl;
+    }
+};
+
+template <> struct default_delete<multio_configurationcontext_t> {
+    void operator() (multio_configurationcontext_t* cc) { 
+        EXPECT(multio_delete_configurationcontext(cc) == MULTIO_SUCCESS);
+        eckit::Log::error() << "Configuration Context Object Deleted" << std::endl;
     }
 };
 }
 
-/***
-
-
-namespace std {
-
-
-    template <typename T>
-    struct default_delete {
-        void operator() (const T* p) { delete p; }
+void test_check(int rc, const char* doc){    
+    if(rc != MULTIO_SUCCESS){
+        eckit::Log::error() << "Failed to " << doc << std::endl;
+        EXPECT(rc == MULTIO_SUCCESS);    
     }
-
-
-    template <typename T>
-    struct unique_ptr {
-        unique_ptr(T* p) : ptr_(p) {}
-        unique_ptr(unique_ptr<T>&& rhs) { ... }
-        ~unique_ptr() {
-            default_delete<T>{}(p);
-        }
-        const T& operator*() const { return *ptr_; }
-        T& operator*() { return *ptr_; }
-    private:
-        T* ptr_;
-    };
-
-
-    template <>
-    struct default_delete<multio_metadata_t> {
-        void operator() (const multio_metadata_t* p) { EXPECT(multio_metadata_delete(p) == MULTIO_SUCCESS); }
-    };
-}
-
-
-multio_metadata_t* md;
-EXPECT(multio_new_metadata(&md) == MULTIO_SUCCESS);
-unique_ptr<multio_metdata_t> del(md);
-...
-
-
-
-int abcd = 1234;
-std::vector<int> v {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-
-eckit::Log::info() << "My value: " << abcd << std::endl;
-
-template <typename T>
-struct eckit_vector_print_condense {
-    static bool condense = false;
-}
-
-
-template <> struct eckit_vector_print_condense<int> {
-    static bool condense = true;
-}
-
-
-
-std::ostream& operator<<(std::ostream& s, const std::vector<T>& v) {
-
-    if (eckit_vector_print_condense<T>:±condense) {}
-    [1, 2, ..., 10]
-    ...
-    return s;
-}
-
-
-
-
-
-{
-    {
-        // std::unique_ptr<MY_TYPE> p{new MY_TYPE(a, b, c)};
-        MY_TYPE* p = new MY_TYPE(a, b, c)
-        
-        try {
-            ...
-            ...
-            throw Exception();
-            ...
-            ...
-        } catch (...) {
-            delete p;
-            throw;
-        }
-
-        delete p;
+    else{
+        eckit::Log::error() << doc << std::endl;
+        EXPECT(rc == MULTIO_SUCCESS);
     }
 }
-
-***/
-
 
 
 class MultioReplayExampleCApi final : public multio::MultioTool {
@@ -204,176 +131,110 @@ namespace test{
 
 CASE("Initial Test for version") {
     const char *version = nullptr;
-    EXPECT(multio_version(&version) == MULTIO_SUCCESS);
+    test_check(multio_version(&version), "Version returned");
     EXPECT(std::strcmp(version, multio_version_str())==0);
 }
 
 CASE("Test Multio Initialisation") {
-    EXPECT(multio_initialise() == MULTIO_SUCCESS);
+    test_check(multio_initialise(), "Initialise Multio");
     eckit::Main::instance(); // throws if not initialised
 }
 
-CASE("Test loading config") {
+CASE("Test loading configuration") {
     multio_handle_t* multio_handle = nullptr;
+    
     multio_configurationcontext_t* multio_cc = nullptr;
-    int rc;
 
-    rc = multio_new_configurationcontext(&multio_cc);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Config created from environment path" << std::endl;
-    }
-    else{
-        std::cout << "Config NOT created from environment path" << std::endl;
-    }
+    //int rc;
+
+    test_check(multio_new_configurationcontext(&multio_cc), "Config Created from Environment Path");
+    std::unique_ptr<multio_configurationcontext_t> configuration_context_deleter(multio_cc);
 
     const char *conf_path = "/Users/maaw/multio/tests/multio/";
-    //std::cout << conf_path << std::endl;
 
-    rc = multio_conf_set_path(multio_cc, conf_path);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Config Path set" << std::endl;
-    }
-    else{
-        std::cout << "Config Path NOT set" << std::endl;
-    }
+    test_check(multio_conf_set_path(multio_cc, conf_path), "Configuration Path Changed");
 
     auto configPath = configuration_path_name();
     auto configFile = configuration_file_name();
-    std::cout << "Configuration Path: " << configPath.asString().c_str() << std::endl;
-    std::cout << "Configuration File: " << configFile.asString().c_str() << std::endl;
-    rc = multio_new_configurationcontext_from_filename(&multio_cc, configFile.asString().c_str());
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Config created from Filename" << std::endl;
-    }
-    else{
-        std::cout << "Config NOT created from filename" << std::endl;
-    }
+    eckit::Log::error() << "Configuration Path: " << configPath.asString().c_str() << std::endl;
+    eckit::Log::error() << "Configuration File: " << configFile.asString().c_str() << std::endl;
 
-    rc = multio_new_handle(&multio_handle, multio_cc);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Config handle" << std::endl;
-    }
-    else{
-        std::cout << "Config NOT handle" << std::endl;
-    }
+    test_check(multio_new_configurationcontext_from_filename(&multio_cc, configFile.asString().c_str()), "Configuration Context Created From Filename");
 
-    rc = multio_start_server(multio_cc);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Server started" << std::endl;
-    }
-    else{
-        std::cout << "Server NOT started" << std::endl;
-    }
+    test_check(multio_new_handle(&multio_handle, multio_cc), "Create Handle");
+    std::unique_ptr<multio_handle_t> handle_deleter(multio_handle);
 
-    rc = multio_open_connections(multio_handle);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Connections opened" << std::endl;
-    }
-    else{
-        std::cout << "Connections NOT opened" << std::endl;
-    }
+    //test_check(multio_start_server(multio_cc), "Start Server");
 
-    rc = multio_close_connections(multio_handle);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Connections closed" << std::endl;
-    }
-    else{
-        std::cout << "Connections NOT closed" << std::endl;
-    }
+    test_check(multio_open_connections(multio_handle), "Open Connections");
 
-    rc = multio_delete_configurationcontext(multio_cc);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Config deleted" << std::endl;
-    }
-    else{
-        std::cout << "Config NOT deleted" << std::endl;
-    }
+    test_check(multio_close_connections(multio_handle), "Close Connections");
 
-    rc = multio_delete_handle(multio_handle);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Handle deleted" << std::endl;
-    }
-    else{
-        std::cout << "Handle NOT deleted" << std::endl;
-    }
-    
-    EXPECT(rc==MULTIO_SUCCESS);
+    //Dont know if these needed as objects already auto deleted using type traits
+    //test_check(multio_delete_configurationcontext(multio_cc), "Delete Configuration object");
+
+    //test_check(multio_delete_handle(multio_handle), "Delete Handle");   
 }
 
 
 CASE("Test creating metadata"){
     int rc;
     multio_metadata_t* md = nullptr;
-    EXPECT(multio_new_metadata(&md) == MULTIO_SUCCESS);
-    EXPECT(md != nullptr);
 
+    test_check(multio_new_metadata(&md), "Create New Metadata");
     std::unique_ptr<multio_metadata_t> multio_deleter(md);
-    
+  
     const char* key = "test_int";
     int value=1;
 
-    multio_metadata_set_int(md, key, value);
+    test_check(multio_metadata_set_int(md, key, value), "Set Int");
 
     key = "test_long";
     long long_value = 1;
-
-    //EXPECT(...);
-    
-    multio_metadata_set_long(md, key, long_value);
+ 
+    test_check(multio_metadata_set_long(md, key, long_value), "Set Long");
     
     key = "test_long_long";
     long long ll_value = 1;
 
-    multio_metadata_set_longlong(md, key, ll_value);
+    test_check(multio_metadata_set_longlong(md, key, ll_value), "Set Long Long");
 
     key = "test_string";
     const char * s_value = "test_val";
 
-    multio_metadata_set_string(md, key, s_value);   
- 
+    test_check(multio_metadata_set_string(md, key, s_value), "Set String");   
 }
 
 CASE("Test read from grib file"){
     const char* path = "/Users/maaw/multio/tests/multio/test.grib";
     auto field = eckit::PathName{path};
-    int rc;
 
-    std::cout << field << std::endl;
+    eckit::Log::error() << field << std::endl;
 
     eckit::FileHandle infile{field.fullName()};
     size_t bytes = infile.openForRead();
-
-    eckit::Buffer buffer(bytes);
-    infile.read(buffer.data(), bytes);
+    eckit::Length len = field.size();
+    eckit::Buffer buffer(len);
+    {
+        eckit::AutoClose closer(infile);
+        EXPECT(infile.read(buffer.data(), len) == len);
+    }
 
     infile.close();
    
     auto sz = static_cast<int>(buffer.size()) / sizeof(double);
     std::cout << "Size of Buffer: " << sz << std::endl;
-    
-    EXPECT(1==1);
-
 }
 
 CASE("Test write field"){
 
     multio_configurationcontext_t* multio_cc = nullptr;
-    int rc;
 
-    rc = multio_new_configurationcontext(&multio_cc);
-    const char *conf_path = "/Users/maaw/multio/tests/multio/";
-    std::cout << conf_path << std::endl;
-
-    rc = multio_conf_set_path(multio_cc, conf_path);
-    if(rc==MULTIO_SUCCESS){
-        std::cout << "Config Path set" << std::endl;
-    }
-    else{
-        std::cout << "Config Path NOT set" << std::endl;
-    }
+    test_check(multio_new_configurationcontext(&multio_cc), "Configuration Context Created");
+    std::unique_ptr<multio_configurationcontext_t> configuration_context_deleter(multio_cc);
 
     multio_handle_t* multio_handle = nullptr;
-    EXPECT(multio_new_handle(&multio_handle, multio_cc) == MULTIO_SUCCESS);
+    test_check(multio_new_handle(&multio_handle, multio_cc), "Create New handle");
     EXPECT(multio_handle);
     std::unique_ptr<multio_handle_t> handle_deleter(multio_handle);
 
@@ -392,27 +253,24 @@ CASE("Test write field"){
     std::cout << "Size of Buffer: " << sz << std::endl;
 
     multio_metadata_t* md = nullptr;
-    multio_new_metadata(&md);
-
+    test_check(multio_new_metadata(&md), "Create New Metadata Object");
     std::unique_ptr<multio_metadata_t> multio_deleter(md);
 
-    multio_metadata_set_string(md, "category", "test_data");
-    multio_metadata_set_int(md, "globalSize", sz);
-    multio_metadata_set_int(md, "level", 1);
-    multio_metadata_set_int(md, "step", 1);
+    test_check(multio_metadata_set_string(md, "category", "test_data"), "Set String");
+    test_check(multio_metadata_set_int(md, "globalSize", sz), "Set Int");
+    test_check(multio_metadata_set_int(md, "level", 1), "Set Int");
+    test_check(multio_metadata_set_int(md, "step", 1), "Set Int");
 
-    multio_metadata_set_double(md, "missingValue", 0.0);
-    multio_metadata_set_bool(md, "bitmapPresent", false);
-    multio_metadata_set_int(md, "bitsPerValue", 16);
+    test_check(multio_metadata_set_double(md, "missingValue", 0.0), "Set Doubel");
+    test_check(multio_metadata_set_bool(md, "bitmapPresent", false), "Set bool");
+    test_check(multio_metadata_set_int(md, "bitsPerValue", 16), "Set Int");
 
-    multio_metadata_set_bool(md, "toAllServers", false);
+    test_check(multio_metadata_set_bool(md, "toAllServers", false), "Set Bool");
 
     // Overwrite these fields in the existing metadata object
-    multio_metadata_set_string(md, "name", "test");
+    test_check(multio_metadata_set_string(md, "name", "test"), "Set String)");
 
-    rc = multio_write_field(multio_handle, md, reinterpret_cast<const double*>(buffer.data()), sz);
-    
-    EXPECT(rc==MULTIO_SUCCESS);
+    test_check(multio_write_field(multio_handle, md, reinterpret_cast<const double*>(buffer.data()), sz), "Write Field");
 
 }
 }

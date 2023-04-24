07f128706e7a1a79cd6d7886965154b0b75bba89
Adam Warde <adam.warde@ecmwf.int>
2023-04-24 13:47:13 +0200

    MUL-80: Move new tests from test_multio_example.cc to test_multio_capi.cc

--- tests/multio/test_multio_capi.cc
+++ tests/multio/test_multio_capi.cc
@@ -16,14 +16,52 @@
 #include <limits>
 
 #include "eckit/filesystem/TmpFile.h"
+#include "eckit/io/FileHandle.h"
 #include "eckit/testing/Test.h"
 
 #include "multio/api/multio_c.h"
 #include "multio/api/multio_c_cpp_utils.h"
 #include "multio/message/Metadata.h"
+#include "multio/multio_version.h"
 
 #include "TestDataContent.h"
 #include "TestHelpers.h"
+
+using multio::util::configuration_file_name;
+using multio::util::configuration_path_name;
+
+namespace std {
+template <>
+struct default_delete<multio_metadata_t> {
+    void operator()(multio_metadata_t* md) {
+        EXPECT(multio_delete_metadata(md) == MULTIO_SUCCESS);
+        eckit::Log::error() << "Metadata Object Deleted" << std::endl;
+    }
+};
+
+template <>
+struct default_delete<multio_handle_t> {
+    void operator()(multio_handle_t* mio) {
+        EXPECT(multio_delete_handle(mio) == MULTIO_SUCCESS);
+        eckit::Log::error() << "Handle Object Deleted" << std::endl;
+    }
+};
+
+template <>
+struct default_delete<multio_configurationcontext_t> {
+    void operator()(multio_configurationcontext_t* cc) {
+        EXPECT(multio_delete_configurationcontext(cc) == MULTIO_SUCCESS);
+        eckit::Log::error() << "Configuration Context Object Deleted" << std::endl;
+    }
+};
+}  // namespace std
+
+void test_check(int rc, const char* doc) {
+    if (rc != MULTIO_SUCCESS) {
+        eckit::Log::error() << "Failed to " << doc << std::endl;
+    }
+    EXPECT(rc == MULTIO_SUCCESS);
+}
 
 
 namespace multio {
@@ -41,6 +79,17 @@ typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type almost_
 }
 
 static std::string expectedMPIError("No communicator \"multio\" and no default given.");
+
+CASE("Test Multio Initialisation") {
+    test_check(multio_initialise(), "Initialise Multio");
+    eckit::Main::instance();  // throws if not initialised
+}
+
+CASE("Initial Test for version") {
+    const char* version = nullptr;
+    test_check(multio_version(&version), "Version returned");
+    EXPECT(std::strcmp(version, multio_version_str()) == 0);
+}
 
 CASE("Try Create handle with wrong configuration path") {
     multio_configuration_t* cc = nullptr;
@@ -136,6 +185,32 @@ CASE("Start server with default configuration") {
     EXPECT(err == MULTIO_ERROR_ECKIT_EXCEPTION);
     EXPECT(errStr.rfind(expectedMPIError) != std::string::npos);
     multio_delete_configuration(cc);
+}
+
+CASE("Test loading configuration") {
+
+    multio_configurationcontext_t* multio_cc = nullptr;
+
+    test_check(multio_new_configurationcontext(&multio_cc), "Config Created from Environment Path");
+    std::unique_ptr<multio_configurationcontext_t> configuration_context_deleter(multio_cc);
+
+    auto configFile = configuration_file_name();
+    const char* conf_path = configFile.localPath();
+
+    test_check(multio_conf_set_path(multio_cc, conf_path), "Configuration Path Changed");
+
+    auto configPath = configuration_path_name() / "testPlan.yaml";
+
+    test_check(multio_new_configurationcontext_from_filename(&multio_cc, configPath.localPath()),
+               "Configuration Context Created From Filename");
+
+    multio_handle_t* multio_handle = nullptr;
+    test_check(multio_new_handle(&multio_handle, multio_cc), "Create Handle");
+    std::unique_ptr<multio_handle_t> handle_deleter(multio_handle);
+
+    test_check(multio_open_connections(multio_handle), "Open Connections");
+
+    test_check(multio_close_connections(multio_handle), "Close Connections");
 }
 
 CASE("Metadata is created and delected sucessfully") {
@@ -251,6 +326,57 @@ CASE("Metadata can set values") {
 
     err = multio_delete_metadata(mdp);
     EXPECT(err == MULTIO_SUCCESS);
+}
+
+CASE("Test write field") {
+    multio_configurationcontext_t* multio_cc = nullptr;
+
+    auto configPath = configuration_path_name() / "testPlan.yaml";
+
+    test_check(multio_new_configurationcontext_from_filename(&multio_cc, configPath.localPath()),
+               "Configuration Context Created From Filename");
+    std::unique_ptr<multio_configurationcontext_t> configuration_context_deleter(multio_cc);
+
+    multio_handle_t* multio_handle = nullptr;
+    test_check(multio_new_handle(&multio_handle, multio_cc), "Create New handle");
+    EXPECT(multio_handle);
+    std::unique_ptr<multio_handle_t> handle_deleter(multio_handle);
+
+    auto field = configuration_path_name() / "../test.grib";
+    eckit::Length len = field.size();
+    eckit::Buffer buffer(len);
+
+    eckit::FileHandle infile{field};
+    infile.openForRead();
+    {
+        eckit::AutoClose closer(infile);
+        EXPECT(infile.read(buffer.data(), len) == len);
+    }
+
+    multio_metadata_t* md = nullptr;
+    test_check(multio_new_metadata(&md), "Create New Metadata Object");
+    std::unique_ptr<multio_metadata_t> multio_deleter(md);
+
+    test_check(multio_metadata_set_string(md, "category", "test_data"), "Set String");
+    test_check(multio_metadata_set_int(md, "globalSize", len), "Set Int");
+    test_check(multio_metadata_set_int(md, "level", 1), "Set Int");
+    test_check(multio_metadata_set_int(md, "step", 1), "Set Int");
+
+    test_check(multio_metadata_set_double(md, "missingValue", 0.0), "Set Double");
+    test_check(multio_metadata_set_bool(md, "bitmapPresent", false), "Set bool");
+    test_check(multio_metadata_set_int(md, "bitsPerValue", 16), "Set Int");
+
+    test_check(multio_metadata_set_bool(md, "toAllServers", false), "Set Bool");
+
+    // Overwrite these fields in the existing metadata object
+    test_check(multio_metadata_set_string(md, "name", "test"), "Set String");
+
+    test_check(multio_write_field(multio_handle, md, reinterpret_cast<const double*>(buffer.data()), len),
+               "Write Field");
+
+    auto file_name = configuration_path_name() / "../../../build/tests/multio/testWriteOutput.grib";
+    std::cout << file_name.localPath() << std::endl;
+    EXPECT(std::filesystem::exists(file_name.localPath()));
 }
 
 // TODO:

4ea59554c5d6f65c01c7108daf0979adbe90acd5
Adam Warde <adam.warde@ecmwf.int>
2023-06-09 14:41:19 +0200

    Add config class to pymultio with corresponding features from C API

--- pymultio/pymultio/__init__.py
+++ pymultio/pymultio/__init__.py
@@ -1,5 +1,6 @@
 from .constants import SUCCESS, ERROR_ECKIT_EXCEPTION, ERROR_GENERAL_EXCEPTION, ERROR_UNKNOWN_EXCEPTION
 from .lib import MultioException
+from .config import Config
 
 
 __version__ = "0.0.1"

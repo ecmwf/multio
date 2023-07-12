a02163394b85d18ab6df11ce1c047e48e7f51ef1
Adam Warde <adam.warde@ecmwf.int>
2023-07-12 13:38:50 +0200

    MUL-85: Change test file for new multio interface

--- pymultio/test.py
+++ pymultio/test.py
@@ -1,15 +1,23 @@
 import pymultio
 
-conf = pymultio.Config(allow_world=True, parent_comm=1, client_comm=[2,3], server_comm=[4,5])
+conf_dict = {
+      "allow_world" : True,
+      "parent_comm" : 1,
+      "client_comm" : [2,3],
+      "server_comm" : [4,5]
+}
+
+conf = pymultio.Config(**conf_dict)
+
+#conf = pymultio.Config(allow_world=True, parent_comm=1, client_comm=[2,3], server_comm=[4,5])
 
 handle = pymultio.Handler(conf)
 
-md = {'category' : 'path',
+metadata = {'category' : 'path',
       'new' : 1,
-      'new_float' : 1.0,
-      'Error' : (0,1)}
+      'new_float' : 1.0}
 
-md = pymultio.Metadata(handle, md)
+md = pymultio.Metadata(handle, metadata)
 md.metadata_set_string('category', 'path')
 md.metadata_set_int('globalSize', 4)
 md.metadata_set_int('level', 1)
@@ -26,3 +34,6 @@ handle.write_field_double(md, [1.0,2.0,3.0,4.0], 4)
 handle.notify(md)
 handle.flush(md)
 handle.field_accepted(md, False)
+
+multio_object = pymultio.Multio(conf_dict)
+multio_object.create_metadata(md=metadata)

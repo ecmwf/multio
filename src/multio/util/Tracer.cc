d6ddf3870063387b86a24387b5db077883aa1dd6
Razvan Aguridan <razvan.aguridan@ecmwf.int>
2023-07-19 09:44:43 +0200

    Add tracing support
    The Tracer class allows reporting and saving events happening in
    the multio code.

---
+++ src/multio/util/Tracer.cc
@@ -0,0 +1,127 @@
+#include "Tracer.h"
+
+#include <chrono>
+
+#include <iostream>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+namespace {
+const uint32_t CHUNK_MASK = 0xFF000000;
+const uint32_t CHUNK_SHIFT = 24;
+const uint32_t INDEX_MASK = 0x00FFFFFF;
+}  // namespace
+
+namespace multio::util {
+
+Tracer::Tracer(uint32_t numChunks, uint32_t chunkSize, const std::string& outputPath) :
+    chunkSize_(chunkSize * 2),
+    traceChunks_(numChunks, nullptr),
+    currentChunkAndIndex_(0),
+    availableQueue_(numChunks),
+    writeQueue_(numChunks),
+    outputFile_(outputPath),
+    running_(true),
+    traceWriterThread_([this]() { writerThread_(); }) {
+    for (auto i = 0; i < numChunks; ++i) {
+        traceChunks_[i] = new uint64_t[chunkSize_];
+        if (i > 0) {
+            availableQueue_.push(i);
+        }
+    }
+}
+
+Tracer::~Tracer() {
+    uint32_t chunkAndIndex = currentChunkAndIndex_.load(std::memory_order::memory_order_acquire);
+    uint32_t chunk = (chunkAndIndex & CHUNK_MASK) >> CHUNK_SHIFT;
+
+    writeQueue_.push(chunk);
+
+    // Removing this will cause the last chunk to be ocassionally lost.
+    // (race condition between write queue and running flag)
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    running_ = false;
+
+    traceWriterThread_.join();
+}
+
+void Tracer::recordEvent(uint64_t event) {
+    const uint64_t timestamp = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+    bool updated = false;
+    uint32_t chunk = 0;
+    uint32_t index = 0;
+    uint32_t chunkAndIndex = currentChunkAndIndex_.load(std::memory_order::memory_order_acquire);
+
+    do {
+        chunk = (chunkAndIndex & CHUNK_MASK) >> CHUNK_SHIFT;
+        index = chunkAndIndex & INDEX_MASK;
+
+        // check if we still have place in the current chunk to log a new event
+        const auto nextSize = index + 2;
+        const auto switchToNextChunk = nextSize >= chunkSize_;
+        const auto indexNext = switchToNextChunk ? 0 : nextSize;
+
+        auto chunkNext = chunk;
+        if (switchToNextChunk) {
+            // we don't have place in the current chunk to log a new event,
+            // get the next available chunk from the available queue
+            auto availableChunkId = availableQueue_.pop();
+            while (!availableChunkId) {
+                availableChunkId = availableQueue_.pop();
+            }
+
+            chunkNext = availableChunkId.value();
+        }
+
+        const auto chunkAndIndexNext = (chunkNext << CHUNK_SHIFT) | indexNext;
+        updated = currentChunkAndIndex_.compare_exchange_strong(
+            chunkAndIndex, chunkAndIndexNext, std::memory_order::memory_order_acq_rel, std::memory_order_acquire);
+
+        if (switchToNextChunk) {
+            // we wanted to change the chunk
+            if (updated) {
+                // we changed the chunk, push the completed chunk to the write queue
+                writeQueue_.push(chunk);
+                chunk = chunkNext;
+            }
+            else {
+                // some other thread updated the chunk counters, most likely also changing the chunk,
+                // so we put the available chunk we retrieved back in the available queue
+                availableQueue_.push(chunkNext);
+            }
+        }
+    } while (!updated);
+
+    traceChunks_[chunk][index] = event;
+    traceChunks_[chunk][index + 1] = timestamp;
+}
+
+void Tracer::writerThread_() {
+    const auto traceFileHandle = open(outputFile_.c_str(), O_CREAT | O_TRUNC | O_WRONLY, S_IWUSR | S_IRUSR);
+
+    while (running_.load(std::memory_order_acquire)) {
+        const auto finishedChunkId = writeQueue_.pop();
+        if (finishedChunkId) {
+            const auto id = finishedChunkId.value();
+
+            const auto numBytesToWrite = 2 * chunkSize_ * sizeof(uint64_t);
+            const auto bytes = reinterpret_cast<const void*>(traceChunks_[id]);
+
+            write(traceFileHandle, bytes, numBytesToWrite);
+            // fsync(traceFileHandle);
+
+            availableQueue_.push(id);
+        }
+        else {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+
+    close(traceFileHandle);
+}
+
+}  // namespace multio::util
---
+++ tests/multio/test_multio_tracing.cc
@@ -0,0 +1,70 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+// @author Razvan Aguridan
+
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include "eckit/testing/Test.h"
+
+#include "multio/util/Tracer.h"
+
+namespace multio::test {
+CASE("Test single thread tracing.") {
+    multio::util::Tracer tracer(8, 32768, "./trace_info.bin");
+
+    const auto startTime = std::chrono::high_resolution_clock::now();
+
+    for (auto i = 0; i < 1000000; ++i) {
+        tracer.recordEvent(i);
+    }
+
+    const auto endTime = std::chrono::high_resolution_clock::now();
+    std::cout << "Time to trace 1000000 events from 1 single thread: " << (endTime - startTime).count() << " ns!"
+              << std::endl;
+}
+
+CASE("Test multiple thread tracing.") {
+    multio::util::Tracer tracer(8, 32768, "./trace_info2.bin");
+
+    std::atomic_bool start = false;
+
+    std::vector<std::thread> producers;
+    for (uint32_t i = 0; i < 10; ++i) {
+        producers.push_back(std::thread([&tracer, &start, i]() {
+            while (!start.load(std::memory_order_acquire))
+                ;
+
+            for (auto j = 0; j < 1000000; ++j) {
+                tracer.recordEvent(i * 10 + j);
+            }
+        }));
+    }
+
+    const auto startTime = std::chrono::high_resolution_clock::now();
+
+    start.store(true, std::memory_order_release);
+
+    for (auto& t : producers) {
+        t.join();
+    }
+
+    const auto endTime = std::chrono::high_resolution_clock::now();
+    std::cout << "Time to trace 10000000 events from 10 threads: " << (endTime - startTime).count() << " ns!"
+              << std::endl;
+}
+}  // namespace multio::test
+
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}

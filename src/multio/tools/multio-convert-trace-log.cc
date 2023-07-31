86641174a3f62b14e5286146187c9ca34bdeeeb0
Razvan Aguridan <razvan.aguridan@ecmwf.int>
2023-07-31 14:23:41 +0200

    Rework trace conversion tool to reduce memory consumption

--- src/multio/tools/multio-convert-trace-log.cc
+++ src/multio/tools/multio-convert-trace-log.cc
@@ -4,19 +4,12 @@
 #include "eckit/log/Log.h"
 #include "eckit/option/SimpleOption.h"
 
+#include <algorithm>
 #include <map>
 #include <vector>
 
 #include <fcntl.h>
 #include <unistd.h>
-
-namespace {
-struct TraceData {
-    uint64_t timestamp;
-    uint32_t traceEventId;
-    uint16_t extraInfo;
-};
-}  // namespace
 
 class MultioConvertTraceLog final : public multio::MultioTool {
 public:  // methods
@@ -50,80 +43,102 @@ void MultioConvertTraceLog::execute(const eckit::option::CmdArgs& args) {
     std::string output = "sfc";
     args.get("output", output);
 
-    const auto traceFileHandle = open(input.c_str(), O_RDONLY);
-
-    const auto fileSize = lseek(traceFileHandle, 0, SEEK_END);
-    lseek(traceFileHandle, 0, SEEK_SET);
-
-    std::vector<uint64_t> traceData(fileSize / sizeof(uint64_t));
-
-    read(traceFileHandle, reinterpret_cast<void*>(traceData.data()), fileSize);
-
-    close(traceFileHandle);
-
-    std::map<uint32_t, TraceData> starts;
-    std::map<uint32_t, TraceData> ends;
-
-    bool finished = false;
-    for (auto i = 0; (i < traceData.size() / 2) && !finished; ++i) {
-        const auto event = traceData[2 * i];
-        if (event == 0) {
-            continue;
-        }
-
-        TraceData item;
-
-        item.timestamp = traceData[2 * i + 1];
-        const auto uniqueEventId = event & 0xFFFFFFFFULL;
-        item.traceEventId = (event & (0xFFULL << 56)) >> 56;
-        const auto extraInfo = (event & (0xFFFFULL << 32)) >> 32;
-        item.extraInfo = extraInfo;
-        const auto started = (event & (0xFFULL << 48)) > 0;
-
-        if (started) {
-            if (starts.count(uniqueEventId) == 0) {
-                starts[uniqueEventId] = std::move(item);
-            }
-            else {
-                std::cerr << "Event with id: " << uniqueEventId << " comming from value: " << event
-                          << " already found in the start data!" << std::endl;
-            }
-        }
-        else {
-            if (ends.count(uniqueEventId) == 0) {
-                ends[uniqueEventId] = std::move(item);
-            }
-            else {
-                std::cerr << "Event with id: " << uniqueEventId << " comming from value: " << event
-                          << " already found in the end data!" << std::endl;
-            }
-        }
-    }
-
-    traceData.clear();
-
     const auto csvFileHandle = open(output.c_str(), O_CREAT | O_TRUNC | O_WRONLY, S_IWUSR | S_IRUSR);
 
     const std::string header("unique_id,event_id,extraInfo,start_time,end_time\r\n");
     write(csvFileHandle, reinterpret_cast<const void*>(header.c_str()), header.size());
 
-    for (const auto& startEvent : starts) {
-        try {
-            const auto& correspondingEnd = ends.at(startEvent.first);
+    std::map<uint32_t, uint64_t> starts;
+    std::map<uint32_t, uint64_t> ends;
 
-            std::ostringstream oss;
-            oss << startEvent.first << "," << startEvent.second.traceEventId << "," << startEvent.second.extraInfo
-                << "," << startEvent.second.timestamp << "," << correspondingEnd.timestamp << "\r\n";
+    const auto traceFileHandle = open(input.c_str(), O_RDONLY);
 
-            const auto line = oss.str();
+    const auto fileSize = lseek(traceFileHandle, 0, SEEK_END);
+    lseek(traceFileHandle, 0, SEEK_SET);
 
-            write(csvFileHandle, reinterpret_cast<const void*>(line.c_str()), line.size());
+    off_t processedSize = 0;
+    while (processedSize < fileSize) {
+        off_t sizeToRead = std::min(static_cast<off_t>(1024 * 1024 * 1024), (fileSize - processedSize));
+
+        std::vector<uint64_t> traceData(sizeToRead / sizeof(uint64_t));
+
+        read(traceFileHandle, reinterpret_cast<void*>(traceData.data()), sizeToRead);
+
+        for (auto i = 0; (i < traceData.size() / 2); ++i) {
+            const auto event = traceData[2 * i];
+            if (event == 0) {
+                continue;
+            }
+
+            const auto uniqueEventId = event & 0xFFFFFFFFULL;
+            const auto traceEventId = (event & (0xFFULL << 56)) >> 56;
+            const auto extraInfo = (event & (0xFFFFULL << 32)) >> 32;
+            const auto started = (event & (0xFFULL << 48)) > 0;
+
+            const auto timestamp = traceData[2 * i + 1];
+
+            if (started) {
+                if (ends.count(uniqueEventId) == 0) {
+                    if (starts.count(uniqueEventId) == 0) {
+                        starts[uniqueEventId] = timestamp;
+                    }
+                    else {
+                        std::cerr << "Event with id: " << uniqueEventId << " comming from value: " << event
+                                  << " already found in the start data!" << std::endl;
+                    }
+                }
+                else {
+                    const auto correspondingEnd = ends.at(uniqueEventId);
+
+                    std::ostringstream oss;
+                    oss << uniqueEventId << "," << traceEventId << "," << extraInfo << "," << timestamp << ","
+                        << correspondingEnd << "\r\n";
+
+                    const auto line = oss.str();
+
+                    write(csvFileHandle, reinterpret_cast<const void*>(line.c_str()), line.size());
+
+                    ends.erase(uniqueEventId);
+                }
+            }
+            else {
+                if (starts.count(uniqueEventId) == 0) {
+                    if (ends.count(uniqueEventId) == 0) {
+                        ends[uniqueEventId] = timestamp;
+                    }
+                    else {
+                        std::cerr << "Event with id: " << uniqueEventId << " comming from value: " << event
+                                  << " already found in the end data!" << std::endl;
+                    }
+                }
+                else {
+                    const auto correspondingStart = starts.at(uniqueEventId);
+
+                    std::ostringstream oss;
+                    oss << uniqueEventId << "," << traceEventId << "," << extraInfo << "," << correspondingStart << ","
+                        << timestamp << "\r\n";
+
+                    const auto line = oss.str();
+
+                    write(csvFileHandle, reinterpret_cast<const void*>(line.c_str()), line.size());
+
+                    starts.erase(uniqueEventId);
+                }
+            }
         }
-        catch (const std::out_of_range& ex) {
-            std::cerr << "Event with id: " << startEvent.first << " does not have an end timestamp." << std::endl;
-        }
+
+        processedSize += sizeToRead;
     }
 
+    for (auto const& ev : starts) {
+        std::cerr << "Event with id: " << ev.first << " does not have a matching end event!" << std::endl;
+    }
+
+    for (auto const& ev : ends) {
+        std::cerr << "Event with id: " << ev.first << " does not have a matching start event!" << std::endl;
+    }
+
+    close(traceFileHandle);
     close(csvFileHandle);
 }
 

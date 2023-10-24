f6fc485b4bbee1a86acc989eec6cd1573fb9f2c3
Mirco Valentini <mirco.valentini@ecmwf.int>
2023-10-24 04:36:22 +0200

    MUL-106: Allow expanding FDB configuration with envvars

--- src/multio/fdb5/FDB5Sink.cc
+++ src/multio/fdb5/FDB5Sink.cc
@@ -16,6 +16,9 @@
 #include "multio/LibMultio.h"
 
 #include "fdb5/config/Config.h"
+#include "eckit/config/LocalConfiguration.h"
+#include "multio/util/Substitution.h"
+#include "eckit/value/Value.h"
 
 
 namespace multio::sink {
@@ -23,8 +26,27 @@ namespace multio::sink {
 using config::ComponentConfiguration;
 
 namespace {
+
+void replaceCurly( const ComponentConfiguration& compConf, eckit::LocalConfiguration& cfg ){
+    for (auto& key : cfg.keys()) {
+        // Replace the value if it is string
+        if ( cfg.getSubConfiguration(key).get().isString() ){
+            cfg.set(key, compConf.multioConfig().replaceCurly(cfg.getString(key)) );
+        }
+        // Recursive replace of curly brackets
+        if ( cfg.getSubConfiguration(key).get().isMap() ){
+            auto tmp = cfg.getSubConfiguration(key);
+            replaceCurly( compConf, tmp );
+            cfg.set(key,tmp);
+        }
+
+    }
+    return;
+}
+
 fdb5::Config fdb5_configuration(const ComponentConfiguration& compConf) {
     auto fdb_configuration = compConf.parsedConfig().getSubConfiguration("config");
+    replaceCurly(compConf,fdb_configuration);
 
     eckit::LocalConfiguration userConfig;
     if (fdb_configuration.has("userConfig")) {
@@ -39,8 +61,10 @@ fdb5::Config fdb5_configuration(const ComponentConfiguration& compConf) {
     LOG_DEBUG_LIB(LibMultio) << "FDB5 Config = " << fdb_config << std::endl;
 
     return fdb_config;
+
 }
 }  // namespace
+
 
 FDB5Sink::FDB5Sink(const ComponentConfiguration& compConf) : DataSink(compConf), fdb_{fdb5_configuration(compConf)} {
     LOG_DEBUG_LIB(LibMultio) << "Config = " << compConf.parsedConfig() << std::endl;

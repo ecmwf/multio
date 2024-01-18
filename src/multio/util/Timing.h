f0d753f0609769641c59ace7cfe4d75d9abb0087
Philipp Geier <philipp.geier@ecmwf.int>
2024-01-18 19:51:34 +0100

    Remove commented code

--- src/multio/util/Timing.h
+++ src/multio/util/Timing.h
@@ -106,27 +106,5 @@ public:
 template <typename Clock = std::chrono::steady_clock, std::size_t NAgg = 16>
 ScopedTiming(Timing<Clock, NAgg>&) -> ScopedTiming<Clock, NAgg>;
 
-// class ScopedTimer {
-//     eckit::Timing& timing_;
-//     eckit::Timer timer_;
-
-// public:
-//     explicit ScopedTimer(eckit::Timing& t) : timing_{t} { timer_.start(); }
-//     ~ScopedTimer() {
-//         timer_.stop();
-//         timing_ += timer_;
-//     }
-// };
-
-// class ScopedTiming {
-//     eckit::Timer& timer_;
-//     eckit::Timing& timing_;
-//     eckit::Timing start_;
-
-// public:
-//     ScopedTiming(eckit::Timer& timer, eckit::Timing& timing) : timer_{timer}, timing_{timing}, start_{timer} {}
-
-//     ~ScopedTiming() { timing_ += eckit::Timing{timer_} - start_; }
-// };
 
 }  // namespace multio::util

7e7e21f1032757cca3437e0e344213eb13f02387
Razvan Aguridan <razvan.aguridan@ecmwf.int>
2024-02-26 16:03:36 +0000

    Wrap codes_handle inside std::unique_ptr

--- src/multio/tools/multio-generate-grib-template.cc
+++ src/multio/tools/multio-generate-grib-template.cc
@@ -181,6 +181,23 @@ void setRegularLLFields(codes_handle* handle, const eckit::option::CmdArgs& args
     handleCodesError("eccodes error while setting the longitudeOfLastGridPointInDegrees value: ", err, Here());
 }
 
+void codesHandleDeleter(codes_handle* ptr) {
+    auto err = codes_handle_delete(ptr);
+    handleCodesError("eccodes error while freeing the handle: ", err, Here());
+}
+
+using GribHandle = std::unique_ptr<codes_handle, decltype(&codesHandleDeleter)>;
+
+GribHandle createGribFromFile(const std::string& path) {
+    eckit::AutoStdFile fin(path);
+
+    int err = 0;
+    auto handle = codes_handle_new_from_file(nullptr, fin, PRODUCT_GRIB, &err);
+    handleCodesError("Could not create an eccodes handle from this file: ", err, Here());
+
+    return GribHandle(handle, &codesHandleDeleter);
+}
+
 }  // namespace
 
 //----------------------------------------------------------------------------------------------------------------
@@ -223,14 +240,16 @@ void MultioGenerateGribTemplate::finish(const eckit::option::CmdArgs&) {
 }
 
 void MultioGenerateGribTemplate::execute(const eckit::option::CmdArgs& args) {
+    int err = 0;
+
     std::string gribSamplePath = "";
     args.get("sample", gribSamplePath);
-    eckit::AutoStdFile fin{gribSamplePath};
-    int err;
-    auto sampleHandle = codes_handle_new_from_file(nullptr, fin, PRODUCT_GRIB, &err);
+
+    const auto sampleHandle = createGribFromFile(gribSamplePath);
+
     char buffer[255] = {0};
     size_t bufferLen{255};
-    err = codes_get_string(sampleHandle, "gridType", buffer, &bufferLen);
+    err = codes_get_string(sampleHandle.get(), "gridType", buffer, &bufferLen);
     handleCodesError("eccodes error while reading the grid type: ", err, Here());
 
     bool copyPL = false;
@@ -241,53 +260,49 @@ void MultioGenerateGribTemplate::execute(const eckit::option::CmdArgs& args) {
     if (copyPL) {
         std::string plSourcePath = "";
         args.get("plSource", plSourcePath);
-        eckit::AutoStdFile plFile{plSourcePath};
-        auto plSource = codes_handle_new_from_file(nullptr, plFile, PRODUCT_GRIB, &err);
-        handleCodesError("eccodes error while reading the PL array source: ", err, Here());
+
+        const auto plSource = createGribFromFile(plSourcePath);
 
         long nj = 0;
-        err = codes_get_long(plSource, "Nj", &nj);
+        err = codes_get_long(plSource.get(), "Nj", &nj);
         handleCodesError("eccodes error while reading the number of PL values: ", err, Here());
 
-        err = codes_set_long(sampleHandle, "Nj", nj);
+        err = codes_set_long(sampleHandle.get(), "Nj", nj);
         handleCodesError("eccodes error while setting the Nj value: ", err, Here());
 
         std::vector<double> plArray(nj, 0.0);
         size_t plSize = plArray.size();
-        err = codes_get_double_array(plSource, "pl", plArray.data(), &plSize);
+        err = codes_get_double_array(plSource.get(), "pl", plArray.data(), &plSize);
         handleCodesError("eccodes error while reading the PL array: ", err, Here());
 
-        err = codes_set_double_array(sampleHandle, "pl", plArray.data(), plArray.size());
+        err = codes_set_double_array(sampleHandle.get(), "pl", plArray.data(), plArray.size());
         handleCodesError("eccodes error while setting the PL array: ", err, Here());
-
-        err = codes_handle_delete(plSource);
-        handleCodesError("eccodes error while freeing the PL source handle: ", err, Here());
 
         const auto numValues = std::accumulate(plArray.cbegin(), plArray.cend(), 0.0);
         std::vector<double> values(numValues, 0.0);
-        err = codes_set_double_array(sampleHandle, "values", values.data(), values.size());
+        err = codes_set_double_array(sampleHandle.get(), "values", values.data(), values.size());
         handleCodesError("eccodes error while setting the values array: ", err, Here());
     }
 
     if (clearValues) {
         size_t numValues = 0;
-        err = codes_get_size(sampleHandle, "values", &numValues);
+        err = codes_get_size(sampleHandle.get(), "values", &numValues);
         handleCodesError("eccodes error while reading the number of values: ", err, Here());
 
         std::vector<double> values(numValues, 0.0);
-        err = codes_set_double_array(sampleHandle, "values", values.data(), values.size());
+        err = codes_set_double_array(sampleHandle.get(), "values", values.data(), values.size());
         handleCodesError("eccodes error while setting the values array: ", err, Here());
     }
 
     if (!clearValues && !copyPL) {
         long generatingProcessIdentifier = 153;
         args.get("generatingProcessIdentifier", generatingProcessIdentifier);
-        err = codes_set_long(sampleHandle, "generatingProcessIdentifier", generatingProcessIdentifier);
+        err = codes_set_long(sampleHandle.get(), "generatingProcessIdentifier", generatingProcessIdentifier);
         handleCodesError("eccodes error while setting the generatingProcessIdentifier: ", err, Here());
 
         char buffer[255] = {0};
         size_t bufferLen{255};
-        err = codes_get_string(sampleHandle, "gridType", buffer, &bufferLen);
+        err = codes_get_string(sampleHandle.get(), "gridType", buffer, &bufferLen);
         handleCodesError("eccodes error while reading the grid type: ", err, Here());
 
         using UpdateFunctionType = std::function<void(codes_handle*, const eckit::option::CmdArgs&)>;
@@ -301,7 +316,7 @@ void MultioGenerateGribTemplate::execute(const eckit::option::CmdArgs& args) {
         const std::string sampleGridType(buffer);
         if (updateFunctionMap.count(sampleGridType) != 0) {
             auto updateFunction = updateFunctionMap.at(sampleGridType);
-            updateFunction(sampleHandle, args);
+            updateFunction(sampleHandle.get(), args);
         }
         else {
             std::ostringstream oss;
@@ -312,11 +327,8 @@ void MultioGenerateGribTemplate::execute(const eckit::option::CmdArgs& args) {
 
     std::string outputFilePath = "";
     args.get("output", outputFilePath);
-    err = codes_write_message(sampleHandle, outputFilePath.c_str(), "wb");
+    err = codes_write_message(sampleHandle.get(), outputFilePath.c_str(), "wb");
     handleCodesError("eccodes error while writing output file: ", err, Here());
-
-    err = codes_handle_delete(sampleHandle);
-    handleCodesError("eccodes error while freeing the template handle: ", err, Here());
 }
 
 //---------------------------------------------------------------------------------------------------------------

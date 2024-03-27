bb79357d05086e8e9ec1061f8b93c02d58ec29d2
Domokos Sármány <domokos.sarmany@ecmwf.int>
2024-03-27 21:45:39 +0000

    Fix order of imports

--- pymultio/multiopython/multio.py
+++ pymultio/multiopython/multio.py
@@ -1,10 +1,8 @@
+import importlib.util
 import os
 
 from .lib import ffi, lib
 from .metadata import Metadata
-
-
-import importlib.util
 
 numpy_spec = importlib.util.find_spec("numpy")
 haveNumpy = numpy_spec is not None

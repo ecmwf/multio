d7438f054fcbca9f684cb58ce73a4e74913aa01c
Mirco Valentini <mirco.valentini@ecmwf.int>
2024-04-02 06:39:03 +0200

    MUL-120: Run scripts and configuration

--- src/ecom/scripts/run.sh
+++ src/ecom/scripts/run.sh
@@ -1,86 +1,109 @@
 #!/bin/bash
 
-#
 usage(){
-    echo "Usage: $0 -p paramId -l level -s step -d databaseFile -i inputDir -b binaryPath -c samplesPath"
+    echo "Usage: $0 -m <dirName> -s <[0-9]*> -l <[ml|pl|sfc|t|v]> -f <0|1> [-h]"
     echo
     echo "Options:"
-    echo "  -p paramId        Parameter ID."
-    echo "  -l level          Level."
-    echo "  -s step           Step."
-    echo "  -d databaseFile   Path to the input index file."
-    echo "  -i inputDir       Path to the directory containing the input files."
-    echo "  -b binaryPath     Path to the directory containing the binary files."
-    echo "  -c samplesPath    Path to the directory containing the samples."
-    echo "  -y yamlConfig     Path to the yaml configuration file."
-    echo "  -h                Print this message."
+    echo "  -m mainDir     Main directory where the databaase has been extracted/filtered."
+    echo "  -s step        Step"
+    echo "  -l levType     levType"
+    echo "  -f isSperical  true if it is a spherical harmonics"
+    echo "  -h             Print this message."
     echo
-    echo "EXAMPLE: run.sh -y "/ec/res4/scratch/mavm/develop_v7/raps/multio_yaml/output-manager-config.yaml" -i "/ec/res4/scratch/mavm/develop_v7/runs/rundir/tco399l137/i0e9/hres/intel.hpcx/blq.intel.sp/h24.N8.T120xt8xh1+ioT8xt8xh0.iomode_od.i16r0w16.ORCA025_Z75.htco399-33824009" -p 77  -l 4 -s 0 -d "./index.sorted.dat" -b "/ec/res4/scratch/mavm/develop_v7/ifs-bundle/build/bin" -c "/ec/res4/scratch/mavm/develop_v7/ifs-bundle/source/eccodes/ifs_samples/grib1_mlgrib2""
-    echo "All parameters must be provided. The script will exit with an error if any parameter is missing."
+    echo "All paths must be directories that exist. The script will exit with an error if any path does not exist."
+    echo "The -v option is optional and enables verbose mode."
 }
+
+
+#
+# 
+levType2enum(){
+# echo "Input option:: " ${1}
+case ${1} in
+  ml)
+    echo 1
+    return;
+    ;;
+  pl) 
+    echo 2
+    return;
+    ;;
+  sfc)
+    echo 5	  
+    return;
+    ;;
+  t)
+    echo 4
+    return;
+    ;;
+  v)
+    echo 3
+    return;
+    ;;
+  *)
+    echo "ERROR: unknown levType"
+    exit 1
+  esac
+}
+
 
 #
 # Initialize our own variables:
-paramId=-1
-level=-1
-step=-1
-databaseFile=""
-inputDir=""
-binaryPath=""
-samplesPath=""
-yamlConfig=""
-nProcs=1
+mainDir="./compareDB.mavm"
+step=4
+cstep="step_0004"
+levType="ml"
+levTypeEnum=0
+isSpehrical=0
+executable="./secom"
+reproducerFolder="./reproducer"
+configurationFile="output_manager_config.yaml"
+
+# Bold green text for [OK]
+OK="\e[1;32mOK\e[0m"
+
+# Bold red text for [KO]
+KO="\e[1;31mKO\e[0m"
 
 #
 # Parse command-line arguments
-while getopts "p:l:s:d:i:b:c:y:n:h" opt; do
+while getopts "b:r:y:m:s:l:f:h" opt; do
   case ${opt} in
-    p) # paramId
-      paramId="${OPTARG}"
+    b)
+      executable=${OPTARG};
+      if ! [[ -x ${executable} ]] ; then
+        echo "ERROR: executable does not exist"
+      fi
       ;;
-    l) # level
-      level="${OPTARG}"
+    r)
+      reproducerFolder=${OPTARG};
+      if ! [[ -d ${reproducerFolder} ]] ; then
+        echo "ERROR: reproducer folder does not exist"
+      fi
       ;;
-    s) # step
+    y)
+      configurationFile=${OPTARG};
+      if ! [[ -f ${configurationFile} ]] ; then
+        echo "ERROR: configuration file does not exist"
+      fi
+      ;;
+    m)
+      mainDir="${OPTARG}"
+      ;;
+    s)
       step="${OPTARG}"
+      cstep=`printf "step_%4.4d" ${step}`
       ;;
-    d) # input index file
-      databaseFile="${OPTARG}"
-      if [[ ! -f ${databaseFile} ]] ; then
-          echo "Error: The input index file does not exist"
-          exit 1
+    l)
+      levType="${OPTARG}"
+      levTypeEnum=`levType2enum  ${levType}`
+      ;;
+    f)
+      isSpherical="${OPTARG}"
+      if [[ ${isSpherical} != "0" ]] && [[ ${isSpherical} != "1" ]] ; then
+        echo "ERROR: isSperical wrong value: ${isSpherical}"
+	exit 1
       fi
-      ;;
-    i) # input index file
-      inputDir="${OPTARG}"
-      if [[ ! -d ${inputDir} ]] ; then
-          echo "Error: The input directory does not exist"
-          exit 1
-      fi
-      ;;
-    b) # binary path
-      binaryPath="${OPTARG}"
-      if [[ ! -d ${binaryPath} ]] ; then
-          echo "Error: The binary path does not exist"
-          exit 1
-      fi
-      ;;
-    c) # codes samples path
-      samplesPath="${OPTARG}"
-      if [[ ! -d ${samplesPath} ]] ; then
-          echo "Error: The samples path does not exist"
-          exit 1
-      fi
-      ;;
-    y) # codes samples path
-      yamlConfig="${OPTARG}"
-      if [[ ! -f ${yamlConfig} ]] ; then
-          echo "Error: The yaml configuration file does not exist: ${yamlConfig}"
-          exit 1
-      fi
-      ;;
-    n) # number of processors
-      nProcs="${OPTARG}"
       ;;
     h)
       usage
@@ -94,68 +117,111 @@ while getopts "p:l:s:d:i:b:c:y:n:h" opt; do
 done
 
 #
+# 
+export ECCODES_SAMPLES_PATH=/home/valentini/ecmwf/ecom/multio-bundle/source/eccodes/ifs_samples/grib1_mlgrib2
+
+#
+# Create the main directory name
+dirName=`printf "%s/step_%4.4d/%s_%d" ${mainDir} ${step} ${levType} ${isSpherical}`
+
+#
 # Error handling
-if !command -v ${binaryPath}/output_manager_tool.SP &> /dev/null; then
-    echo "output manager tool is not in the path is not in the PATH"
+if ! [[ -d ${mainDir} ]] ; then
+  echo "Error: The main directory does not exist: ${mainDir}"
+  exit 1
+fi
+
+if ! [[ -d ${mainDir}/${cstep} ]] ; then
+  echo "Error: The main directory does not exist: ${mainDir}/${cstep}"
+  exit 1
+fi
+
+if ! [[ -d ${dirName} ]] ; then
+  echo "Error: The directory does not exist: ${dirName}"
+  exit 1
+fi
+
+
+# Error handling
+if !command -v ${bin_path}/grib_get &> /dev/null; then
+    echo "grib_get is not in the PATH"
     exit 1
 fi
 
-if !command -v "${binaryPath}"/grib_coompare &> /dev/null; then
-    echo "grib_compare is not in the PATH"
-    exit 1
-fi
 
-#
-# Generate the report of the input configuration
-echo "Input Configuration Report:"
-echo "---------------------------"
-echo "Parameter ID.........: $paramId"
-echo "Level................: $level"
-echo "Step.................: $step"
-echo "Representation.......: $representation"
-echo "Level Type...........: $levType"
-echo "Input Index File.....: $databaseFile"
-echo "Input Directory......: $inputDir"
-echo "Binary Path..........: $binaryPath"
-echo "Samples Path.........: $samplesPath"
-echo "YAML Configuration...: $yamlConfig"
+# echo ${dirName}
+cd ${dirName}
+rm -f *.grib
+rm -f *.log
+rm -f *.report
+rm -rf ./errors
+mkdir ./errors
+N=`find . -type f -iname 'msg_*' | wc -l`;
+cnt=0;
+err=0;
+echo " - ${N} fields to check: ${dirName}"
+for i in `find . -type f -iname 'msg_*'` ; do
+  cnt=$((${cnt}+1))	
+  tmp=`echo "scale=10; ${cnt}/${N}*100" | bc -l | sed -e 's/\./,/'`;
+  adv=`printf "%6.2f" ${tmp}`;
+  # Extract paramId and levelist
+  # echo "Processing file: ", $i
+  pt=`echo $i | sed -e '{s/_/ /g}' | awk '{print $2;}'`;
+  l=`echo $i | sed -e '{s/_/ /g}' | awk '{print $3;}'`;
+  if [[ ${levTypeEnum} -eq 2  ]] ; then
+    l=$((${l}*100))
+  fi
+  p=`echo ${pt} | awk -F '.' '{ if( NF==2 ){ if ( $2 != 128 ) {printf "%3.3d%3.3d", $2, $1;} else { print $1; } } else { print $1; }  }'`;
 
-#
-# Set the environment variables
-export ECCODES_SAMPLES_PATH=${samplesPath}
+  r=$((${isSpherical} + 1))
+  # Checking the field
+  command="${executable} -t GRIBX -i ${reproducerFolder}  -y ${configurationFile} -s ${step}  -q ${levTypeEnum} -r ${r} -p ${p} -l ${l} -n 8"
+  echo -n "[${adv}%].........................................................."
+  rm -f *.grib
+  rm -f *.log
+  rm -f *.report
+  ${executable} -t "GRIBX" -i "${reproducerFolder}"  -y "${configurationFile}" -s "${step}"  -q "${levTypeEnum}" -r "${r}" -p "${p}" -l "${l}" -n "8"   > encoding.log
+  if [[ -f ./allfields_00000001.grib  ]] ; then
+    grib_compare -H "./allfields_00000001.grib" "${i}" > grib_compare.log 2>&1; 
+    if [[ $? -ne 0 ]] ; then  
+      # echo "ERROR: encoded file not match"
+      # cat encoding.log
+      # echo -e "["${KO}"]"
+      echo -e "["${KO}"]"
+      mkdir "./errors/${i}"
+      echo ${command} > "./errors/${i}/command.log"
+      mv -f *.report "./errors/${i}" 2&> /dev/null
+      mv grib_compare.log "./errors/${i}"
+      mv *.log ./errors/${i}/
+      cp $i ./errors/${i}/
+      mv *.grib ./errors/${i}/
+      err=$((${err}+1))
+    else
+      echo -e "["${OK}"]"
+    fi
+  else
+    # echo "ERROR: encoded file not generated"
+    # cat encoding.log
+    echo -e "["${KO}"]"
+    mkdir "./errors/${i}"
+    echo ${command} > "./errors/${i}/command.log"
+    mv -f *.report "./errors/${i}" 2&> /dev/null
+    mv grib_compare.log "./errors/${i}"
+    mv *.log ./errors/${i}/
+    cp $i ./errors/${i}/
+    mv *.grib ./errors/${i}/
+    err=$((${err}+1))
+  fi
+  rm -f *.grib
+  rm -f *.log
+  rm -f *.report
+done
 
-${binaryPath}/codes_info
-
-#
-# Run the standalone tool
-${binaryPath}/standalone-output-manager -t "GRIBX" -n 8 -i "${inputDir}" -p "${paramId}" -l "${level}" -s "${step}" -y "${yamlConfig}"
-
-#
-# Search and extract the reference message
-sourceSearch01.sh -p "${paramId}" -l "${level}" -s "${step}" -i "${databaseFile}" -b "${binaryPath}"
-
-#
-# Check if the reference file was created
-if ! [[ -f "reference.grib" ]]; then
-    echo "Error: The reference file was not created"
-    exit 1
-fi
-
-if ! [[ -f "allfields_00000001.grib" ]]; then
-    echo "Error: The reference file was not created"
-    exit 1
-fi
-
-#
-# Compare the reference file with the fields file
-${binaryPath}/grib_compare -H "allfields_00000001.grib" "reference.grib"
-
-#
-# Report comparison result
-if [[ $? -ne 0 ]]; then
-    echo "Error: The files are different"
-    exit 1
+if [[ ${err} -gt 0 ]] ; then
+  echo -e "\e[1;31m $err tests out of ${N} failed \e[0m"
 else
-    echo "The files are the same"
-    exit 0
+  echo -e "\e[1;32m all tests passed\e[0m"
 fi
+# Exit on success
+exit 0
+

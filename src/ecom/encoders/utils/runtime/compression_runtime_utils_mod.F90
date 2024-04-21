3e95898121468ee9cda8649e6109a4b4ad30fe49
Mirco Valentini <mirco.valentini@ecmwf.int>
2024-04-21 22:30:26 +0200

    bugfix on packing

--- src/ecom/encoders/utils/runtime/compression_runtime_utils_mod.F90
+++ src/ecom/encoders/utils/runtime/compression_runtime_utils_mod.F90
@@ -14,9 +14,6 @@ IMPLICIT NONE
 ! Default visibility
 PRIVATE
 
-CHARACTER(LEN=32) :: PACKING_TYPE
-
-
 ! Whitelist of public symbols
 PUBLIC :: COMPRESSION_RUNTIME_INIT
 PUBLIC :: COMPRESSION_RUNTIME_FREE
@@ -48,10 +45,6 @@ IMPLICIT NONE
   TYPE(FCKIT_CONFIGURATION), INTENT(IN) :: CFG
   TYPE(MODEL_PAR_T), TARGET, INTENT(IN) :: MODEL_PARAMS
 
-  ! Local variables
-  TYPE(FCKIT_CONFIGURATION) :: COMPRESSION_RUNTIME_CFG
-  CHARACTER(LEN=:), ALLOCATABLE :: CLTMP
-
   ! Local variables declared by the preprocessor for debugging purposes
   PP_DEBUG_DECL_VARS
 
@@ -61,58 +54,10 @@ IMPLICIT NONE
   ! Trace begin of procedure
   PP_TRACE_ENTER_PROCEDURE()
 
-  ! Default compression type
-  PACKING_TYPE = 'grid_simple'
-
-  ! Parse the configuration
-  IF ( CFG%GET( 'compression-runtime-utils', COMPRESSION_RUNTIME_CFG ) ) THEN
-
-    IF ( ALLOCATED(CLTMP) ) DEALLOCATE(CLTMP)
-    IF ( COMPRESSION_RUNTIME_CFG%GET( 'packing-type', CLTMP  ) ) THEN
-      PP_DEBUG_CRITICAL_COND_THROW( .NOT.ALLOCATED(CLTMP), 1 )
-      PP_DEBUG_CRITICAL_COND_THROW( LEN(CLTMP).GT.LEN(PACKING_TYPE), 2 )
-      PACKING_TYPE = TRIM(CLTMP)
-      IF ( ALLOCATED(CLTMP) ) DEALLOCATE( CLTMP )
-      CALL COMPRESSION_RUNTIME_CFG%FINAL()
-    ELSE
-      CALL COMPRESSION_RUNTIME_CFG%FINAL()
-      PACKING_TYPE = 'grid_simple'
-    ENDIF
-
-  ENDIF
   ! Trace end of procedure (on success)
   PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
 
   ! Exit point
-  RETURN
-
-! Error handler
-PP_ERROR_HANDLER
-
-  ErrorHandler: BLOCK
-
-    ! Error handling variables
-    CHARACTER(LEN=:), ALLOCATABLE :: STR
-
-    ! HAndle different errors
-    SELECT CASE(ERRIDX)
-    CASE (1)
-      PP_DEBUG_CREATE_ERROR_MSG( STR, 'Unable to read packing type' )
-    CASE (2)
-      PP_DEBUG_CREATE_ERROR_MSG( STR, 'PackingType too long' )
-    CASE DEFAULT
-      PP_DEBUG_CREATE_ERROR_MSG( STR, 'Unhandled error' )
-    END SELECT
-
-    ! Trace end of procedure (on error)
-    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
-
-    ! Write the error message and stop the program
-    PP_DEBUG_ABORT( STR )
-
-  END BLOCK ErrorHandler
-
-  ! Exit point on error
   RETURN
 
 END SUBROUTINE COMPRESSION_RUNTIME_INIT
@@ -159,8 +104,14 @@ SUBROUTINE SET_COMPRESSION_ATM( MODEL_PARAMS, GRIB_INFO, MSG, METADATA )
   ! Symbols imported from other modules within the project.
   USE :: OM_CORE_MOD,        ONLY: MODEL_PAR_T
   USE :: OM_CORE_MOD,        ONLY: OM_ATM_MSG_T
+  USE :: OM_CORE_MOD,        ONLY: PACKING_TYPE_GRIB_SIMPLE_E
+  USE :: OM_CORE_MOD,        ONLY: PACKING_TYPE_GRIB_CCSDE_E
+  USE :: OM_CORE_MOD,        ONLY: PACKING_TYPE_GRIB_COMPLEX_E
   USE :: GRIB_INFO_DATA_MOD, ONLY: GRIB_INFO_T
   USE :: METADATA_BASE_MOD,  ONLY: METADATA_BASE_A
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
 
   ! Symbols imported by the preprocessor for debugging purposes
   PP_DEBUG_USE_VARS
@@ -177,6 +128,9 @@ IMPLICIT NONE
   CLASS(METADATA_BASE_A), POINTER, INTENT(INOUT) :: METADATA
 
   ! Local variables declared by the preprocessor for debugging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for debugging purposes
   PP_DEBUG_DECL_VARS
 
   ! Local variables declared by the preprocessor for tracing purposes
@@ -186,8 +140,16 @@ IMPLICIT NONE
   PP_TRACE_ENTER_PROCEDURE()
   PP_METADATA_ENTER_PROCEDURE( METADATA )
 
-
-  PP_METADATA_SET( METADATA,  'packingType', TRIM(PACKING_TYPE) )
+  SELECT CASE (GRIB_INFO%PACKING_TYPE)
+  CASE (PACKING_TYPE_GRIB_SIMPLE_E)
+    PP_METADATA_SET( METADATA,  'packingType', 'grid_simple' )
+  CASE (PACKING_TYPE_GRIB_CCSDE_E)
+    PP_METADATA_SET( METADATA,  'packingType', 'grid_ccsds' )
+  CASE (PACKING_TYPE_GRIB_COMPLEX_E)
+    PP_METADATA_SET( METADATA, 'packingType', 'spectral_complex'  )
+  CASE DEFAULT
+    PP_LOG_DEVELOP_STR( 'ENCODER: warning invalid "packingType"' )
+  END SELECT
 
   ! Trace end of procedure (on success)
   PP_METADATA_EXIT_PROCEDURE( METADATA )
@@ -210,8 +172,14 @@ SUBROUTINE SET_COMPRESSION_WAM( MODEL_PARAMS, GRIB_INFO, MSG, METADATA )
   ! Symbols imported from other modules within the project.
   USE :: OM_CORE_MOD,        ONLY: MODEL_PAR_T
   USE :: OM_CORE_MOD,        ONLY: OM_WAM_MSG_T
+  USE :: OM_CORE_MOD,        ONLY: PACKING_TYPE_GRIB_SIMPLE_E
+  USE :: OM_CORE_MOD,        ONLY: PACKING_TYPE_GRIB_CCSDE_E
+  USE :: OM_CORE_MOD,        ONLY: PACKING_TYPE_GRIB_COMPLEX_E
   USE :: GRIB_INFO_DATA_MOD, ONLY: GRIB_INFO_T
   USE :: METADATA_BASE_MOD,  ONLY: METADATA_BASE_A
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
 
   ! Symbols imported by the preprocessor for debugging purposes
   PP_DEBUG_USE_VARS
@@ -228,6 +196,9 @@ IMPLICIT NONE
   CLASS(METADATA_BASE_A), POINTER, INTENT(INOUT) :: METADATA
 
   ! Local variables declared by the preprocessor for debugging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for debugging purposes
   PP_DEBUG_DECL_VARS
 
   ! Local variables declared by the preprocessor for tracing purposes
@@ -238,7 +209,16 @@ IMPLICIT NONE
   PP_METADATA_ENTER_PROCEDURE( METADATA )
 
   ! Type of level Need to be moved out from here
-  PP_METADATA_SET( METADATA,  'packingType', TRIM(PACKING_TYPE) )
+  SELECT CASE (GRIB_INFO%PACKING_TYPE)
+  CASE (PACKING_TYPE_GRIB_SIMPLE_E)
+    PP_METADATA_SET( METADATA,  'packingType', 'grid_simple' )
+  CASE (PACKING_TYPE_GRIB_CCSDE_E)
+    PP_METADATA_SET( METADATA,  'packingType', 'grid_ccsds' )
+  CASE (PACKING_TYPE_GRIB_COMPLEX_E)
+    PP_METADATA_SET( METADATA, 'packingType', 'spectral_complex'  )
+  CASE DEFAULT
+    PP_LOG_DEVELOP_STR( 'ENCODER: warning invalid "packingType"' )
+  END SELECT
 
   ! Trace end of procedure (on success)
   PP_METADATA_EXIT_PROCEDURE( METADATA )

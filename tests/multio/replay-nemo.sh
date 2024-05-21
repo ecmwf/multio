31e36ed5f77db9a128499327a626ff69bd2c7135
Philipp Geier <philipp.geier@ecmwf.int>
2024-05-21 10:59:35 +0200

    Test dependency matrix for ci-hpc

--- tests/multio/replay-nemo.sh
+++ tests/multio/replay-nemo.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# (C) Copyright 2022 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation nor
+# does it submit to any jurisdiction.
+
+
 client_binary=${1:-multio-replay}
 server_binary=${2:-multio-probe}
 mpi_executable=${3:-mpiexec}
 mpi_args=${4:- }
 
+
 function launch_mpi {
     local cmd="$mpi_executable $mpi_args -n 7 $client_binary : -n 2 $server_binary --test"
+    echo "Running MPI command: $cmd"
     eval $cmd
 }
 

3dbd102a41df4704a70de05d56e1d4fb9598c383
Razvan Aguridan <razvan.aguridan@ecmwf.int>
2024-08-09 10:21:06 +0000

    Fix multio python API tests

--- pymultio/tests/test_multio_encode.py
+++ pymultio/tests/test_multio_encode.py
@@ -13,42 +13,46 @@ def test_encode_stream_mmsf_1():
     dir = os.path.dirname(__file__)
 
     os.environ['MULTIO_PLANS'] = json.dumps({
+        "on-error": "propagate",
         "plans": [
-        {
-            "name": "Write grib",
-            "actions": [
-                { "type": "print",
-                  "stream": "cout",
-                  "prefix": " +++ ",
-                },
-                {
-                    "type": "encode",
-                    "format": "grib",
-                    "template": "{dir}/test.grib".format(dir=dir),
-                    "overwrite": [
-                        {"setLocalDefinition": 1},
-                        {"localDefinitionNumber": 15},
-                        {"productDefinitionTemplateNumber": 11}, # 11 for stat, 1 for instant
-                    ],
-                    "additional-metadata": {
-                        "stream": "mmsf",
-                        "type": "fc",
-                        "methodNumber": 4,
-                        "systemNumber": 3,
-                        "perturbationNumber": 2,
+            {
+                "name": "Write grib",
+                "actions": [
+                    {
+                        "type": "print",
+                        "stream": "cout",
+                        "prefix": " +++ ",
+                    },
+                    {
+                        "type": "encode",
+                        "format": "grib",
+                        "template": "{dir}/test.grib".format(dir=dir),
+                        "overwrite": [
+                            {"setLocalDefinition": 1},
+                            {"localDefinitionNumber": 15},
+                            {"productDefinitionTemplateNumber": 11}, # 11 for stat, 1 for instant
+                        ],
+                        "additional-metadata": {
+                            "stream": "mmsf",
+                            "type": "fc",
+                            "methodNumber": 4,
+                            "systemNumber": 3,
+                            "perturbationNumber": 2,
+                        }
+                    },
+                    {
+                        "type": "sink",
+                        "sinks": [
+                            {
+                                "type": "file",
+                                "append": False,
+                                "per-server": False,
+                                "path": "{dir}/testMMSF1.grib".format(dir=dir)
+                            }
+                        ]
                     }
-                },
-                {
-                    "type": "sink",
-                    "sinks": [{
-                        "type": "file",
-                        "append": False,
-                        "per-server": False,
-                        "path": "{dir}/testMMSF1.grib".format(dir=dir)
-                    }]
-                }
-            ]
-        }
+                ]
+            }
         ]
     })
 
@@ -59,7 +63,8 @@ def test_encode_stream_mmsf_1():
     # Opening the binary file in binary mode as rb(read binary)
 
     data = np.ones(1024, dtype=np.float64)
-    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1, "endStep": 2, "globalSize": len(data)})
+    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1,
+                                    "endStep": 2, "globalSize": len(data), "param": 228050})
     mio.flush()
 
     gribs = list(Reader("{dir}/testMMSF1.grib".format(dir=dir)))
@@ -82,6 +87,7 @@ def test_encode_stream_mmsf_2():
     dir = os.path.dirname(__file__)
 
     os.environ['MULTIO_PLANS'] = json.dumps({
+        "on-error": "propagate",
         "plans": [
         {
             "name": "Write grib",
@@ -126,7 +132,8 @@ def test_encode_stream_mmsf_2():
     # Opening the binary file in binary mode as rb(read binary)
 
     data = np.ones(1024, dtype=np.float64)
-    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1, "endStep": 2, "globalSize": len(data)})
+    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1,
+                                    "endStep": 2, "globalSize": len(data), "param": 228050})
     mio.flush()
 
     gribs = list(Reader("{dir}/testMMSF2.grib".format(dir=dir)))
@@ -150,6 +157,7 @@ def test_encode_stream_msmm_1():
     dir = os.path.dirname(__file__)
 
     os.environ['MULTIO_PLANS'] = json.dumps({
+        "on-error": "propagate",
         "plans": [
         {
             "name": "Write grib",
@@ -196,7 +204,8 @@ def test_encode_stream_msmm_1():
     # Opening the binary file in binary mode as rb(read binary)
 
     data = np.ones(1024, dtype=np.float64)
-    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1, "endStep": 2, "globalSize": len(data)})
+    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1,
+                                    "endStep": 2, "globalSize": len(data), "param": 228050})
     mio.flush()
 
     gribs = list(Reader("{dir}/testMSMM1.grib".format(dir=dir)))
@@ -221,6 +230,7 @@ def test_encode_stream_lwda_1():
     dir = os.path.dirname(__file__)
 
     os.environ['MULTIO_PLANS'] = json.dumps({
+        "on-error": "propagate",
         "plans": [
         {
             "name": "Write grib",
@@ -266,7 +276,8 @@ def test_encode_stream_lwda_1():
     # Opening the binary file in binary mode as rb(read binary)
 
     data = np.ones(1024, dtype=np.float64)
-    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1, "endStep": 2, "globalSize": len(data)})
+    mio.write_field(data, metadata={"startDate": 20240101, "startTime": 1200, "startStep":1,
+                                    "endStep": 2, "globalSize": len(data), "param": 228050})
     mio.flush()
 
     gribs = list(Reader("{dir}/testLWDA1.grib".format(dir=dir)))

e505cbf1c6a733f1767ce1a0d00bc7c32e7db6e7
Mirco Valentini <mirco.valentini@ecmwf.int>
2024-12-17 03:12:46 +0000

    Fix better management of folder and enviroment in multiom test

---
+++ tests/multiom/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# General path
+HERE=/ec/res4/hpcperm/mavm/multiom
+cd ${HERE}
+
+# environment variables
+export MULTIO_INSTALL_DIR="${HERE}/install-debug-intel-2021.4.0-hpcx/share/multiom"
+export KNOWLEDGE_VERSION='49r2v9'
+export MULTIO_LOG_PATH="${HERE}/"
+
+
+# Call the feed
+${HERE}/install-debug-intel-2021.4.0-hpcx/bin/multiom-feed -t "GRIB-HEADER-TO-MULTIO" -i "/perm/mavm/ERAdata/dataset.00002/fc_init/dump.02/" -y "${MULTIO_INSTALL_DIR}/config/output_manager_cfg.yaml" -p 8
+

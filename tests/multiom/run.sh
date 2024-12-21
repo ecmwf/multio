a43961fc69bcdc3bb3b70f4a779b24440b907630
Mirco Valentini <mirco.valentini@ecmwf.int>
2024-12-21 15:34:48 +0000

    Fix updated test/install dir

--- tests/multiom/run.sh
+++ tests/multiom/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # General path
-HERE=/ec/res4/hpcperm/mavm/multiom
+HERE=/ec/res4/hpcperm/mavm/multiom-rules
 cd ${HERE}
 
 # environment variables
@@ -11,5 +11,5 @@ export MULTIO_LOG_PATH="${HERE}/"
 
 
 # Call the feed
-${HERE}/install-debug-intel-2021.4.0-hpcx/bin/multiom-feed -t "GRIB-HEADER-TO-MULTIO" -i "/perm/mavm/ERAdata/dataset.00002/fc_init/dump.02/" -y "${MULTIO_INSTALL_DIR}/config/output_manager_cfg.yaml" -p 8
+${HERE}/install-debug-intel-2021.4.0-hpcx/bin/multiom-feed -t "GRIB-HEADER-TO-MULTIO" -i "/perm/mavm/ERAdata/dataset.00002/fc_init/dump.02/" -y "${MULTIO_INSTALL_DIR}/output_manager_cfg.yaml" -p 8
 

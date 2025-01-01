7b0795d11b6e4236c2ba9c66355d583bab4c7b61
Mirco Valentini <mirco.valentini@ecmwf.int>
2025-01-01 05:15:19 +0000

    WIP: Resolved numerous complex challenges related to data interoperability between C and Fortran.
     - Implemented a second-layer interface for passing strings along with their lengths.
     - Utilized array handles to manage and track dictionary types.
     - Integrated a destructor (function pointer) into the maps to prevent memory leaks.
     - Developed test cases for the creation and destruction of dictionaries and encoders.
     - Investigated and resolved an unusual compiler behavior related to error units.
     - Designed custom maps to store non-interoperable objects efficiently

---
+++ src/multiom/test/test_api01_prog.c
@@ -0,0 +1,52 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "api.h"
+
+int main(){
+int ret;
+void* optdict;
+void* multio_grib2_01 = NULL;
+void* multio_grib2_02 = NULL;
+void* multio_grib2_03 = NULL;
+void* multio_grib2_04 = NULL;
+void* multio_grib2_05 = NULL;
+void* multio_grib2_06 = NULL;
+void* multio_grib2_07 = NULL;
+void* multio_grib2_08 = NULL;
+void* multio_grib2_09 = NULL;
+void* multio_grib2_10 = NULL;
+
+
+printf( "Welcome from c API (test01) %p\n", multio_grib2_01 );
+
+optdict = NULL;
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_01 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_02 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_03 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_04 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_05 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_06 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_07 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_08 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_09 );
+ret = multio_grib2_encoder_open( optdict, &multio_grib2_10 );
+
+printf( "Hello from c API (test01) %p\n", multio_grib2_01 );
+
+ret = multio_grib2_encoder_close( multio_grib2_10 );
+ret = multio_grib2_encoder_close( multio_grib2_09 );
+ret = multio_grib2_encoder_close( multio_grib2_08 );
+ret = multio_grib2_encoder_close( multio_grib2_07 );
+ret = multio_grib2_encoder_close( multio_grib2_06 );
+ret = multio_grib2_encoder_close( multio_grib2_05 );
+ret = multio_grib2_encoder_close( multio_grib2_04 );
+ret = multio_grib2_encoder_close( multio_grib2_03 );
+ret = multio_grib2_encoder_close( multio_grib2_02 );
+ret = multio_grib2_encoder_close( multio_grib2_01 );
+
+printf( "Goodbye from c API (test01) \n" );
+
+
+return 0;
+
+}
---
+++ src/multiom/test/test_api02_prog.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "api.h"
+
+int main(){
+int ret;
+void* dictionary_01 = NULL;
+void* dictionary_02 = NULL;
+void* dictionary_03 = NULL;
+
+printf( "Welcome from c API (test02) %p\n", dictionary_01 );
+
+ret = multio_grib2_dict_create( &dictionary_01, "mars" );
+ret = multio_grib2_dict_create( &dictionary_02, "parametrization" );
+// ret = multio_grib2_dict_create( &dictionary_03, "options" );
+
+
+printf( "Hello from c API (test01) \n" );
+
+ret = multio_grib2_dict_destroy( dictionary_02 );
+ret = multio_grib2_dict_destroy( dictionary_01 );
+// ret = multio_grib2_dict_create( &dictionary_03, "options" );
+
+printf( "Goodbye from c API (test02)\n" );
+
+return 0;
+
+}

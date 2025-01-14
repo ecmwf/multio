29265ce7bd7799cc897fd3e8de4521f3c8c2f8f2
Philipp Geier <philipp.geier@ecmwf.int>
2025-01-14 15:13:18 +0000

    MUL-146 MULTIOM - dictionary API setting MARS Keys

--- src/multiom/test/test_api03_prog.c
+++ src/multiom/test/test_api03_prog.c
@@ -14,6 +14,30 @@ ret = multio_grib2_dict_create( &dictionary_01, "mars" );
 printf( "Set from c API (test01) \n" );
 ret = multio_grib2_dict_set( dictionary_01, "stream", "oper" );
 ret = multio_grib2_dict_set( dictionary_01, "type", "fc" );
+ret = multio_grib2_dict_set( dictionary_01, "class", "rd" );
+ret = multio_grib2_dict_set( dictionary_01, "origin", "ecmf" );
+ret = multio_grib2_dict_set( dictionary_01, "anoffset", "1234" ); // TODO find a valid offset
+ret = multio_grib2_dict_set( dictionary_01, "packing", "ccst" ); 
+ret = multio_grib2_dict_set( dictionary_01, "number", "123" ); 
+ret = multio_grib2_dict_set( dictionary_01, "ident", "1" ); 
+ret = multio_grib2_dict_set( dictionary_01, "instrument", "2" ); 
+ret = multio_grib2_dict_set( dictionary_01, "channel", "3" ); 
+ret = multio_grib2_dict_set( dictionary_01, "paramType", "optical" ); 
+ret = multio_grib2_dict_set( dictionary_01, "chem", "1" );  // TODO find valid chem
+ret = multio_grib2_dict_set( dictionary_01, "param", "123" );
+ret = multio_grib2_dict_set( dictionary_01, "model", "123" );
+ret = multio_grib2_dict_set( dictionary_01, "levtype", "ml" );
+ret = multio_grib2_dict_set( dictionary_01, "levelist", "10" );
+ret = multio_grib2_dict_set( dictionary_01, "direction", "11" );
+ret = multio_grib2_dict_set( dictionary_01, "frequency", "12" );
+ret = multio_grib2_dict_set( dictionary_01, "date", "20220812" );
+ret = multio_grib2_dict_set( dictionary_01, "time", "800" ); // no starting 0, e.g. 0800 would not be valid
+ret = multio_grib2_dict_set( dictionary_01, "step", "8" );
+ret = multio_grib2_dict_set( dictionary_01, "repres", "gg" );
+ret = multio_grib2_dict_set( dictionary_01, "truncation", "3" ); // TODO find valid truncation
+ret = multio_grib2_dict_set( dictionary_01, "timeproc", "3" ); // TODO find valid timeproc
+ret = multio_grib2_dict_set( dictionary_01, "expver", "ABC" ); // TODO find valid expver
+ret = multio_grib2_dict_set( dictionary_01, "grid", "g" ); // TODO find valid grid
 
 printf( "Destroy from c API (test01) \n" );
 

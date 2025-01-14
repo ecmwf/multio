cf8af1603373acf972351a175d51f1b91876e5fc
Philipp Geier <philipp.geier@ecmwf.int>
2025-01-14 11:52:39 +0100

    MUL-152: Update Metadata.h

--- src/multio/message/Metadata.h
+++ src/multio/message/Metadata.h
@@ -51,7 +51,15 @@ public:
     using Iterator = typename MapType::iterator;
     using ConstIterator = typename MapType::const_iterator;
 
-    // Onlything we need to change to support lookups in parametrization
+    // The single place we need to change to support lookups in parametrization.
+    // Important: The non-const version will copy values from the global parametrization dictionary 
+    //            to the local one - It will always return an iterator to the local dictionary.
+    //            This allows values to be modified in the local object directly.
+    //        
+    //            The const version will return either an iterator to the local object or the 
+    //            global parametrization dictionary.
+    //            If keys are assumed to hold nested objects or large arrays which won't be modified anyway -
+    //            it is reasonable to explicitly const-cast the object to select the const lookup version.
     Iterator find(const KeyType& k) override;
     ConstIterator find(const KeyType& k) const override;
 

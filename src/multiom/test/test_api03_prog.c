c0bfdfc14be6fdd9d53d9eb92fd20cddb5d04e3a
Philipp Geier <philipp.geier@ecmwf.int>
2025-01-16 12:10:37 +0000

    MUL-147: WIP MULTIOM - dictionary API setting Parametrization Keys

--- src/multiom/test/test_api03_prog.c
+++ src/multiom/test/test_api03_prog.c
@@ -9,13 +9,14 @@ int main(){
 
 int ret;
 void* dictionary_01 = NULL;
+void* dictionary_02 = NULL;
 char* readVal = NULL;
 
 printf( "Welcome from c API (test03) %p\n", dictionary_01 );
 
 ret = multio_grib2_dict_create( &dictionary_01, "mars" );
 
-printf( "Set from c API (test01) \n" );
+printf( "Set from c API (test03 MARS keys) \n" );
 
 ret = multio_grib2_dict_set( dictionary_01, "stream", "oper" );
 ret = multio_grib2_dict_get( dictionary_01, "stream", &readVal );
@@ -174,6 +175,102 @@ free(readVal);
 printf( "Destroy from c API (test01) \n" );
 
 ret = multio_grib2_dict_destroy( dictionary_01 );
+
+dictionary_01 = NULL;
+
+
+ret = multio_grib2_dict_create( &dictionary_02, "parametrization" );
+
+printf( "Set from c API (test03 PARAMETRIZATION keys) \n" );
+
+ret = multio_grib2_dict_set( dictionary_02, "tablesversion", "42" );
+ret = multio_grib2_dict_get( dictionary_02, "tablesversion", &readVal );
+printf("tablesversion: %s\n", readVal);
+assert(strcmp(readVal, "42") == 0);
+free(readVal);
+ 
+ret = multio_grib2_dict_set( dictionary_02, "generatingprocessidentifier", "52" );
+ret = multio_grib2_dict_get( dictionary_02, "generatingprocessidentifier", &readVal );
+printf("generatingprocessidentifier: %s\n", readVal);
+assert(strcmp(readVal, "52") == 0);
+free(readVal);
+
+ret = multio_grib2_dict_set( dictionary_02, "initialstep", "2" );
+ret = multio_grib2_dict_get( dictionary_02, "initialstep", &readVal );
+printf("initialstep: %s\n", readVal);
+assert(strcmp(readVal, "2") == 0);
+free(readVal);
+
+ret = multio_grib2_dict_set( dictionary_02, "lengthoftimestepinseconds", "30" );
+ret = multio_grib2_dict_get( dictionary_02, "lengthoftimestepinseconds", &readVal );
+printf("lengthoftimestepinseconds: %s\n", readVal);
+assert(strcmp(readVal, "30") == 0);
+free(readVal);
+
+ret = multio_grib2_dict_set( dictionary_02, "lengthoftimerangeinseconds", "600" );
+ret = multio_grib2_dict_get( dictionary_02, "lengthoftimerangeinseconds", &readVal );
+printf("lengthoftimerangeinseconds: %s\n", readVal);
+assert(strcmp(readVal, "600") == 0);
+free(readVal);
+
+ret = multio_grib2_dict_set( dictionary_02, "valuesscalefactor", "1000.1" );
+ret = multio_grib2_dict_get( dictionary_02, "valuesscalefactor", &readVal );
+printf("valuesscalefactor: %s\n", readVal);
+assert(strcmp(readVal, "1000.1") == 0);
+free(readVal);
+
+
+ret = multio_grib2_dict_set( dictionary_02, "numberofmissingvalues", "1002" );
+ret = multio_grib2_dict_get( dictionary_02, "numberofmissingvalues", &readVal );
+printf("numberofmissingvalues: %s\n", readVal);
+assert(strcmp(readVal, "1002") == 0);
+free(readVal);
+
+
+ret = multio_grib2_dict_set( dictionary_02, "valueofmissingvalues", "99.99" );
+ret = multio_grib2_dict_get( dictionary_02, "valueofmissingvalues", &readVal );
+printf("valueofmissingvalues: %s\n", readVal);
+assert(strcmp(readVal, "99.99") == 0);
+free(readVal);
+
+
+ret = multio_grib2_dict_set( dictionary_02, "systemnumber", "99" );
+ret = multio_grib2_dict_get( dictionary_02, "systemnumber", &readVal );
+printf("systemnumber: %s\n", readVal);
+assert(strcmp(readVal, "99") == 0);
+free(readVal);
+
+
+ret = multio_grib2_dict_set( dictionary_02, "methodnumber", "98" );
+ret = multio_grib2_dict_get( dictionary_02, "methodnumber", &readVal );
+printf("methodnumber: %s\n", readVal);
+assert(strcmp(readVal, "98") == 0);
+free(readVal);
+
+
+ret = multio_grib2_dict_set( dictionary_02, "typeofensembleforecast", "3" );
+ret = multio_grib2_dict_get( dictionary_02, "typeofensembleforecast", &readVal );
+printf("typeofensembleforecast: %s\n", readVal);
+assert(strcmp(readVal, "3") == 0);
+free(readVal);
+
+
+ret = multio_grib2_dict_set( dictionary_02, "lengthoftimewindow", "5" );
+ret = multio_grib2_dict_get( dictionary_02, "lengthoftimewindow", &readVal );
+printf("lengthoftimewindow: %s\n", readVal);
+assert(strcmp(readVal, "5") == 0);
+free(readVal);
+
+
+ret = multio_grib2_dict_set( dictionary_02, "bitspervalue", "16" );
+ret = multio_grib2_dict_get( dictionary_02, "bitspervalue", &readVal );
+printf("bitspervalue: %s\n", readVal);
+assert(strcmp(readVal, "16") == 0);
+free(readVal);
+
+printf( "Destroy from c API (test02) \n" );
+
+ret = multio_grib2_dict_destroy( dictionary_02 );
 
 
 printf( "Goodbye from c API (test03)\n" );

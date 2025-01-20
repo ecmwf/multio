d03996bfdbbc9707ebba29f91ebd1a76015d5cc0
Mirco Valentini <mirco.valentini@ecmwf.int>
2025-01-20 01:50:38 +0000

    ENH: Added tests and functionalities to the c-api dictionaries

--- src/multiom/api/c/api.h
+++ src/multiom/api/c/api.h
@@ -10,6 +10,8 @@ int multio_grib2_dict_iterate( void* dict, void** iterator, char** key, char** v
 int multio_grib2_dict_destroy_iterator( void* dict, void** iterator );
 int multio_grib2_dict_destroy( void** dict );
 
+int multio_grib2_dict_set_geometry( void* dict, const void* geom_dict );
+
 // Initialize all the options to a relevant default
 int multio_grib2_init_options( void** opt_dict );
 
@@ -18,4 +20,4 @@ int multio_grib2_encoder_open( void* opt_dict, void** multio_grib2 );
 int multio_grib2_encoder_extract_metadata( void* multio_grib2, grib_handle* grib, void** mars_dict, void** par_dict );
 int multio_grib2_encoder_encode64( void* multio_grib2, void* mars_dict, void* par_dict, double* data, size_t data_len, grib_handle* out_handle );
 int multio_grib2_encoder_encode32( void* multio_grib2, void* mars_dict, void* par_dict, float*  data, size_t data_len, grib_handle* out_handle );
-int multio_grib2_encoder_close( void* multio_grib2 );
+int multio_grib2_encoder_close( void** multio_grib2 );
---
+++ src/multiom/tests-api/test_api13_sh_dict_iterator_prog.c
@@ -0,0 +1,70 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "api.h"
+
+int main(){
+
+int ret;
+int has1;
+int has2;
+void* dictionary_01 = NULL;
+char* readVal = NULL;
+
+printf( "Welcome from c API (test03) %p\n", dictionary_01 );
+
+ret = multio_grib2_dict_create( &dictionary_01, "sh" );
+
+printf( "Set from c API (test03 reduced-gg keys) \n" );
+
+
+ret = multio_grib2_dict_has( dictionary_01, "pentagonal-resolution-j", &has1 );
+ret = multio_grib2_dict_set( dictionary_01, "pentagonal-resolution-j", "1" );
+ret = multio_grib2_dict_has( dictionary_01, "pentagonal-resolution-j", &has2 );
+ret = multio_grib2_dict_get( dictionary_01, "pentagonal-resolution-j", &readVal );
+printf("pentagonal-resolution-j: %s, %d, %d, %d\n", readVal, has1, has2, ret );
+assert(strcmp(readVal, "1") == 0);
+free(readVal); readVal=NULL;
+
+ret = multio_grib2_dict_has( dictionary_01, "pentagonal-resolution-k", &has1 );
+ret = multio_grib2_dict_set( dictionary_01, "pentagonal-resolution-k", "2" );
+ret = multio_grib2_dict_has( dictionary_01, "pentagonal-resolution-k", &has2 );
+ret = multio_grib2_dict_get( dictionary_01, "pentagonal-resolution-k", &readVal );
+printf("pentagonal-resolution-k': %s, %d, %d, %d\n", readVal, has1, has2, ret );
+assert(strcmp(readVal, "2") == 0);
+free(readVal); readVal=NULL;
+
+ret = multio_grib2_dict_has( dictionary_01, "pentagonal-resolution-m", &has1 );
+ret = multio_grib2_dict_set( dictionary_01, "pentagonal-resolution-m", "3" );
+ret = multio_grib2_dict_has( dictionary_01, "pentagonal-resolution-m", &has2 );
+ret = multio_grib2_dict_get( dictionary_01, "pentagonal-resolution-m", &readVal );
+printf("pentagonal-resolution-m: %s, %d, %d, %d\n", readVal, has1, has2, ret );
+assert(strcmp(readVal, "3") == 0);
+free(readVal); readVal=NULL;
+
+
+
+printf( " + iterate through the mars dictionary\n" );
+void* iterator = NULL;
+char* key = NULL;
+char* value = NULL;
+ret = multio_grib2_dict_iterate( dictionary_01, &iterator, &key, &value );
+while( iterator != NULL ) {
+  printf( "Iterate: \"%s\": \"%s\"\n", key, value );
+  free( key );
+  free( value );
+  ret = multio_grib2_dict_iterate( dictionary_01, &iterator, &key, &value );
+};
+
+printf( "Destroy from c API (test01) \n" );
+
+ret = multio_grib2_dict_destroy( &dictionary_01 );
+
+// Test that the destroyer reset also to NULL
+printf( "Goodbye from c API (test03) %p\n", dictionary_01 );
+
+return 0;
+
+}

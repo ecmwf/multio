f31e0992fd1a6cc98162efe1438896e69fa6369e
Mirco Valentini <mirco.valentini@ecmwf.int>
2025-01-31 04:57:21 +0000

    Add code to link the proper Fortran encoder to the C API

--- src/multiom/api/api_extract_metadata_c.cc
+++ src/multiom/api/api_extract_metadata_c.cc
@@ -18,7 +18,7 @@ namespace {
   bool hasKey(codes_handle* handle, const char* key) {
       return codes_is_defined(handle, key) != 0;
   }
-  
+
   std::string getString(codes_handle* handle, const char* key) {
       // char keyval[1024];
       // size_t keylen = sizeof(keyval);
@@ -32,40 +32,39 @@ namespace {
       // Now a second resize will only shrink (never enlarge) the buffer and just adjust the size without modifying the buffer
       ret.resize(keylen-1);
       std::cout << "Get string " << key << " (len: " << keylen << ", stringsize: " << ret.size() << ": " << std::string(ret) <<std::endl;
-      return ret;                              
+      return ret;
   }
 
   long getLong(codes_handle* handle, const char* key) {
       long ret;
       CODES_CHECK(codes_get_long(handle, key, &ret), nullptr);
-      return ret;                              
+      return ret;
   }
-  
+
   std::size_t getSize(codes_handle* handle, const char* key) {
       std::size_t ret;
       CODES_CHECK(codes_get_size(handle, key, &ret), nullptr);
-      return ret;                              
+      return ret;
   }
-  
+
   std::vector<double> getDoubleArray(codes_handle* handle, const char* key) {
       std::vector<double> ret;
       std::size_t size = getSize(handle, key);
       ret.resize(size);
       CODES_CHECK(codes_get_double_array(handle, key, ret.data(), &size), nullptr);
       ret.resize(size);
-      return ret;                              
+      return ret;
   }
-  
+
   std::vector<long> getLongArray(codes_handle* handle, const char* key) {
       std::vector<long> ret;
       std::size_t size = getSize(handle, key);
       ret.resize(size);
       CODES_CHECK(codes_get_long_array(handle, key, ret.data(), &size), nullptr);
       ret.resize(size);
-      return ret;                              
+      return ret;
   }
-  
-  
+
   int getAndSet(codes_handle* h, void* dict, const char* key, const char* setName=NULL, std::optional<std::string> defaultVal = {}) {
     if(hasKey(h, key)) {
         std::string val = getString(h, key);
@@ -77,7 +76,7 @@ namespace {
     }
     return 0;
   }
-  
+
   template<typename T>
   std::string arrayToJSONString(const std::vector<T>& arr) {
     std::ostringstream oss;
@@ -92,12 +91,12 @@ namespace {
         oss << v;
     }
     oss << "]";
-    
+
     std::cout << "Array: " << oss.str() << std::endl;
-    
+
     return oss.str();
   }
-  
+
   int getAndSetDoubleArray(codes_handle* h, void* dict, const char* key, const char* setName=NULL) {
     if(hasKey(h, key)) {
         int ret = multio_grib2_dict_set(dict, setName == NULL ? key : setName, arrayToJSONString(getDoubleArray(h, key)).data());
@@ -105,7 +104,7 @@ namespace {
     }
     return 0;
   }
-  
+
   int getAndSetLongArray(codes_handle* h, void* dict, const char* key, const char* setName=NULL) {
     if(hasKey(h, key)) {
         int ret = multio_grib2_dict_set(dict, setName == NULL ? key : setName, arrayToJSONString(getLongArray(h, key)).data());
@@ -113,40 +112,38 @@ namespace {
     }
     return 0;
   }
-  
+
   using GridTypeFunction = std::function<int(codes_handle*, void*)>;
-  
+
   int handleReducedGG(codes_handle* h, void* par_dict) {
       void* geom = NULL;
       int ret = multio_grib2_dict_create( &geom, "reduced-gg" );
-      
-      
+
+
       ret = getAndSet(h, geom, "truncateDegrees", "truncate-degrees");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "numberOfPointsALongAMeridian", "number-of-points-along-a-meridian");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "numberOfParallelsBetweenPoleAndEquator", "number-of-parallels-between-pole-and-equator");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "latitudeOfFirstGridPointInDegrees", "latitude-of-first-grid-point-in-degrees");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "longitudeOfFirstGridPointInDegrees", "longitude-of-first-grid-point-in-degrees");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "latitudeOfLastGridPointInDegrees", "latitude-of-last-grid-point-in-degrees");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "longitudeOfLastGridPointInDegrees", "longitude-of-last-grid-point-in-degrees");
       if(ret != 0) return ret;
-      
+
       ret = getAndSetLongArray(h, geom, "pl", "pl");
       if(ret != 0) return ret;
-      
-      
-      
+
       return multio_grib2_dict_set_geometry( par_dict, geom );
   }
 
@@ -157,17 +154,17 @@ namespace {
 
       ret = getAndSet(h, geom, "pentagonalResolutionParameterJ", "pentagonal-resolution-j");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "pentagonalResolutionParameterK", "pentagonal-resolution-k");
       if(ret != 0) return ret;
-      
+
       ret = getAndSet(h, geom, "pentagonalResolutionParameterM", "pentagonal-resolution-m");
       if(ret != 0) return ret;
 
 
       return multio_grib2_dict_set_geometry( par_dict, geom );
   }
-  
+
   int handleGridType(codes_handle* h, const std::string& gridType, void* par_dict) {
         const static std::unordered_map<std::string, GridTypeFunction> gridMap{
             { "reduced_gg", &handleReducedGG },
@@ -180,11 +177,11 @@ namespace {
         if (gridTypeFunc != gridMap.cend()) {
             return gridTypeFunc->second(h, par_dict);
         }
-        
-        std::cerr << "Unhandled gridType '" << gridType  << "'" << std::endl; 
+
+        std::cerr << "Unhandled gridType '" << gridType  << "'" << std::endl;
         return -1;
   };
-  
+
 }
 
 extern "C" {
@@ -192,30 +189,29 @@ extern "C" {
 int multio_grib2_encoder_extract_metadata(void* multio_grib2, void* grib, void** mars_dict, void** par_dict ) {
     int ret=0;
     codes_handle* h=NULL;
-    
+
     h = (codes_handle*) grib;
     printf("extract metadat...\n");
-    
+
     ret = multio_grib2_dict_create(mars_dict, "mars");
     if(ret != 0) return ret;
-    
+
     ret = multio_grib2_dict_create(par_dict, "parametrization");
     if(ret != 0) return ret;
-    
-    
-    // Handling mars keys 
+
+    // Handling mars keys
     ret = getAndSet(h, *mars_dict, "stream");
     if(ret != 0) return ret;
-    
+
     ret = getAndSet(h, *mars_dict, "type");
     if(ret != 0) return ret;
-    
+
     ret = getAndSet(h, *mars_dict, "class");
     if(ret != 0) return ret;
-    
+
     ret = getAndSet(h, *mars_dict, "origin");
     if(ret != 0) return ret;
-    
+
     ret = getAndSet(h, *mars_dict, "anoffest");
     if(ret != 0) return ret;
 
@@ -233,17 +229,17 @@ int multio_grib2_encoder_extract_metadata(void* multio_grib2, void* grib, void**
 
     ret = getAndSet(h, *mars_dict, "channel");
     if(ret != 0) return ret;
-    
+
     // TODO paramType is experimental
     ret = getAndSet(h, *mars_dict, "paramType");
     if(ret != 0) return ret;
 
     ret = getAndSet(h, *mars_dict, "chem");
     if(ret != 0) return ret;
-    
+
     ret = getAndSet(h, *mars_dict, "paramId");
     if(ret != 0) return ret;
-    
+
     ret = getAndSet(h, *mars_dict, "model");
     if(ret != 0) return ret;
 
@@ -288,7 +284,7 @@ int multio_grib2_encoder_extract_metadata(void* multio_grib2, void* grib, void**
 
 
 
-    // // Handling parametrization keys 
+    // Handling parametrization keys
     ret = getAndSet(h, *par_dict, "tablesversion");
     if(ret != 0) return ret;
 
@@ -354,23 +350,21 @@ int multio_grib2_encoder_extract_metadata(void* multio_grib2, void* grib, void**
 
     ret = getAndSet(h, *par_dict, "scaledvalueofcentralwavenumber");
     if(ret != 0) return ret;
-    
-    
+
     if(hasKey(h, "gridType")) {
         std::string gridType = getString(h, "gridType");
         std::cout << "Grid: " << gridType << std::endl;
-        
+
         ret = handleGridType(h, gridType, *par_dict);
         if(ret != 0) return ret;
     }
 
     // ret = multio_grib2_dict_set(*par_dict, "geometry" geometry);
     // if(ret != 0) return ret;
-    
-    return 0;    
+
+    return 0;
 }
-      
-      
+
 }
 
 

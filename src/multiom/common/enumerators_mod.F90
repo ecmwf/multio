2c825edabc60efe3d5fc558b7a5c574a8ed9c720
Mirco Valentini <mirco.valentini@ecmwf.int>
2025-02-06 15:32:31 +0000

    Re-enable the possibility of MultIO sinking binary messages to FDB

--- src/multiom/common/enumerators_mod.F90
+++ src/multiom/common/enumerators_mod.F90
@@ -547,6 +547,12 @@ IMPLICIT NONE
   INTEGER(KIND=JPIB_K), PARAMETER :: VALUES_SP_E=1_JPIB_K
   INTEGER(KIND=JPIB_K), PARAMETER :: VALUES_DP_E=2_JPIB_K
 
+  ! Sink types
+  INTEGER(KIND=JPIB_K), PARAMETER :: SINK_NONE_E=0_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: SINK_MESSAGE_TO_FILE_E=1_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: SINK_MESSAGE_TO_FDB_E=2_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: SINK_GRIB_HEADER_TO_MULTIO_E=3_JPIB_K
+
   !>
   !>
   !>  Whitelist of public symbols (parameters)
@@ -1126,6 +1132,14 @@ IMPLICIT NONE
   PUBLIC :: IINT2CINT
   PUBLIC :: CFLOAT2IFLOAT
   PUBLIC :: IFLOAT2CFLOAT
+  PUBLIC :: CSINK2ISINK
+  PUBLIC :: ISINK2CSINK
+
+  ! Sinks
+  PUBLIC :: SINK_NONE_E
+  PUBLIC :: SINK_MESSAGE_TO_FILE_E
+  PUBLIC :: SINK_MESSAGE_TO_FDB_E
+  PUBLIC :: SINK_GRIB_HEADER_TO_MULTIO_E
 
 CONTAINS
 
@@ -1593,6 +1607,241 @@ PP_ERROR_HANDLER
 END FUNCTION CIFACES2IIFACES
 #undef PP_PROCEDURE_NAME
 #undef PP_PROCEDURE_TYPE
+
+
+#define PP_PROCEDURE_TYPE 'FUNCTION'
+#define PP_PROCEDURE_NAME 'ISINK2CSINK'
+PP_THREAD_SAFE FUNCTION ISINK2CSINK( ISINK, CSINK, HOOKS ) RESULT(RET)
+
+  !> Symbols imported from other modules within the project.
+  USE :: DATAKINDS_DEF_MOD, ONLY: JPIB_K
+  USE :: HOOKS_MOD,         ONLY: HOOKS_T
+
+  ! Symbols imported by the preprocessor for debugging purposes
+  PP_DEBUG_USE_VARS
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
+
+  ! Symbols imported by the preprocessor for tracing purposes
+  PP_TRACE_USE_VARS
+
+IMPLICIT NONE
+
+  !> Dummy arguments
+  INTEGER(KIND=JPIB_K), INTENT(IN)    :: ISINK
+  CHARACTER(LEN=16),    INTENT(OUT)   :: CSINK
+  TYPE(HOOKS_T),        INTENT(INOUT) :: HOOKS
+
+  !> Function result
+  INTEGER(KIND=JPIB_K) :: RET
+
+  !> Local error codes
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNKNOWN_PREFIX=1_JPIB_K
+
+  ! Local variables declared by the preprocessor for debugging purposes
+  PP_DEBUG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for logging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for tracing purposes
+  PP_TRACE_DECL_VARS
+
+  ! Trace begin of procedure
+  PP_TRACE_ENTER_PROCEDURE()
+
+  ! Initialization of good path return value
+  PP_SET_ERR_SUCCESS( RET )
+
+  !> Initialization of the output variable
+  CSINK = REPEAT(' ', 16)
+
+  !> Select the prefix
+  SELECT CASE ( ISINK )
+
+  CASE ( SINK_NONE_E )
+    CSINK = 'none'
+  CASE ( SINK_MESSAGE_TO_FILE_E )
+    CSINK = 'message-to-file'
+  CASE ( SINK_MESSAGE_TO_FDB_E )
+    CSINK = 'message-to-fdb'
+  CASE ( SINK_GRIB_HEADER_TO_MULTIO_E )
+    CSINK = 'header-to-multio'
+  CASE DEFAULT
+    PP_DEBUG_CRITICAL_THROW( ERRFLAG_UNKNOWN_PREFIX )
+  END SELECT
+
+  ! Trace end of procedure (on success)
+  PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
+
+  ! Exit point (On success)
+  RETURN
+
+! Error handler
+PP_ERROR_HANDLER
+
+  ! Initialization of bad path return value
+  PP_SET_ERR_FAILURE( RET )
+
+#if defined( PP_DEBUG_ENABLE_ERROR_HANDLING )
+!$omp critical(ERROR_HANDLER)
+
+  BLOCK
+    CHARACTER(LEN=16) :: TMPSTR
+
+    ! Error handling variables
+    PP_DEBUG_PUSH_FRAME()
+
+    ! Handle different errors
+    SELECT CASE(ERRIDX)
+    CASE (ERRFLAG_UNKNOWN_PREFIX)
+      TMPSTR = REPEAT(' ', 16)
+      WRITE(TMPSTR,*) ISINK
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unknown isink: '//TRIM(ADJUSTL(TMPSTR)) )
+    CASE DEFAULT
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unhandled error' )
+    END SELECT
+
+    ! Trace end of procedure (on error)
+    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
+
+    ! Write the error message and stop the program
+    PP_DEBUG_ABORT
+
+  END BLOCK
+
+!$omp end critical(ERROR_HANDLER)
+#endif
+
+  ! Exit point (on error)
+  RETURN
+
+END FUNCTION ISINK2CSINK
+#undef PP_PROCEDURE_NAME
+#undef PP_PROCEDURE_TYPE
+
+
+#define PP_PROCEDURE_TYPE 'FUNCTION'
+#define PP_PROCEDURE_NAME 'CSINK2ISINK'
+PP_THREAD_SAFE FUNCTION CSINK2ISINK( CSINK, ISINK, HOOKS ) RESULT(RET)
+
+  !> Symbols imported from other modules within the project.
+  USE :: DATAKINDS_DEF_MOD, ONLY: JPIB_K
+  USE :: HOOKS_MOD,         ONLY: HOOKS_T
+  USE :: GENERAL_UTILS_MOD, ONLY: TOLOWER
+
+  ! Symbols imported by the preprocessor for debugging purposes
+  PP_DEBUG_USE_VARS
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
+
+  ! Symbols imported by the preprocessor for tracing purposes
+  PP_TRACE_USE_VARS
+
+IMPLICIT NONE
+
+  !> Dummy arguments
+  CHARACTER(LEN=*),     INTENT(IN)    :: CSINK
+  INTEGER(KIND=JPIB_K), INTENT(OUT)   :: ISINK
+  TYPE(HOOKS_T),        INTENT(INOUT) :: HOOKS
+
+  !> Function result
+  INTEGER(KIND=JPIB_K) :: RET
+
+  !> Local variables
+  CHARACTER(LEN=LEN_TRIM(CSINK)) :: LOC_CSINK
+
+  !> Local error codes
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNKNOWN_PREFIX=1_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_TO_CONVERT_LC=2_JPIB_K
+
+  ! Local variables declared by the preprocessor for debugging purposes
+  PP_DEBUG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for logging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for tracing purposes
+  PP_TRACE_DECL_VARS
+
+  ! Trace begin of procedure
+  PP_TRACE_ENTER_PROCEDURE()
+
+  ! Initialization of good path return value
+  PP_SET_ERR_SUCCESS( RET )
+
+  !> Initialization of the output variable
+  ISINK = UNDEF_PARAM_E
+
+  !> Convert prefix to lowercase
+  PP_TRYCALL(ERRFLAG_UNABLE_TO_CONVERT_LC) TOLOWER( CSINK, LOC_CSINK, HOOKS )
+
+  !> Select the prefix
+  SELECT CASE ( TRIM(ADJUSTL(LOC_CSINK)) )
+
+  CASE ( 'none' )
+    ISINK = SINK_NONE_E
+  CASE ( 'message-to-file' )
+    ISINK = SINK_MESSAGE_TO_FILE_E
+  CASE ( 'message-to-fdb' )
+    ISINK = SINK_MESSAGE_TO_FDB_E
+  CASE ( 'header-to-multio' )
+    ISINK = SINK_GRIB_HEADER_TO_MULTIO_E
+  CASE DEFAULT
+    PP_DEBUG_CRITICAL_THROW( ERRFLAG_UNKNOWN_PREFIX )
+  END SELECT
+
+  ! Trace end of procedure (on success)
+  PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
+
+  ! Exit point (On success)
+  RETURN
+
+! Error handler
+PP_ERROR_HANDLER
+
+  ! Initialization of bad path return value
+  PP_SET_ERR_FAILURE( RET )
+
+#if defined( PP_DEBUG_ENABLE_ERROR_HANDLING )
+!$omp critical(ERROR_HANDLER)
+
+  BLOCK
+
+    ! Error handling variables
+    PP_DEBUG_PUSH_FRAME()
+
+    ! Handle different errors
+    SELECT CASE(ERRIDX)
+    CASE (ERRFLAG_UNABLE_TO_CONVERT_LC)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unable to convert to lowercase' )
+    CASE (ERRFLAG_UNKNOWN_PREFIX)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unknown csink: '//TRIM(ADJUSTL(CSINK)) )
+    CASE DEFAULT
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unhandled error' )
+    END SELECT
+
+    ! Trace end of procedure (on error)
+    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
+
+    ! Write the error message and stop the program
+    PP_DEBUG_ABORT
+
+  END BLOCK
+
+!$omp end critical(ERROR_HANDLER)
+#endif
+
+  ! Exit point (on error)
+  RETURN
+
+END FUNCTION CSINK2ISINK
+#undef PP_PROCEDURE_NAME
+#undef PP_PROCEDURE_TYPE
+
+
 
 
 #define PP_PROCEDURE_TYPE 'FUNCTION'

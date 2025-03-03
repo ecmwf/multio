7ffd7f2e55d0f42d2ad43bf213e1ee25ed74abc0
Philipp Geier <philipp.geier@ecmwf.int>
2025-03-03 10:16:14 +0000

    Add model configurator and set model + generatingProcessIdentifier

--- src/multiom/encoders-base/grib_encoder_lazy_rules_mod.F90
+++ src/multiom/encoders-base/grib_encoder_lazy_rules_mod.F90
@@ -16,6 +16,7 @@ PRIVATE
 
 ! Whitelist of public symbols
 PUBLIC :: PARAM_CONFIGURATOR_NAME
+PUBLIC :: MODEL_CONFIGURATOR_NAME
 PUBLIC :: LEVEL_CONFIGURATOR_NAME
 PUBLIC :: ENSEMBLE_CONFIGURATOR_NAME
 PUBLIC :: TIME_CONFIGURATOR_NAME
@@ -119,6 +120,107 @@ PP_ERROR_HANDLER
   RETURN
 
 END FUNCTION PARAM_CONFIGURATOR_NAME
+#undef PP_PROCEDURE_NAME
+#undef PP_PROCEDURE_TYPE
+
+
+#define PP_PROCEDURE_TYPE 'FUNCTION'
+#define PP_PROCEDURE_NAME 'MODEL_CONFIGURATOR_NAME'
+PP_THREAD_SAFE FUNCTION MODEL_CONFIGURATOR_NAME( &
+&               MSG, PAR, MODEL_NAME, OPT, HOOKS ) RESULT(RET)
+
+  !> Symbols imported from other modules within the project.
+  USE :: DATAKINDS_DEF_MOD,        ONLY: JPIB_K
+  USE :: GRIB_ENCODER_OPTIONS_MOD, ONLY: GRIB_ENCODER_OPTIONS_T
+  USE :: FORTRAN_MESSAGE_MOD,      ONLY: FORTRAN_MESSAGE_T
+  USE :: PARAMETRIZATION_MOD,      ONLY: PARAMETRIZATION_T
+  USE :: HOOKS_MOD,                ONLY: HOOKS_T
+
+  ! Symbols imported by the preprocessor for debugging purposes
+  PP_DEBUG_USE_VARS
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
+
+  ! Symbols imported by the preprocessor for tracing purposes
+  PP_TRACE_USE_VARS
+
+IMPLICIT NONE
+
+  !> Dummy arguments
+  TYPE(FORTRAN_MESSAGE_T),      INTENT(IN)    :: MSG
+  TYPE(PARAMETRIZATION_T),      INTENT(IN)    :: PAR
+  CHARACTER(LEN=*),             INTENT(OUT)   :: MODEL_NAME
+  TYPE(GRIB_ENCODER_OPTIONS_T), INTENT(IN)    :: OPT
+  TYPE(HOOKS_T),                INTENT(INOUT) :: HOOKS
+
+  !> Function result
+  INTEGER(KIND=JPIB_K) :: RET
+
+  !> Local error codes
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_NOT_IMPLEMENTED=1_JPIB_K
+
+  ! Local variables declared by the preprocessor for debugging purposes
+  PP_DEBUG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for logging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for tracing purposes
+  PP_TRACE_DECL_VARS
+
+  ! Trace begin of procedure
+  PP_TRACE_ENTER_PROCEDURE()
+
+  ! Initialization of good path return value
+  PP_SET_ERR_SUCCESS( RET )
+
+  !> Not implemented
+  PP_DEBUG_CRITICAL_THROW( ERRFLAG_NOT_IMPLEMENTED )
+
+  ! Trace end of procedure (on success)
+  PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
+
+  ! Exit point (On success)
+  RETURN
+
+! Error handler
+PP_ERROR_HANDLER
+
+  ! Initialization of bad path return value
+  PP_SET_ERR_FAILURE( RET )
+
+#if defined( PP_DEBUG_ENABLE_ERROR_HANDLING )
+!$omp critical(ERROR_HANDLER)
+
+  BLOCK
+
+    ! Error handling variables
+    PP_DEBUG_PUSH_FRAME()
+
+    ! Handle different errors
+    SELECT CASE(ERRIDX)
+    CASE (ERRFLAG_NOT_IMPLEMENTED)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'not implemented' )
+    CASE DEFAULT
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unhandled error' )
+    END SELECT
+
+    ! Trace end of procedure (on error)
+    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
+
+    ! Write the error message and stop the program
+    PP_DEBUG_ABORT
+
+  END BLOCK
+
+!$omp end critical(ERROR_HANDLER)
+#endif
+
+  ! Exit point (on error)
+  RETURN
+
+END FUNCTION MODEL_CONFIGURATOR_NAME
 #undef PP_PROCEDURE_NAME
 #undef PP_PROCEDURE_TYPE
 

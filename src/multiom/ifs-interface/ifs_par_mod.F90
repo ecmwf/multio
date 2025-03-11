89af3d86b1a023cbba42d5b71ad91c7a9f7b6b1c
Mirco Valentini <mirco.valentini@ecmwf.int>
2025-03-11 01:58:13 +0000

    Implement automatic endianness detection for dump files

--- src/multiom/ifs-interface/ifs_par_mod.F90
+++ src/multiom/ifs-interface/ifs_par_mod.F90
@@ -558,6 +558,7 @@ PUBLIC :: SAT_PAR_T
 
 ! Whitelist of public symbols (procedures)
 PUBLIC :: PAR_CREATE_NAME
+PUBLIC :: PAR_GET_ENDIANNES
 PUBLIC :: PAR_WOPEN
 PUBLIC :: PAR_ROPEN
 PUBLIC :: PAR_CLOSE
@@ -898,6 +899,156 @@ PP_ERROR_HANDLER
   RETURN
 
 END FUNCTION PAR_ROPEN
+#undef PP_PROCEDURE_NAME
+#undef PP_PROCEDURE_TYPE
+
+
+
+
+
+#define PP_PROCEDURE_TYPE 'FUNCTION'
+#define PP_PROCEDURE_NAME 'PAR_GET_ENDIANNES'
+PP_THREAD_SAFE FUNCTION PAR_GET_ENDIANNES( PARFNAME, BIG_ENDIAN_READ, HOOKS ) RESULT(RET)
+
+  ! Symbols imported from other modules within the project.
+  USE :: DATAKINDS_DEF_MOD, ONLY: JPIB_K
+  USE :: HOOKS_MOD,         ONLY: HOOKS_T
+
+  ! Symbols imported by the preprocessor for debugging purposes
+  PP_DEBUG_USE_VARS
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
+
+  ! Symbols imported by the preprocessor for tracing purposes
+  PP_TRACE_USE_VARS
+
+IMPLICIT NONE
+
+  ! Dummy arguments
+  CHARACTER(LEN=*),     INTENT(IN)    :: PARFNAME
+  LOGICAL,              INTENT(OUT)   :: BIG_ENDIAN_READ
+  TYPE(HOOKS_T),        INTENT(INOUT) :: HOOKS
+
+  ! Function result
+  INTEGER(KIND=JPIB_K) :: RET
+
+  ! Local variables
+  INTEGER(KIND=JPIB_K) :: PARUNIT
+  INTEGER(KIND=JPIB_K) :: TABLE_VERSION
+  LOGICAL :: PAREXIST
+  INTEGER(KIND=JPIB_K) :: STAT
+
+  !> Error flags
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_TO_FIND_PAR_FILE = 1_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_TO_OPEN_PAR_FILE = 2_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_TO_READ_TABLE_VERSION = 3_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_TO_CLOSE_PAR_FILE = 4_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_UNABLE_TO_DETERMINE_ENDIANNESS = 5_JPIB_K
+
+
+  ! Local variables declared by the preprocessor for debugging purposes
+  PP_DEBUG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for logging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for tracing purposes
+  PP_TRACE_DECL_VARS
+
+  ! Trace begin of procedure
+  PP_TRACE_ENTER_PROCEDURE()
+
+  ! Initialization of good path return value
+  PP_SET_ERR_SUCCESS( RET )
+
+  ! Check if the file is opened
+  INQUIRE( FILE=TRIM(PARFNAME), EXIST=PAREXIST )
+  PP_DEBUG_CRITICAL_COND_THROW( .NOT.PAREXIST, ERRFLAG_UNABLE_TO_FIND_PAR_FILE )
+
+  ! First guess is that the file is little endian
+  BIG_ENDIAN_READ = .FALSE.
+
+  ! Open the PAR file
+  PP_TRYCALL(ERRFLAG_UNABLE_TO_OPEN_PAR_FILE) PAR_ROPEN( PARFNAME, PARUNIT, BIG_ENDIAN_READ, HOOKS )
+
+  ! Read the first integer
+  PP_TRYCALL(ERRFLAG_UNABLE_TO_READ_TABLE_VERSION) READ_TABLE_VERSION_PAR( PARUNIT, TABLE_VERSION, HOOKS )
+
+  ! Close the PAR file
+  PP_TRYCALL(ERRFLAG_UNABLE_TO_CLOSE_PAR_FILE) PAR_CLOSE( PARUNIT, HOOKS )
+
+  ! Check if the table version is in the expected range
+  IF ( TABLE_VERSION.LT.0 .OR. TABLE_VERSION.GT.200 ) THEN
+
+    ! Update the endinaness
+    BIG_ENDIAN_READ = .TRUE.
+
+    ! Open the PAR file
+    PP_TRYCALL(ERRFLAG_UNABLE_TO_OPEN_PAR_FILE) PAR_ROPEN( PARFNAME, PARUNIT, BIG_ENDIAN_READ, HOOKS )
+
+    ! Read the first integer
+    PP_TRYCALL(ERRFLAG_UNABLE_TO_READ_TABLE_VERSION) READ_TABLE_VERSION_PAR( PARUNIT, TABLE_VERSION, HOOKS )
+
+    ! Close the PAR file
+    PP_TRYCALL(ERRFLAG_UNABLE_TO_CLOSE_PAR_FILE) PAR_CLOSE( PARUNIT, HOOKS )
+
+    ! Check if the table version is in the expected range
+    PP_DEBUG_CRITICAL_COND_THROW( TABLE_VERSION.LT.0 .OR. TABLE_VERSION.GT.200, ERRFLAG_UNABLE_UNABLE_TO_DETERMINE_ENDIANNESS )
+
+  ENDIF
+
+  ! Trace end of procedure (on success)
+  PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
+
+  ! Exit point on success
+  RETURN
+
+! Error handler
+PP_ERROR_HANDLER
+
+  ! Initialization of bad path return value
+  PP_SET_ERR_FAILURE( RET )
+
+#if defined( PP_DEBUG_ENABLE_ERROR_HANDLING )
+!$omp critical(ERROR_HANDLER)
+
+  BLOCK
+
+    ! Error handling variables
+    PP_DEBUG_PUSH_FRAME()
+
+    ! HAndle different errors
+    SELECT CASE(ERRIDX)
+    CASE (ERRFLAG_UNABLE_TO_FIND_PAR_FILE)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unable to find par file: '//TRIM(PARFNAME) )
+    CASE (ERRFLAG_UNABLE_TO_OPEN_PAR_FILE)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unable to open par file: '//TRIM(PARFNAME) )
+    CASE (ERRFLAG_UNABLE_TO_READ_TABLE_VERSION)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unable to read table version' )
+    CASE (ERRFLAG_UNABLE_TO_CLOSE_PAR_FILE)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unable to close par file' )
+    CASE (ERRFLAG_UNABLE_UNABLE_TO_DETERMINE_ENDIANNESS)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unable to determine endianness' )
+    CASE DEFAULT
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unhandled error' )
+    END SELECT
+
+    ! Trace end of procedure (on error)
+    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
+
+    ! Write the error message and stop the program
+    PP_DEBUG_ABORT
+
+  END BLOCK
+
+!$omp end critical(ERROR_HANDLER)
+#endif
+
+  ! Exit point (on error)
+  RETURN
+
+END FUNCTION PAR_GET_ENDIANNES
 #undef PP_PROCEDURE_NAME
 #undef PP_PROCEDURE_TYPE
 
@@ -1721,6 +1872,113 @@ PP_ERROR_HANDLER
 END FUNCTION WRITE_SIM_PAR
 #undef PP_PROCEDURE_NAME
 #undef PP_PROCEDURE_TYPE
+
+
+
+#define PP_PROCEDURE_TYPE 'FUNCTION'
+#define PP_PROCEDURE_NAME 'READ_TABLE_VERSION_PAR'
+PP_THREAD_SAFE FUNCTION READ_TABLE_VERSION_PAR( UNIT, TABLE_VERSION, HOOKS ) RESULT(RET)
+
+  ! Symbolds imported from intrinsic modules
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: INT64
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL64
+
+  ! Symbols imported from other modules within the project.
+  USE :: DATAKINDS_DEF_MOD, ONLY: JPIB_K
+  USE :: HOOKS_MOD,         ONLY: HOOKS_T
+
+  ! Symbols imported by the preprocessor for debugging purposes
+  PP_DEBUG_USE_VARS
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
+
+  ! Symbols imported by the preprocessor for tracing purposes
+  PP_TRACE_USE_VARS
+
+IMPLICIT NONE
+
+  ! Dummy arguments
+  INTEGER(KIND=JPIB_K), INTENT(IN)    :: UNIT
+  INTEGER(KIND=JPIB_K), INTENT(OUT)   :: TABLE_VERSION
+  TYPE(HOOKS_T),        INTENT(INOUT) :: HOOKS
+
+  ! Function result
+  INTEGER(KIND=JPIB_K) :: RET
+
+  ! Local variables
+  INTEGER(KIND=INT64)  :: ITMP
+  INTEGER(KIND=JPIB_K) :: STAT
+
+  !> Error flags
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_TO_READ = 1_JPIB_K
+
+  ! Local variables declared by the preprocessor for debugging purposes
+  PP_DEBUG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for logging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for tracing purposes
+  PP_TRACE_DECL_VARS
+
+  ! Trace begin of procedure
+  PP_TRACE_ENTER_PROCEDURE()
+
+  ! Initialization of good path return value
+  PP_SET_ERR_SUCCESS( RET )
+
+  ! Read the table version
+  READ(UNIT, IOSTAT=STAT) ITMP
+  PP_DEBUG_DEVELOP_COND_THROW( STAT.NE.0, ERRFLAG_UNABLE_TO_READ )
+  TABLE_VERSION = INT( ITMP, KIND(TABLE_VERSION))
+
+  ! Trace end of procedure (on success)
+  PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
+
+  ! Exit point on success
+  RETURN
+
+! Error handler
+PP_ERROR_HANDLER
+
+  ! Initialization of bad path return value
+  PP_SET_ERR_FAILURE( RET )
+
+#if defined( PP_DEBUG_ENABLE_ERROR_HANDLING )
+!$omp critical(ERROR_HANDLER)
+
+  BLOCK
+
+    ! Error handling variables
+    PP_DEBUG_PUSH_FRAME()
+
+    ! Handle different errors
+    SELECT CASE(ERRIDX)
+    CASE (ERRFLAG_UNABLE_TO_READ)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unable to Read simulation parameters' )
+    CASE DEFAULT
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'Unhandled error' )
+    END SELECT
+
+    ! Trace end of procedure (on error)
+    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
+
+    ! Write the error message and stop the program
+    PP_DEBUG_ABORT
+
+  END BLOCK
+
+!$omp end critical(ERROR_HANDLER)
+#endif
+
+  ! Exit point (on error)
+  RETURN
+
+END FUNCTION READ_TABLE_VERSION_PAR
+#undef PP_PROCEDURE_NAME
+#undef PP_PROCEDURE_TYPE
+
 
 
 

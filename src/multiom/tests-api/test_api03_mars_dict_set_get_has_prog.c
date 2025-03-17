63ec73207f6f5d306af4dc96ac133f369dbaeb0c
Philipp Geier <philipp.geier@ecmwf.int>
2025-03-17 16:52:31 +0000

    MUL-182: Add PDT 60 & 61 (reforecast) and handle mars key hdate

--- src/multiom/tests-api/test_api03_mars_dict_set_get_has_prog.c
+++ src/multiom/tests-api/test_api03_mars_dict_set_get_has_prog.c
@@ -197,6 +197,16 @@ int main() {
     free(readVal);
     readVal = NULL;
 
+
+    ret = multio_grib2_dict_has(dictionary_01, "hdate", &has1);
+    ret = multio_grib2_dict_set(dictionary_01, "hdate", "20230913");
+    ret = multio_grib2_dict_has(dictionary_01, "hdate", &has2);
+    ret = multio_grib2_dict_get(dictionary_01, "hdate", &readVal);
+    printf("hdate: %s, %d, %d, %d\n", readVal, has1, has2, ret);
+    assert(strcmp(readVal, "20230913") == 0);
+    free(readVal);
+    readVal = NULL;
+
     ret = multio_grib2_dict_has(dictionary_01, "time", &has1);     // no starting 0, e.g. 0800 would not be valid
     ret = multio_grib2_dict_set(dictionary_01, "time", "800");     // no starting 0, e.g. 0800 would not be valid
     ret = multio_grib2_dict_has(dictionary_01, "time", &has2);     // no starting 0, e.g. 0800 would not be valid

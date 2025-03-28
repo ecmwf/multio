db85dfa5e90942496666c2de71f622c886959892
Domokos Sármány <domokos.sarmany@ecmwf.int>
2025-03-28 14:35:15 +0000

    Update C API exposed to python

--- python/pymultio/src/multio/processed_multio.h
+++ python/pymultio/src/multio/processed_multio.h
@@ -15,26 +15,31 @@ enum MultioErrorValues
 };
 const char* multio_error_string(int err);
 const char* multio_error_string_info(int err, multio_failure_info_t* info);
+
 typedef void (*multio_failure_handler_t)(void*, int error_code, multio_failure_info_t* info);
-int multio_config_set_failure_handler(multio_configuration_t* config, multio_failure_handler_t handler,
-                                      void* usercontext);
+
 int multio_initialise();
 int multio_version(const char** version);
 int multio_vcs_version(const char** sha1);
+
 int multio_new_configuration(multio_configuration_t** cc);
 int multio_new_configuration_from_filename(multio_configuration_t** cc, const char* configuration_file_name);
 int multio_delete_configuration(multio_configuration_t* cc);
 int multio_config_set_path(multio_configuration_t* cc, const char* configuration_path);
+int multio_config_set_failure_handler(multio_configuration_t* config, multio_failure_handler_t handler,
+                                      void* usercontext);
+
 int multio_mpi_allow_world_default_comm(multio_configuration_t* cc, bool allow);
 int multio_mpi_parent_comm(multio_configuration_t* cc, int parent_comm);
 int multio_mpi_return_client_comm(multio_configuration_t* cc, int* return_client_comm);
 int multio_mpi_return_server_comm(multio_configuration_t* cc, int* return_server_comm);
+
 int multio_new_handle(multio_handle_t** mio, multio_configuration_t* cc);
-int multio_copy_handle(multio_handle_t** mio, multio_handle_t* mdFrom);
 int multio_delete_handle(multio_handle_t* mio);
-int multio_start_server(multio_configuration_t* cc);
 int multio_open_connections(multio_handle_t* mio);
 int multio_close_connections(multio_handle_t* mio);
+
+int multio_start_server(multio_configuration_t* cc);
 int multio_flush(multio_handle_t* mio, multio_metadata_t* md);
 int multio_notify(multio_handle_t* mio, multio_metadata_t* md);
 int multio_write_domain(multio_handle_t* mio, multio_metadata_t* md, int* data, int size);
@@ -43,13 +48,12 @@ int multio_write_mask_double(multio_handle_t* mio, multio_metadata_t* md, const 
 int multio_write_field_float(multio_handle_t* mio, multio_metadata_t* md, const float* data, int size);
 int multio_write_field_double(multio_handle_t* mio, multio_metadata_t* md, const double* data, int size);
 int multio_write_grib_encoded(multio_handle_t* mio, void* data, int size);
+int multio_field_accepted(multio_handle_t* mio, const multio_metadata_t* md, bool* accepted);
+
 int multio_new_metadata(multio_metadata_t** md, multio_handle_t* mio);
+int multio_copy_metadata(multio_metadata_t** md, multio_metadata_t* mdFrom);
 int multio_delete_metadata(multio_metadata_t* md);
 int multio_metadata_set_int(multio_metadata_t* md, const char* key, int value);
-int multio_metadata_set_long(multio_metadata_t* md, const char* key, long value);
-int multio_metadata_set_longlong(multio_metadata_t* md, const char* key, long long value);
 int multio_metadata_set_string(multio_metadata_t* md, const char* key, const char* value);
 int multio_metadata_set_bool(multio_metadata_t* md, const char* key, bool value);
-int multio_metadata_set_float(multio_metadata_t* md, const char* key, float value);
 int multio_metadata_set_double(multio_metadata_t* md, const char* key, double value);
-int multio_field_accepted(multio_handle_t* mio, const multio_metadata_t* md, bool* accepted);

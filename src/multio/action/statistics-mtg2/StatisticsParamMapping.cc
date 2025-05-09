22c9328749aaa9530119599bfe69af871c87bf39
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-05-09 12:02:21 +0000

    MUL-217: Replace singleton pattern with factory pattern

--- src/multio/action/statistics-mtg2/StatisticsParamMapping.cc
+++ src/multio/action/statistics-mtg2/StatisticsParamMapping.cc
@@ -14,25 +14,30 @@ namespace multio::action::statistics_mtg2 {
 using message::glossary;
 
 
-StatisticsParamMapping::StatisticsParamMapping() {
+StatisticsParamMapping::StatisticsParamMapping(ParamTypeOfStatisticalProcessingToParamMap paramMappings) :
+    paramMappings_{paramMappings} {}
+
+StatisticsParamMapping StatisticsParamMapping::makeStatisticsParamMapping() {
     // TODO : This path might not always be correct?
     eckit::LocalConfiguration mappingConf{eckit::YAMLConfiguration{eckit::PathName{
         multio::LibMultio::instance().libraryHome() +
         "/multio/share/multio/statistics-mtg2/statistics_param_mappings.yml"
     }}};
 
+    ParamTypeOfStatisticalProcessingToParamMap paramMappings;
     for (auto& mappings : mappingConf.getSubConfigurations()) {
         auto paramIn = mappings.getInt64("paramIn");
         for (auto& mapping : mappings.getSubConfigurations("mappings")) {
             auto typeOfStatisticalProcessing = mapping.getInt64("typeOfStatisticalProcessing");
             auto paramOut = mapping.getInt64("paramOut");
-            mapping_[{paramIn, typeOfStatisticalProcessing}] = paramOut;
+            paramMappings[{paramIn, typeOfStatisticalProcessing}] = paramOut;
         }
     }
+    return StatisticsParamMapping(paramMappings);
 }
 
-std::int64_t StatisticsParamMapping::getMapping(std::int64_t param, std::int64_t typeOfStatisticalProcessing) {
-    if (auto search = mapping_.find({param, typeOfStatisticalProcessing}); search != mapping_.end()) {
+std::int64_t StatisticsParamMapping::getMapping(std::int64_t param, std::int64_t typeOfStatisticalProcessing) const {
+    if (auto search = paramMappings_.find({param, typeOfStatisticalProcessing}); search != paramMappings_.end()) {
         return search->second;
     }
 
@@ -41,7 +46,7 @@ std::int64_t StatisticsParamMapping::getMapping(std::int64_t param, std::int64_t
     throw eckit::SeriousBug(os.str() , Here());
 }
 
-void StatisticsParamMapping::applyMapping(message::Metadata& metadata, std::int64_t typeOfStatisticalProcessing) {
+void StatisticsParamMapping::applyMapping(message::Metadata& metadata, std::int64_t typeOfStatisticalProcessing) const {
     if (auto paramOld = metadata.getOpt<std::int64_t>(glossary().param); paramOld) {
         auto paramNew = getMapping(*paramOld, typeOfStatisticalProcessing);
         metadata.set(glossary().param, paramNew);
@@ -59,24 +64,11 @@ const std::map<const std::string, const std::int64_t> opname_to_typeOfStatistica
 
 // NOTE : This function will not apply a mapping, nor throw an exception, if the operation is not an official
 //        type of statistical processing as defined in table 4.10.
-void StatisticsParamMapping::applyMapping(message::Metadata& metadata, const std::string& opname) {
+void StatisticsParamMapping::applyMapping(message::Metadata& metadata, const std::string& opname) const {
     if (auto search = opname_to_typeOfStatisticalProcessing.find(opname); search != opname_to_typeOfStatisticalProcessing.end()) {
         applyMapping(metadata, search->second);
     }
 }
-
-StatisticsParamMapping* StatisticsParamMapping::instance() {
-    if (instance_ == nullptr) {
-        instance_ = new StatisticsParamMapping();
-    }
-    return instance_;
-}
-
-StatisticsParamMapping* paramMapping() {
-    return StatisticsParamMapping::instance();
-}
-
-StatisticsParamMapping* StatisticsParamMapping::instance_ = nullptr;
 
 
 }  // namespace multio::action::statistics_mtg2

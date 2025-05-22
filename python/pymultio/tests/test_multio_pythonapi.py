963186983b0f169ce469b621392c64467433ed69
Domokos Sármány <domokos.sarmany@ecmwf.int>
2025-05-22 22:19:38 +0100

    Bump version number to 2.6.0

--- python/pymultio/tests/test_multio_pythonapi.py
+++ python/pymultio/tests/test_multio_pythonapi.py
@@ -45,7 +45,7 @@ def test_initialisation():
 
 
 def test_multio_version():
-    assert multio.Multio(**default_dict).__version__() == "2.5.0"
+    assert multio.Multio(**default_dict).__version__() == "2.6.0"
 
 
 def test_initialisation_no_config():

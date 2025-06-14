8338f0e21091a6a003b44328eb9dc68ca497f8a2
Mirco Valentini <mirco.valentini@ecmwf.int>
2025-06-14 14:23:54 +0000

    Add emuerators for the HEALPix ordering convention

--- src/multiom/common/enumerators_mod.F90
+++ src/multiom/common/enumerators_mod.F90
@@ -18,6 +18,11 @@ IMPLICIT NONE
   ! Default visibility
   PRIVATE
 
+  ! Enumerators for ordering convention
+  INTEGER(KIND=JPIB_K), PARAMETER :: ORDERING_CONVENTION_RING_E=1_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: ORDERING_CONVENTION_NESTED_E=2_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: N_ORDERING_CONVENTION_E=2_JPIB_K
+
   ! Enumerator for options
   INTEGER(KIND=JPIB_K), PARAMETER :: OPT_CACHE_NONE_E=1_JPIB_K
   INTEGER(KIND=JPIB_K), PARAMETER :: OPT_CACHE_INTEMEDIATE_E=2_JPIB_K
@@ -652,6 +657,11 @@ END INTERFACE
   PUBLIC :: VALUES_SP_E
   PUBLIC :: VALUES_DP_E
 
+  ! Ordering conventions
+  PUBLIC :: ORDERING_CONVENTION_RING_E
+  PUBLIC :: ORDERING_CONVENTION_NESTED_E
+  PUBLIC :: N_ORDERING_CONVENTION_E
+
 
   ! Cache options
   PUBLIC :: OPT_CACHE_NONE_E
@@ -1246,6 +1256,8 @@ END INTERFACE
   !>  Whitelist of public symbols (procedures)
   PUBLIC :: ITYPE_OF_PROCESSED_DATA2CTYPE_OF_PROCESSED_DATA
   PUBLIC :: CTYPE_OF_PROCESSED_DATA2ITYPE_OF_PROCESSED_DATA
+  PUBLIC :: IORDERING_CONVENTION2CORDERING_CONVENTION
+  PUBLIC :: CORDERING_CONVENTION2IORDERING_CONVENTION
   PUBLIC :: IPREFIX2CPREFIX
   PUBLIC :: CPREFIX2IPREFIX
   PUBLIC :: ILEVTYPE2CLEVTYPE
@@ -2547,6 +2559,233 @@ END FUNCTION CLEVTYPE2ILEVTYPE
 #undef PP_PROCEDURE_NAME
 #undef PP_PROCEDURE_TYPE
 
+
+
+
+
+
+#define PP_PROCEDURE_TYPE 'FUNCTION'
+#define PP_PROCEDURE_NAME 'IORDERING_CONVENTION2CORDERING_CONVENTION'
+PP_THREAD_SAFE FUNCTION IORDERING_CONVENTION2CORDERING_CONVENTION( IORDERING_CONVENTION, CORDERING_CONVENTION, HOOKS ) RESULT(RET)
+
+  !> Symbols imported from other modules within the project.
+  USE :: DATAKINDS_DEF_MOD, ONLY: JPIB_K
+  USE :: HOOKS_MOD,         ONLY: HOOKS_T
+
+  ! Symbols imported by the preprocessor for debugging purposes
+  PP_DEBUG_USE_VARS
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
+
+  ! Symbols imported by the preprocessor for tracing purposes
+  PP_TRACE_USE_VARS
+
+IMPLICIT NONE
+
+  !> Dummy arguments
+  INTEGER(KIND=JPIB_K), INTENT(IN)    :: IORDERING_CONVENTION
+  CHARACTER(LEN=16),    INTENT(OUT)   :: CORDERING_CONVENTION
+  TYPE(HOOKS_T),        INTENT(INOUT) :: HOOKS
+
+  !> Function result
+  INTEGER(KIND=JPIB_K) :: RET
+
+  !> Local error codes
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNKNOWN_ORDERING_CONVENTION=1_JPIB_K
+
+  ! Local variables declared by the preprocessor for debugging purposes
+  PP_DEBUG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for logging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for tracing purposes
+  PP_TRACE_DECL_VARS
+
+  ! Trace begin of procedure
+  PP_TRACE_ENTER_PROCEDURE()
+
+  ! Initialization of good path return value
+  PP_SET_ERR_SUCCESS( RET )
+
+  !> Initialization of the output variable
+  CORDERING_CONVENTION = REPEAT(' ', 16)
+
+  !> Select the prefix
+  SELECT CASE ( IORDERING_CONVENTION )
+
+  CASE ( ORDERING_CONVENTION_RING_E )
+    CORDERING_CONVENTION = 'ring'
+  CASE ( ORDERING_CONVENTION_NESTED_E )
+    CORDERING_CONVENTION = 'nested'
+  CASE DEFAULT
+    PP_DEBUG_CRITICAL_THROW( ERRFLAG_UNKNOWN_ORDERING_CONVENTION )
+  END SELECT
+
+  ! Trace end of procedure (on success)
+  PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
+
+  ! Exit point (On success)
+  RETURN
+
+! Error handler
+PP_ERROR_HANDLER
+
+  ! Initialization of bad path return value
+  PP_SET_ERR_FAILURE( RET )
+
+#if defined( PP_DEBUG_ENABLE_ERROR_HANDLING )
+!$omp critical(ERROR_HANDLER)
+
+  BLOCK
+    CHARACTER(LEN=16) :: TMPSTR
+
+    ! Error handling variables
+    PP_DEBUG_PUSH_FRAME()
+
+    ! Handle different errors
+    SELECT CASE(ERRIDX)
+    CASE (ERRFLAG_UNKNOWN_ORDERING_CONVENTION)
+      TMPSTR = REPEAT(' ', 16)
+      WRITE(TMPSTR,*) IORDERING_CONVENTION
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unknown HEALPix ordering convention: '//TRIM(ADJUSTL(TMPSTR)) )
+    CASE DEFAULT
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unhandled error' )
+    END SELECT
+
+    ! Trace end of procedure (on error)
+    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
+
+    ! Write the error message and stop the program
+    PP_DEBUG_ABORT
+
+  END BLOCK
+
+!$omp end critical(ERROR_HANDLER)
+#endif
+
+  ! Exit point (on error)
+  RETURN
+
+END FUNCTION IORDERING_CONVENTION2CORDERING_CONVENTION
+#undef PP_PROCEDURE_NAME
+#undef PP_PROCEDURE_TYPE
+
+
+#define PP_PROCEDURE_TYPE 'FUNCTION'
+#define PP_PROCEDURE_NAME 'CORDERING_CONVENTION2IORDERING_CONVENTION'
+PP_THREAD_SAFE FUNCTION CORDERING_CONVENTION2IORDERING_CONVENTION( CORDERING_CONVENTION, IORDERING_CONVENTION, HOOKS ) RESULT(RET)
+
+  !> Symbols imported from other modules within the project.
+  USE :: DATAKINDS_DEF_MOD, ONLY: JPIB_K
+  USE :: HOOKS_MOD,         ONLY: HOOKS_T
+  USE :: GENERAL_UTILS_MOD, ONLY: TOLOWER
+
+  ! Symbols imported by the preprocessor for debugging purposes
+  PP_DEBUG_USE_VARS
+
+  ! Symbols imported by the preprocessor for logging purposes
+  PP_LOG_USE_VARS
+
+  ! Symbols imported by the preprocessor for tracing purposes
+  PP_TRACE_USE_VARS
+
+IMPLICIT NONE
+
+  !> Dummy arguments
+  CHARACTER(LEN=*),     INTENT(IN)    :: CORDERING_CONVENTION
+  INTEGER(KIND=JPIB_K), INTENT(OUT)   :: IORDERING_CONVENTION
+  TYPE(HOOKS_T),        INTENT(INOUT) :: HOOKS
+
+  !> Function result
+  INTEGER(KIND=JPIB_K) :: RET
+
+  !> Local variables
+  CHARACTER(LEN=LEN_TRIM(CORDERING_CONVENTION)) :: LOC_CORDERING_CONVENTION
+
+  !> Local error codes
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNKNOWN_ORDERING_CONVENTION=1_JPIB_K
+  INTEGER(KIND=JPIB_K), PARAMETER :: ERRFLAG_UNABLE_TO_CONVERT_LC=2_JPIB_K
+
+  ! Local variables declared by the preprocessor for debugging purposes
+  PP_DEBUG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for logging purposes
+  PP_LOG_DECL_VARS
+
+  ! Local variables declared by the preprocessor for tracing purposes
+  PP_TRACE_DECL_VARS
+
+  ! Trace begin of procedure
+  PP_TRACE_ENTER_PROCEDURE()
+
+  ! Initialization of good path return value
+  PP_SET_ERR_SUCCESS( RET )
+
+  !> Initialization of the output variable
+  IORDERING_CONVENTION = UNDEF_PARAM_E
+
+  !> Convert prefix to lowercase
+  PP_TRYCALL(ERRFLAG_UNABLE_TO_CONVERT_LC) TOLOWER( CORDERING_CONVENTION, LOC_CORDERING_CONVENTION, HOOKS )
+
+  !> Select the prefix
+  SELECT CASE ( TRIM(ADJUSTL(LOC_CORDERING_CONVENTION)) )
+  CASE ( 'ring' )
+    IORDERING_CONVENTION = ORDERING_CONVENTION_RING_E
+  CASE ( 'nested' )
+    IORDERING_CONVENTION = ORDERING_CONVENTION_NESTED_E
+  CASE DEFAULT
+    PP_DEBUG_CRITICAL_THROW( ERRFLAG_UNKNOWN_ORDERING_CONVENTION )
+  END SELECT
+
+  ! Trace end of procedure (on success)
+  PP_TRACE_EXIT_PROCEDURE_ON_SUCCESS()
+
+  ! Exit point (On success)
+  RETURN
+
+! Error handler
+PP_ERROR_HANDLER
+
+  ! Initialization of bad path return value
+  PP_SET_ERR_FAILURE( RET )
+
+#if defined( PP_DEBUG_ENABLE_ERROR_HANDLING )
+!$omp critical(ERROR_HANDLER)
+
+  BLOCK
+
+    ! Error handling variables
+    PP_DEBUG_PUSH_FRAME()
+
+    ! Handle different errors
+    SELECT CASE(ERRIDX)
+    CASE (ERRFLAG_UNABLE_TO_CONVERT_LC)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unable to convert to lowercase' )
+    CASE (ERRFLAG_UNKNOWN_ORDERING_CONVENTION)
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unknown HEALPix ordering convention: '//TRIM(ADJUSTL(CORDERING_CONVENTION)) )
+    CASE DEFAULT
+      PP_DEBUG_PUSH_MSG_TO_FRAME( 'unhandled error' )
+    END SELECT
+
+    ! Trace end of procedure (on error)
+    PP_TRACE_EXIT_PROCEDURE_ON_ERROR()
+
+    ! Write the error message and stop the program
+    PP_DEBUG_ABORT
+
+  END BLOCK
+
+!$omp end critical(ERROR_HANDLER)
+#endif
+
+  ! Exit point (on error)
+  RETURN
+
+END FUNCTION CORDERING_CONVENTION2IORDERING_CONVENTION
+#undef PP_PROCEDURE_NAME
+#undef PP_PROCEDURE_TYPE
 
 
 #define PP_PROCEDURE_TYPE 'FUNCTION'

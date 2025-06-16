c079eb5899e7518433426a14a7abbb839fb16530
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-06-16 17:00:39 +0000

    MUL-228: Add (inverse) difference statistics operations tests

---
+++ tests/multio/action/statistics-mtg2/operations/Difference.cc
@@ -0,0 +1,29 @@
+
+#include "eckit/testing/Test.h"
+
+#include "Operation.h"
+
+namespace multio::test::statistics_mtg2 {
+
+class DifferenceTest : public StatisticsOperationTest {
+public:
+
+    DifferenceTest() : StatisticsOperationTest("difference") {}
+
+    double reference(const std::vector<double> &input) {
+        EXPECT_NOT_EQUAL(input.size(), 0);
+        return input[input.size()-1] - input[0];
+    }
+
+};
+
+auto test = DifferenceTest();
+
+CASE("single test") { test.runSingle(); }
+CASE("multiple test") { test.runMultiple(); }
+
+}  // multio::test::statistics_mtg2
+
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}
---
+++ tests/multio/action/statistics-mtg2/operations/InverseDifference.cc
@@ -0,0 +1,29 @@
+
+#include "eckit/testing/Test.h"
+
+#include "Operation.h"
+
+namespace multio::test::statistics_mtg2 {
+
+class InverseDifferenceTest : public StatisticsOperationTest {
+public:
+
+    InverseDifferenceTest() : StatisticsOperationTest("inverse-difference") {}
+
+    double reference(const std::vector<double> &input) {
+        EXPECT_NOT_EQUAL(input.size(), 0);
+        return input[0] - input[input.size()-1];
+    }
+
+};
+
+auto test = InverseDifferenceTest();
+
+CASE("single test") { test.runSingle(); }
+CASE("multiple test") { test.runMultiple(); }
+
+}  // multio::test::statistics_mtg2
+
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}

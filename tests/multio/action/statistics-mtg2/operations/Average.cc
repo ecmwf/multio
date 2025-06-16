4ada611121be10749cf02e5dfaf7fde147b0d7c9
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-06-16 16:06:39 +0000

    MUL-228: Add average statistics operation test

---
+++ tests/multio/action/statistics-mtg2/operations/Average.cc
@@ -0,0 +1,29 @@
+
+#include "eckit/testing/Test.h"
+
+#include "Operation.h"
+
+namespace multio::test::statistics_mtg2 {
+
+class AverageTest : public StatisticsOperationTest {
+public:
+
+    AverageTest() : StatisticsOperationTest("average") {}
+
+    double reference(const std::vector<double> &input) {
+        EXPECT_NOT_EQUAL(input.size(), 0);
+        return std::accumulate(input.begin(), input.end(), 0.0) / input.size();
+    }
+
+};
+
+auto test = AverageTest();
+
+CASE("single test") { test.runSingle(); }
+CASE("multiple test") { test.runMultiple(); }
+
+}  // multio::test::statistics_mtg2
+
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}

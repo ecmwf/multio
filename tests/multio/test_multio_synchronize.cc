e92d6f977b9c7063b7db6228b3458ff9093b3c0c
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-08-01 13:50:03 +0000

    MUL-38: Add tests for synchronization functionality

---
+++ tests/multio/test_multio_synchronize.cc
@@ -0,0 +1,42 @@
+#include "eckit/mpi/Comm.h"
+#include "eckit/exception/Exceptions.h"
+
+#include "multio/api/c/multio_c.h"
+
+void handleError(int error) {
+    if (error != 0) {
+        exit(error);
+    }
+}
+
+int main(int argc, char** argv) {
+    ASSERT(argc <= 2);
+
+    bool synchronize = true;
+    if (argc == 2 && std::string(argv[1]) == "--skip-sync") {
+        synchronize = false;
+    }
+
+    multio_handle_t* multio_handle = nullptr;
+    multio_configuration_t* multio_cc = nullptr;
+
+    handleError(multio_initialise());
+
+    handleError(multio_new_configuration(&multio_cc));
+
+    handleError(multio_new_handle(&multio_handle, multio_cc));
+
+    handleError(multio_delete_configuration(multio_cc));
+
+    handleError(multio_open_connections(multio_handle));
+
+    if (synchronize) {
+        handleError(multio_synchronize(multio_handle));
+    }
+
+    handleError(multio_close_connections(multio_handle));
+
+    eckit::mpi::comm().barrier();  // Needed to match the barrier in `multio-probe --test`
+
+    handleError(multio_delete_handle(multio_handle));
+}

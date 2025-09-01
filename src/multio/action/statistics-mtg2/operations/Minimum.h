d0b65fd5d5f2ff8fb9f5b393f0001d84ff2ccbe5
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-09-01 13:12:14 +0000

    Add isComposable method to statistics-mtg2

--- src/multio/action/statistics-mtg2/operations/Minimum.h
+++ src/multio/action/statistics-mtg2/operations/Minimum.h
@@ -23,6 +23,8 @@ public:
             const StatisticsOptions& opt) :
         OperationWithData<T>{name, "minimum", true, win, IOmanager, opt, std::numeric_limits<T>::max()} {};
 
+    bool isComposable() const override { return true; }
+
     void compute(eckit::Buffer& buf, const StatisticsConfiguration& cfg) override {
         LOG_DEBUG_LIB(LibMultio) << logHeader_ << ".compute().count=" << win_.count() << std::endl;
         auto val = static_cast<T*>(buf.data());

7315b2aa9a9f1945692f8314fe2849bf7299b349
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-09-09 12:59:49 +0000

    MUL-267: Avoid divide by zero / handle missing values

--- src/multio/action/average-rate/AverageRate.cc
+++ src/multio/action/average-rate/AverageRate.cc
@@ -29,8 +29,8 @@ Param2ParamMap getMappings() {
 
     Param2ParamMap paramMappings;
     for (auto& mappings : mappingConf.getSubConfigurations()) {
-        auto paramIn = mappings.getInt64("paramIn");
-        auto paramOut = mappings.getInt64("paramOut");
+        const auto paramIn = mappings.getInt64("paramIn");
+        const auto paramOut = mappings.getInt64("paramOut");
         paramMappings[paramIn] = paramOut;
     }
     return paramMappings;
@@ -47,7 +47,7 @@ void AverageRate::executeImpl(message::Message msg) {
         return;
     }
 
-    auto md = msg.metadata();
+    const auto md = msg.metadata();
 
     // Throw exception on non-statistics messages
     if (!dm::parseEntry(dm::TIMESPAN, md).isSet()) {
@@ -56,8 +56,15 @@ void AverageRate::executeImpl(message::Message msg) {
         throw eckit::UserError(os.str(), Here());
     }
 
+    // Throw exception on multi-loop statistics messages
+    if (dm::parseEntry(dm::STATTYPE, md).isSet()) {
+        std::ostringstream os;
+        os << "Action average-rate cannot process multi-loop statistics messages : " << msg;
+        throw eckit::UserError(os.str(), Here());
+    }
+
     // Find and apply the mapping for this param, throw exception if not available
-    auto param = md.get<std::int64_t>(dm::legacy::Param);
+    const auto param = md.get<std::int64_t>(dm::legacy::Param);
     if (auto search = paramMappings_.find(param); search != paramMappings_.end()) {
         msg.acquire();
         msg.modifyMetadata().set(dm::legacy::Param, search->second);
@@ -78,15 +85,26 @@ void AverageRate::executeImpl(message::Message msg) {
 
 template <typename Precision>
 void AverageRate::compute(message::Message& msg) const {
-    size_t size = msg.payload().size() / sizeof(Precision);
+    const size_t size = msg.payload().size() / sizeof(Precision);
     auto* data = static_cast<Precision*>(msg.payload().modifyData());
+    const auto& md = msg.metadata();
 
-    auto timespan = dm::parseEntry(dm::TIMESPAN, msg.metadata());
+    const auto timespan = dm::parseEntry(dm::TIMESPAN, md);
     ASSERT(timespan.isSet());
-    const double c = 1.0 / timespan.get().toSeconds();
+    const auto timespanInSeconds = timespan.get().toSeconds();
+    ASSERT(timespanInSeconds > 0);
+    const double c = 1.0 / timespanInSeconds;
 
-    std::transform(data, data + size, data,
-                   [c](Precision value) { return static_cast<Precision>(value * c); });
+    // Compute with/without missing value
+    const auto missingValue = dm::parseEntry(dm::MissingValue, md);
+    if (missingValue.isSet()) {
+        const auto m = static_cast<Precision>(missingValue.get());
+        std::transform(data, data + size, data,
+                       [c, m](Precision v) { return static_cast<Precision>(v == m ? m : v * c); });
+    } else {
+        std::transform(data, data + size, data,
+                       [c](Precision v) { return static_cast<Precision>(v * c); });
+    }
 }
 
 template void AverageRate::compute<float>(message::Message&) const;

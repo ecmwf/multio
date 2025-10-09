1c15e0826a5f477d1cf5c274ff1ab5c07b00f717
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-10-09 10:05:21 +0000

    MUL-310: Add config parser utility
    Co-authored-by: Philipp Geier <philipp.geier@ecmwf.int>

---
+++ src/multio/util/config/Parser.h
@@ -0,0 +1,56 @@
+/*
+ * (C) Copyright 2025- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#pragma once
+
+#include <string_view>
+
+#include "eckit/config/LocalConfiguration.h"
+
+#include "detail/Config.h"
+#include "detail/Entry.h"
+
+
+namespace multio::util::config {
+
+
+/**
+ * Register a required field in the configuration
+ * @param key String value of this field
+ * @param value Location where parsed value will be stored
+ */
+template <typename TConfig, typename TValue>
+constexpr detail::Entry<TConfig, TValue> requiredEntry(const std::string_view& key, TValue TConfig::* value) {
+    return {key, value, true};
+}
+
+/**
+ * Register an optional (or defaulted) field in the configuration
+ * @param key String value of this field
+ * @param value Location where parsed value will be stored
+ */
+template <typename TConfig, typename TValue>
+constexpr detail::Entry<TConfig, TValue> optionalEntry(const std::string_view& key, TValue TConfig::* value) {
+    return {key, value, false};
+}
+
+/**
+ * Create a parsed configuration object from the eckit::LocalConfiguration
+ * @param localConfig An unparsed eckit::LocalConfiguration
+ * @returns A parsed configuration of the desired type
+ * @throws If an unknown key is present in the localConfig, or if a value cannot be parsed
+ */
+template <typename TConfig>
+TConfig parseConfig(const eckit::LocalConfiguration& localConfig) {
+    return detail::parseConfig<TConfig>(localConfig);
+}
+
+
+}  // namespace multio::util::config
---
+++ src/multio/util/config/detail/Entry.cc
@@ -0,0 +1,134 @@
+#include "Entry.h"
+
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+
+#include "eckit/config/LocalConfiguration.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/log/CodeLocation.h"
+
+namespace multio::util::config::detail {
+
+
+void parseIntStrict(std::int64_t& value, const std::string& str) {
+    size_t pos;
+    try {
+        value = std::stol(str, &pos);
+    }
+    catch (std::invalid_argument e) {
+        throw eckit::UserError("Cannot convert '" + str + "' to int64 : input is not numeric");
+    }
+    catch (std::out_of_range e) {
+        throw eckit::UserError("Cannot convert '" + str + "' to int64 : output value is out of range");
+    }
+    if (pos != str.size()) {
+        throw eckit::UserError("Cannot convert '" + str + "' to int64 : input contains trailing characters");
+    }
+}
+
+void parseDoubleStrict(double& value, const std::string& str) {
+    size_t pos;
+    try {
+        value = std::stod(str, &pos);
+    }
+    catch (std::invalid_argument e) {
+        throw eckit::UserError("Cannot convert '" + str + "' to double : input is not numeric");
+    }
+    catch (std::out_of_range e) {
+        throw eckit::UserError("Cannot convert '" + str + "' to double : output value is out of range");
+    }
+    if (pos != str.size()) {
+        throw eckit::UserError("Cannot convert '" + str + "' to double : input contains trailing characters");
+    }
+}
+
+bool parseEntry(std::string& value, const std::string& key, const eckit::LocalConfiguration& localConfig) {
+    if (!localConfig.has(key)) {
+        return false;
+    }
+    if (localConfig.isString(key)) {
+        value = localConfig.getString(key);
+        return true;
+    }
+    throw eckit::UserError{"Could not convert value of key '" + key + "' to string : no conversion method defined",
+                           Here()};
+}
+
+bool parseEntry(std::int64_t& value, const std::string& key, const eckit::LocalConfiguration& localConfig) {
+    if (!localConfig.has(key)) {
+        return false;
+    }
+    if (localConfig.isIntegral(key)) {
+        value = localConfig.getInt64(key);
+        return true;
+    }
+    if (localConfig.isString(key)) {
+        parseIntStrict(value, localConfig.getString(key));
+        return true;
+    }
+    throw eckit::UserError{"Could not convert value of key '" + key + "' to int64 : no conversion method defined",
+                           Here()};
+}
+
+bool parseEntry(double& value, const std::string& key, const eckit::LocalConfiguration& localConfig) {
+    if (!localConfig.has(key)) {
+        return false;
+    }
+    if (localConfig.isFloatingPoint(key)) {
+        value = localConfig.getDouble(key);
+        return true;
+    }
+    if (localConfig.isIntegral(key)) {
+        value = static_cast<double>(localConfig.getInt64(key));
+        return true;
+    }
+    if (localConfig.isString(key)) {
+        parseDoubleStrict(value, localConfig.getString(key));
+        return true;
+    }
+    throw eckit::UserError{"Could not convert value of key '" + key + "' to double : no conversion method defined",
+                           Here()};
+}
+
+bool parseEntry(bool& value, const std::string& key, const eckit::LocalConfiguration& localConfig) {
+    if (!localConfig.has(key)) {
+        return false;
+    }
+    if (localConfig.isBoolean(key)) {
+        value = localConfig.getBool(key);
+        return true;
+    }
+    if (localConfig.isIntegral(key)) {
+        const int64_t configValue = localConfig.getInt64(key);
+        if (configValue == 0) {
+            value = false;
+            return true;
+        }
+        if (configValue == 1) {
+            value = true;
+            return true;
+        }
+        throw eckit::UserError{"Integer value of boolean key '" + key + "' must be '1' or '0', given value was '"
+                                   + std::to_string(configValue) + "'",
+                               Here()};
+    }
+    if (localConfig.isString(key)) {
+        const std::string& configValue = localConfig.getString(key);
+        if (configValue == "0") {
+            value = false;
+            return true;
+        }
+        if (configValue == "1") {
+            value = true;
+            return true;
+        }
+        throw eckit::UserError{
+            "String value of boolean key '" + key + "' must be '1' or '0', given value was '" + configValue + "'",
+            Here()};
+    }
+    throw eckit::UserError{"Could not convert value of key '" + key + "' to double : no conversion method defined",
+                           Here()};
+}
+
+}  // namespace multio::util::config::detail

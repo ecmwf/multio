178de79bcdc9f68542d9baff7d231c6a3ebf2777
Kevin Nobel <kevin.nobel@ecmwf.int>
2025-12-16 14:44:12 +0000

    Call GRIB2 encoder in metkit from encode-mtg2 action

--- src/multio/action/encode-mtg2/EncodeMtg2.cc
+++ src/multio/action/encode-mtg2/EncodeMtg2.cc
@@ -11,36 +11,34 @@
 #include "EncodeMtg2.h"
 
 #include <iostream>
-#include <type_traits>
 
+#include "eckit/config/LocalConfiguration.h"
 #include "eckit/exception/Exceptions.h"
 
 #include "multio/action/encode-mtg2/AtlasGeoSetter.h"
 #include "multio/action/encode-mtg2/EncodeMtg2Exception.h"
 #include "multio/datamod/MarsMiscGeo.h"
+#include "multio/datamod/core/EntryDumper.h"
 #include "multio/datamod/core/EntryParser.h"
 #include "multio/datamod/core/Record.h"
-#include "multio/mars2grib/api/RawAPI.h"
 #include "multio/mars2mars/Rules.h"
 #include "multio/message/Parametrization.h"
 #include "multio/util/PrecisionTag.h"
 
-namespace multio::action {
+namespace multio::action::encode_mtg2 {
 
 namespace dm = multio::datamod;
 
 using message::Message;
 using message::Peer;
 
-mars2grib::RawOptions mapOpts(EncodeMtg2Options opts) {
-    mars2grib::RawOptions ret;
-    ret.cached = opts.cached;
-    return ret;
-};
-
 
 EncodeMtg2::EncodeMtg2(const ComponentConfiguration& compConf) :
-    ChainedAction{compConf}, opts_{cf::parseActionConfig<EncodeMtg2Options>(compConf)}, mars2grib_{mapOpts(opts_)} {}
+    ChainedAction{compConf}, opts_{cf::parseActionConfig<EncodeMtg2Options>(compConf)}, encoder_{} {
+    if (opts_.cached) {
+        throw eckit::NotImplemented{"Encoder cache is currently not implemented!", Here()};
+    }
+}
 
 
 void EncodeMtg2::executeImpl(Message msg) {
@@ -49,94 +47,76 @@ void EncodeMtg2::executeImpl(Message msg) {
         return;
     }
 
+    if (msg.payload().size() == 0) {
+        throw EncodeMtg2Exception("Message has empty payload - no values to encode", Here());
+    }
+
     auto& md = msg.metadata();
 
-    {
-        // Read and set unscoped mars keys
-        auto marsRec = dm::readRecord<dm::FullMarsRecord>(md);
+    // Read and set unscoped mars keys
+    auto marsRec = dm::readRecord<dm::FullMarsRecord>(md);
 
-        // Read scoped misc keys
-        auto scopedMiscRec = dm::scopeRecord(dm::MiscRecord{});
-        dm::readRecord(scopedMiscRec, md);
-        // Write unscoped misc keys
-        auto miscRec = dm::unscopeRecord(std::move(scopedMiscRec));
+    // Read scoped misc keys
+    auto scopedMiscRec = dm::scopeRecord(dm::MiscRecord{});
+    dm::readRecord(scopedMiscRec, md);
+    // Write unscoped misc keys
+    auto miscRec = dm::unscopeRecord(std::move(scopedMiscRec));
 
-        auto scopedGeo = dm::getGeometryRecord(marsRec);
+    auto scopedGeom = dm::getGeometryRecord(marsRec);
 
-        // If grid.. check if atlas is given.
-        if (marsRec.grid.isSet()) {
-            std::string scope{std::visit([](const auto& k) { return dm::getRecordScope(k); }, scopedGeo)};
-            const auto& global = message::Parametrization::instance().get();
-            // Fetch atlas and store in global parametrization (by scoping keys...)
-            // Scoping here may be refactored
-            if (opts_.geoFromAtlas && (global.find(scope) == global.end())) {
-                extract::AtlasGeoSetter::handleGrid(scope, marsRec.grid.get());
-            }
+    // If grid.. check if atlas is given.
+    if (marsRec.grid.isSet()) {
+        std::string scope{std::visit([](const auto& k) { return dm::getRecordScope(k); }, scopedGeom)};
+        const auto& global = message::Parametrization::instance().get();
+        // Fetch atlas and store in global parametrization (by scoping keys...)
+        // Scoping here may be refactored
+        if (opts_.geoFromAtlas && (global.find(scope) == global.end())) {
+            extract::AtlasGeoSetter::handleGrid(scope, marsRec.grid.get());
         }
-
-        // Read & unscope geo keys from metadata
-        auto geo = std::visit(
-            [&](auto&& geoRec) -> dm::Geometry {
-                dm::readRecord(geoRec, md);
-                return unscopeRecord(std::move(geoRec));
-            },
-            std::move(scopedGeo));
-
-
-        // Apply mappings
-        auto mappingResult = mars2mars::applyMappings(mars2mars::allRules(), marsRec, miscRec);
-
-
-        std::unique_ptr<metkit::codes::CodesHandle> sample = mars2grib_.getHandle(marsRec, miscRec, geo);
-
-        if (msg.payload().size() == 0) {
-            throw EncodeMtg2Exception("Message has empty payload - no values to encode", Here());
-        }
-
-        // ecCodes does not support setting float values (yet)
-        auto setValuesAsDouble = [&] (const auto* data, size_t size) {
-            using Precision = std::decay_t<decltype(*data)>;
-            if constexpr (std::is_same_v<Precision, double>) {
-                sample->set("values", metkit::codes::Span<const Precision>(data, size));
-            }
-            else {
-                sample->set("values", std::vector<double>{data, data+size});
-            }
-        };
-
-        executeNext(dispatchPrecisionTag(msg.precision(), [&](auto pt) {
-            using Precision = typename decltype(pt)::type;
-            size_t size = msg.payload().size() / sizeof(Precision);
-
-            // Check if values need scaling
-            if (mappingResult && mappingResult->valuesScaleFactor) {
-                msg.payload().acquire();
-                auto values = static_cast<Precision*>(msg.payload().modifyData());
-                ASSERT(values);
-
-                const auto scaleFactor = *(mappingResult->valuesScaleFactor);
-                std::transform(values, values + size, values,
-                               [&](const Precision& value) -> Precision { return value * scaleFactor; });
-
-                setValuesAsDouble(values, size);
-            }
-            else {
-                // No scaling
-                auto values = static_cast<const Precision*>(msg.payload().data());
-                setValuesAsDouble(values, size);
-            }
-
-            // The +32 is related to bug
-            // TODO(pgeier) Track bug ECC-2130: https://jira.ecmwf.int/browse/ECC-2130
-            eckit::Buffer buf{sample->messageSize()};
-            sample->copyInto(reinterpret_cast<uint8_t*>(buf.data()), buf.size());
-
-            // TODO(pgeier) write mapped metadata
-            return Message{Message::Header{Message::Tag::Field, Peer{msg.source()}, Peer{msg.destination()},
-                                           dm::dumpRecord<message::Metadata>(marsRec)},
-                           std::move(buf)};
-        }));
     }
+
+    // Read & unscope geo keys from metadata
+    auto geomRec = std::visit(
+        [&](auto&& scopedGeom) -> dm::Geometry {
+            dm::readRecord(scopedGeom, md);
+            return unscopeRecord(std::move(scopedGeom));
+        },
+        std::move(scopedGeom));
+
+    // Apply mappings
+    auto mappingResult = mars2mars::applyMappings(mars2mars::allRules(), marsRec, miscRec);
+
+    const auto mars = dm::dumpRecord<eckit::LocalConfiguration>(marsRec);
+    const auto misc = dm::dumpRecord<eckit::LocalConfiguration>(miscRec);
+    const auto geom
+        = std::visit([](const auto& geomRec) { return dm::dumpRecord<eckit::LocalConfiguration>(geomRec); }, geomRec);
+
+    executeNext(dispatchPrecisionTag(msg.precision(), [&](auto pt) {
+        using Precision = typename decltype(pt)::type;
+        msg.payload().acquire();
+        auto values = static_cast<Precision*>(msg.payload().modifyData());
+        size_t size = msg.payload().size() / sizeof(Precision);
+
+        // Check if values need scaling
+        if (mappingResult && mappingResult->valuesScaleFactor) {
+            ASSERT(values);
+
+            const auto scaleFactor = *(mappingResult->valuesScaleFactor);
+            std::transform(values, values + size, values,
+                           [&](const Precision& value) -> Precision { return value * scaleFactor; });
+        }
+
+        // Call the GRIB2 encoder in metkit
+        const auto sample = encoder_.encode(mars, misc, geom, values, size);
+
+        eckit::Buffer buf{sample->messageSize()};
+        sample->copyInto(reinterpret_cast<uint8_t*>(buf.data()), buf.size());
+
+        // TODO(pgeier) write mapped metadata
+        return Message{Message::Header{Message::Tag::Field, Peer{msg.source()}, Peer{msg.destination()},
+                                       dm::dumpRecord<message::Metadata>(marsRec)},
+                       std::move(buf)};
+    }));
 }
 
 void EncodeMtg2::print(std::ostream& os) const {
@@ -146,4 +126,4 @@ void EncodeMtg2::print(std::ostream& os) const {
 
 static ActionBuilder<EncodeMtg2> EncodeMtg2Builder("encode-mtg2");
 
-}  // namespace multio::action
+}  // namespace multio::action::encode_mtg2

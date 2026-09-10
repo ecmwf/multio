646ef704b2d4e76d0fb22197d30fbd35b86e975c
Kevin Nobel <kevin.nobel@ecmwf.int>
2026-09-10 20:35:04 +0000

    Remove mars2mars mapping from encode-mtg2 action

--- src/multio/action/encode-mtg2/EncodeMtg2.cc
+++ src/multio/action/encode-mtg2/EncodeMtg2.cc
@@ -22,7 +22,6 @@
 
 #include "multio/action/encode-mtg2/EncodeMtg2Exception.h"
 #include "multio/datamod/MarsMiscGeo.h"
-#include "multio/mars2mars/Rules.h"
 #include "multio/message/Parametrization.h"
 #include "multio/util/PrecisionTag.h"
 
@@ -85,33 +84,11 @@ void EncodeMtg2::executeImpl(Message msg) {
     auto marsRec = dm::readRecord<dm::FullMarsRecord>(md);
     auto miscRec = dm::readRecord<dm::MiscRecord>(md);
 
-    // Apply mappings
-    auto mappingResult = mars2mars::applyMappings(mars2mars::allRules(), marsRec, miscRec);
-
     executeNext(dispatchPrecisionTag(msg.precision(), [&](auto pt) {
         using Precision = typename decltype(pt)::type;
         msg.payload().acquire();
         auto values = static_cast<Precision*>(msg.payload().modifyData());
         size_t size = msg.payload().size() / sizeof(Precision);
-
-        // Check if values need scaling
-        if (mappingResult && mappingResult->valuesScaleFactor) {
-            ASSERT(values);
-
-            const auto scaleFactor = *(mappingResult->valuesScaleFactor);
-
-            if (miscRec.missingValue.isSet()) {
-                const double missing = miscRec.missingValue.get();
-                std::transform(values, values + size, values, [&](const Precision& value) -> Precision {
-                    return static_cast<Precision>(value == missing ? missing : value * scaleFactor);
-                });
-            }
-            else {
-                std::transform(values, values + size, values, [&](const Precision& value) -> Precision {
-                    return static_cast<Precision>(value * scaleFactor);
-                });
-            }
-        }
 
         // Call the GRIB2 encoder in metkit
         const auto sample = encode(encoder_, cache_, values, size, marsRec, miscRec);
